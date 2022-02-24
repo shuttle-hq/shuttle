fd3ace88d7077c44f0a6747c0be63c9fff7e42ce
Christos <christos@openquery.io>
2022-02-24 13:37:27 +0000

    it compiles

--- api/src/proxy.rs
+++ api/src/proxy.rs
@@ -3,29 +3,32 @@ use std::future::Future;
 use std::net::{IpAddr, SocketAddr};
 use std::sync::Arc;
 use hyper_reverse_proxy::ProxyError;
-use rocket::futures::TryFutureExt;
-use rocket::http::hyper;
-use rocket::http::hyper::{Body, HeaderValue, make_service_fn, Request, Response, Server, service_fn};
+use ::hyper::server::{Server, conn::AddrStream};
+use ::hyper::{Body, Request, Response, StatusCode};
+use ::hyper::service::{service_fn, make_service_fn};
+use ::hyper::http::uri::InvalidUri;
 use lib::Port;
-use crate::DeploymentSystem;
 
+use crate::DeploymentSystem;
 use crate::router::Router;
 
-async fn start_proxy(proxy_port: Port, api_port: Port, deployment_manager: Arc<DeploymentSystem>) {
+pub(crate) async fn start(proxy_port: Port, api_port: Port, deployment_manager: Arc<DeploymentSystem>) {
     let socket_address = ([127, 0, 0, 1], proxy_port).into();
 
     // A `Service` is needed for every connection.
     let make_svc = make_service_fn(|socket: &AddrStream| {
+        let dm_ref = deployment_manager.clone();
         let remote_addr = socket.remote_addr();
         async move {
-            service_fn(move |req| handle(remote_addr, req, api_port, deployment_manager))
+            Ok::<_, Infallible>(service_fn(move |req| handle(remote_addr, req, api_port, dm_ref.clone())))
         }
     });
 
-    let server = Server::bind(&socket_address)
-        .serve(make_svc)
-        .map_err(|e| eprintln!("server error: {}", e))
-        .await;
+    let server = Server::bind(&socket_address).serve(make_svc);
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+    // todo, need to kill everything if proxy dies
 }
 
 async fn handle(
@@ -33,7 +36,7 @@ async fn handle(
     req: Request<Body>,
     api_port: Port,
     deployment_manager: Arc<DeploymentSystem>,
-) -> Result<Response<Body>, ProxyError> {
+) -> Result<Response<Body>, Infallible> {
     // if no subdomain or `unveil.sh`, route to our API.
     let port = match req.headers().get("Host") {
         None => Some(api_port),
@@ -55,12 +58,27 @@ async fn handle(
     };
 
     // let's proxy
-    reverse_proxy(
+    match reverse_proxy(
         remote_addr.ip(),
         port,
         path,
         req,
-    ).await
+    ).await {
+        Ok(response) => { Ok(response) }
+        Err(error) => {
+            match error {
+                ProxyError::InvalidUri(e) => { dbg!("error while handling request in reverse proxy: {}", e); }
+                ProxyError::HyperError(e) => { dbg!("error while handling request in reverse proxy: {}", e); }
+                ProxyError::ForwardHeaderError => { dbg!("error while handling request in reverse proxy: 'fwd header error'"); }
+            };
+            Ok(
+                Response::builder()
+                    .status(StatusCode::INTERNAL_SERVER_ERROR)
+                    .body(Body::empty())
+                    .unwrap()
+            )
+        }
+    }
 }
 
 async fn reverse_proxy(ip: IpAddr, port: Port, path: &str, req: Request<Body>) -> Result<Response<Body>, ProxyError> {

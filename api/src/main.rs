ca4a90eb260dd4db370a9f9190297021660c1a5d
Christos <christos@openquery.io>
2022-03-01 13:18:17 +0000

    logging

--- api/src/main.rs
+++ api/src/main.rs
@@ -57,7 +57,10 @@ struct ApiState {
 //noinspection ALL
 #[launch]
 async fn rocket() -> _ {
-    env_logger::init();
+    env_logger::Builder::new()
+        .filter_module("rocket", log::LevelFilter::Info)
+        .filter_module("_", log::LevelFilter::Info)
+        .init();
 
     let args: Args = Args::from_args();
     let build_system = FsBuildSystem::initialise(args.path).unwrap();

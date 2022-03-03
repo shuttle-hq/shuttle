3de48fb0b70c7ebc82dbc72b5d66373736ca6429
Christos Hadjiaslanis <christos@openquery.io>
2022-03-03 10:42:28 +0000

    feat: do not require unveil toml

--- cargo-unveil/src/main.rs
+++ cargo-unveil/src/main.rs
@@ -3,15 +3,18 @@ mod client;
 mod config;
 
 use crate::args::{Args, DeleteArgs, DeployArgs, StatusArgs};
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
 use cargo::core::resolver::CliFeatures;
 use cargo::core::Workspace;
 use cargo::ops::{PackageOpts, Packages};
 use std::env;
 use std::fs::File;
+use std::io::ErrorKind;
 use std::path::Path;
 use std::rc::Rc;
+use cargo_metadata::MetadataCommand;
 use structopt::StructOpt;
+use lib::ProjectConfig;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -40,13 +43,38 @@ async fn status(args: StatusArgs) -> Result<()> {
 async fn deploy(args: DeployArgs) -> Result<()> {
     let working_directory = env::current_dir()?;
     let api_key = config::get_api_key().context("failed to retrieve api key")?;
-    let project = config::get_project(&working_directory)
+    let project = get_project(&working_directory)
         .context("failed to retrieve project configuration")?;
     let package_file = run_cargo_package(&working_directory, args.allow_dirty)
         .context("failed to package cargo project")?;
     client::deploy(package_file, api_key, project)
         .await
         .context("failed to deploy cargo project")
+}
+
+/// Tries to get the project configuration.
+/// Will first check for an Unveil.toml via `config`.
+///
+/// If it cannot find it, it will create one using sensible
+/// default values such as the name of the crate.
+fn get_project(working_directory: &Path) -> Result<ProjectConfig> {
+    let config = match config::get_project(&working_directory)? {
+        Some(config) => config,
+        None => {
+            let meta = MetadataCommand::new()
+                .current_dir(&working_directory)
+                .exec()
+                .unwrap();
+            let package_name = meta.root_package()
+                .ok_or(anyhow!("could not find Cargo.toml in {:?}", &working_directory))?
+                .name
+                .clone();
+            ProjectConfig {
+                name: package_name
+            }
+        }
+    };
+    Ok(config)
 }
 
 // Packages the cargo project and returns a File to that file

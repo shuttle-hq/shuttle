cea294473f7ade2d7be4c476220b17cef18a02c4
Nodar Daneliya <NodarD@users.noreply.github.com>
2022-03-11 16:31:40 +0000

    header tweak

--- www/lib/constants.ts
+++ www/lib/constants.ts
@@ -1,6 +1,6 @@
 export const APP_NAME = 'Shuttle'
 
-export const SITE_TITLE = 'Deploy a Rust app with a single Cargo command'
+export const SITE_TITLE = 'Deploy Rust apps with a single Cargo command'
 
 export const SITE_DESCRIPTION = 'Shuttle is a crate that uses traits and annotations to configure your backend deployment - including databases.'
 

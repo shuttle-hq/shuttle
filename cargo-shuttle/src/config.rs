2af3932d33f02ec103bdabfed71731419352896f
Christos <christos@openquery.io>
2022-03-14 19:21:07 +0000

    feat: cargo auth

--- cargo-shuttle/src/config.rs
+++ cargo-shuttle/src/config.rs
@@ -1,15 +1,39 @@
 use shuttle_common::ApiKey;
-use serde::Deserialize;
-use std::io::ErrorKind;
+use serde::{Deserialize, Serialize};
+use std::io::{ErrorKind, Write};
 use std::path::{Path, PathBuf};
 
 use anyhow::{anyhow, Result};
 use shuttle_common::project::ProjectConfig;
 
 /// Global client config for things like API keys.
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub(crate) struct Config {
     api_key: ApiKey,
+}
+
+pub(crate) fn create_with_api_key(api_key: String) -> Result<()> {
+    let mut directory = shuttle_config_dir()?;
+    std::fs::create_dir_all(&directory)?;
+
+    let file_path = shuttle_config_file(&mut directory);
+    let mut config_file = std::fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(file_path)?;
+
+    let config = Config {
+        api_key
+    };
+
+    Ok(write!(config_file, "{}", toml::to_string_pretty(&config)?)?)
+}
+
+pub(crate) fn config_file_exists() -> Result<bool> {
+    let mut directory = shuttle_config_dir()?;
+    let file_path = shuttle_config_file(&mut directory);
+    Ok(file_path.exists())
 }
 
 pub(crate) fn get_api_key() -> Result<ApiKey> {

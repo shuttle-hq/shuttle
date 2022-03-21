194383d24cd6b40bd619d0992388015c60f7ce4d
Tsotne Nazarashvili <cotne.95@gmail.com>
2022-03-21 13:22:00 +0400

    feat: use github connection by default (#90)

--- www/pages/api/auth/[...auth0].ts
+++ www/pages/api/auth/[...auth0].ts
@@ -1,15 +1,15 @@
-import { handleAuth, handleCallback } from '@auth0/nextjs-auth0';
-import { getApiKey } from '../../../lib/shuttle-api';
+import { handleAuth, handleCallback, handleLogin } from "@auth0/nextjs-auth0";
+import { getApiKey } from "../../../lib/shuttle-api";
 
-async function afterCallback  (req, res, session, state) {
+async function afterCallback(req, res, session, state) {
   try {
-    session.user.api_key = await getApiKey(session.user.sub.replace('|', '-'));
+    session.user.api_key = await getApiKey(session.user.sub.replace("|", "-"));
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
 
   return session;
-};
+}
 
 export default handleAuth({
   async callback(req, res) {
@@ -18,5 +18,16 @@ export default handleAuth({
     } catch (error) {
       res.status(error.status || 500).end(error.message);
     }
-  }
+  },
+  async login(req, res) {
+    try {
+      await handleLogin(req, res, {
+        authorizationParams: {
+          connection: "github",
+        },
+      });
+    } catch (error) {
+      res.status(error.status || 400).end(error.message);
+    }
+  },
 });

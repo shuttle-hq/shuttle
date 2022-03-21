d0b47718819f50f8066220a850d42f089208fd87
chesedo <pieter@chesedo.me>
2022-03-21 16:44:17 +0200

    feat: custom axum example

--- examples/custom/axum/src/lib.rs
+++ examples/custom/axum/src/lib.rs
@@ -1,6 +1,7 @@
 use async_trait::async_trait;
 use axum::{routing::get, Router};
 use shuttle_service::{rocket::tokio::runtime::Runtime, IntoService, Service};
+use sync_wrapper::SyncWrapper;
 
 #[macro_use]
 extern crate shuttle_service;
@@ -17,31 +18,40 @@ fn axum() -> Routes {
     Routes(router)
 }
 
-declare_service!(Routes<_>, axum);
+declare_service!(Routes, axum);
 
 impl IntoService for Routes {
     type Service = CustomService;
 
     fn into_service(self) -> Self::Service {
         CustomService {
-            router: Some(self.0),
+            router: Some(SyncWrapper::new(self.0)),
             runtime: Runtime::new().unwrap(),
         }
     }
 }
 
 struct CustomService {
-    router: Option<Router>,
+    router: Option<SyncWrapper<Router>>,
     runtime: Runtime,
 }
 
 #[async_trait]
 impl Service for CustomService {
     fn bind(&mut self, addr: std::net::SocketAddr) -> Result<(), shuttle_service::Error> {
-        let router = self.router.take().expect("service has already been bound");
+        let router = self
+            .router
+            .take()
+            .expect("service has already been bound")
+            .into_inner();
 
-        let launched = axum::Server::bind(&addr).serve(router.into_make_service());
-        self.runtime.block_on(launched).unwrap();
+        self.runtime
+            .block_on(async {
+                axum::Server::bind(&addr)
+                    .serve(router.into_make_service())
+                    .await
+            })
+            .unwrap();
 
         Ok(())
     }

0b0c4f7ff56177e4541c32ee29a8010759ab31be
chesedo <pieter@chesedo.me>
2022-03-23 17:20:28 +0200

    feat: test sqlx pool

--- service/tests/loader.rs
+++ service/tests/loader.rs
@@ -11,12 +11,12 @@ struct DummyFactory {}
 #[async_trait]
 impl Factory for DummyFactory {
     async fn get_sql_connection_string(&mut self) -> Result<String, Error> {
-        todo!()
+        Ok("postgres://postgres:password@localhost".to_string())
     }
 }
 
 #[tokio::test]
-async fn hello_world() {
+async fn sleep_async() {
     Command::new("cargo")
         .args(["build"])
         .current_dir("tests/resources/sleep-async")
@@ -27,6 +27,26 @@ async fn hello_world() {
 
     let loader =
         Loader::from_so_file("tests/resources/sleep-async/target/debug/libsleep_async.so").unwrap();
+
+    let mut factory = DummyFactory {};
+    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8001);
+    let handler = loader.load(&mut factory, addr).unwrap();
+
+    handler.await.unwrap().unwrap();
+}
+
+#[tokio::test]
+async fn sqlx_pool() {
+    Command::new("cargo")
+        .args(["build"])
+        .current_dir("tests/resources/sqlx-pool")
+        .spawn()
+        .unwrap()
+        .wait()
+        .unwrap();
+
+    let loader =
+        Loader::from_so_file("tests/resources/sqlx-pool/target/debug/libsqlx_pool.so").unwrap();
 
     let mut factory = DummyFactory {};
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8001);

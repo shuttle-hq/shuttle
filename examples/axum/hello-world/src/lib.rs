4f1c71aea2d7f606e510cd6fba121effbb370a7f
chesedo <pieter@chesedo.me>
2022-03-30 15:01:10 +0200

    refactor: rename to hello_world

--- examples/axum/hello-world/src/lib.rs
+++ examples/axum/hello-world/src/lib.rs
@@ -1,13 +1,13 @@
 use axum::{routing::get, Router};
 use sync_wrapper::SyncWrapper;
 
-async fn root() -> &'static str {
+async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
 #[shuttle_service::main]
 async fn axum() -> Result<SyncWrapper<Router>, shuttle_service::Error> {
-    let router = Router::new().route("/hello", get(root));
+    let router = Router::new().route("/hello", get(hello_world));
     let sync_wrapper = SyncWrapper::new(router);
 
     Ok(sync_wrapper)

f7366d54d0cb0e068aae8e5a7f5fb3d7bfd89344
David Laban <alsuren@gmail.com>
2022-04-09 21:46:17 +0100

    test what happens if run in src dir (it ain't pretty)

--- cargo-shuttle/tests/integration/config.rs
+++ cargo-shuttle/tests/integration/config.rs
@@ -30,6 +30,29 @@ fn get_local_config_finds_name_in_shuttle_toml() {
 }
 
 #[test]
+fn fixme_running_in_src_subdir_finds_crate_but_fails_to_find_config() {
+    let project_args = ProjectArgs {
+        working_directory: path_from_workspace_root("examples/axum/hello-world/src"),
+        name: None,
+    };
+
+    let local_config = RequestContext::get_local_config(&project_args).unwrap();
+
+    // FIXME: make a helper for this?
+    let name = local_config
+        .as_ref()
+        .unwrap()
+        .name
+        .as_ref()
+        .unwrap()
+        .to_string();
+
+    // FIXME: this is not the intended behaviour. We should fix this.
+    // This should really be "hello-world-axum-app", as above.
+    assert_eq!(name, "hello-world");
+}
+
+#[test]
 fn setting_name_overrides_name_in_config() {
     let project_args = ProjectArgs {
         working_directory: path_from_workspace_root("examples/axum/hello-world/"),

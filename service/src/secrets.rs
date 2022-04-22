de25f34549172072d20928b3ebe591f7e8cb3e92
WiredSound <maxoblack@yahoo.com>
2022-04-22 16:22:39 +0100

    fix: readd secrets table creation code

--- service/src/secrets.rs
+++ service/src/secrets.rs
@@ -30,7 +30,8 @@ fn check_and_lower_secret_key(key: &str) -> Result<String, Error> {
 /// Abstraction over a simple key/value 'secret' store. This may be used for any number of
 /// purposes, such as storing API keys. Note that secrets are not encrypted and are stored directly
 /// in a table in the database (meaning they can be accessed via SQL rather than this abstraction
-/// should you prefer).
+/// should you prefer). The table in question is created if it is found to not exist every time
+/// either [`get_secret`] or [`set_secret`] is called.
 #[async_trait]
 pub trait SecretStore<DB>
 where
@@ -44,12 +45,14 @@ where
 
     const GET_QUERY: &'static str;
     const SET_QUERY: &'static str;
+    const CREATE_TABLE_QUERY: &'static str;
 
     /// Read the secret with the given key from the database. Will an error if a secret with the
     /// given key does not exist or otherwise could not be accessed.
     async fn get_secret(&self, key: &str) -> Result<String, Error> {
-        let key = check_and_lower_secret_key(key)?;
+        self.execute(sqlx::query(Self::CREATE_TABLE_QUERY)).await?;
 
+        let key = check_and_lower_secret_key(key)?;
         let query = sqlx::query(Self::GET_QUERY).bind(key);
 
         self.fetch_one(query)
@@ -61,9 +64,13 @@ where
     /// Create (or overwrite if already present) a key/value secret in the database. Will error if
     /// the database could not be accessed or execution of the query otherwise failed.
     async fn set_secret(&self, key: &str, val: &str) -> Result<(), Error> {
+        self.execute(sqlx::query(Self::CREATE_TABLE_QUERY)).await?;
+
         let key = check_and_lower_secret_key(key)?;
         let query = sqlx::query(Self::SET_QUERY).bind(key).bind(val);
+
         self.execute(query).await?;
+
         Ok(())
     }
 }
@@ -73,4 +80,11 @@ impl SecretStore<sqlx::Postgres> for sqlx::PgPool {
     const GET_QUERY: &'static str = "SELECT value FROM secrets WHERE key = $1";
     const SET_QUERY: &'static str = "INSERT INTO secrets (key, value) VALUES ($1, $2)
                                      ON CONFLICT (key) DO UPDATE SET value = $2";
+    const CREATE_TABLE_QUERY: &'static str = "
+        CREATE TABLE IF NOT EXISTS secrets (
+            key TEXT UNIQUE NOT NULL,
+            value TEXT NOT NULL,
+            PRIMARY KEY (key)
+        );
+    ";
 }

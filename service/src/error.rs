18b2620c6a90a3349ae29f7d136e85145af7c22a
WiredSound <maxoblack@yahoo.com>
2022-04-22 02:52:49 +0100

    feat: have secrets use proper error system

--- service/src/error.rs
+++ service/src/error.rs
@@ -6,8 +6,11 @@ use thiserror::Error;
 pub enum Error {
     #[error("IO error: {0}")]
     Io(#[from] std::io::Error),
+    #[cfg(feature = "sqlx-postgres")]
+    #[error("Database error: {0}")]
+    Database(#[from] sqlx::Error),
     #[error("Custom error: {0}")]
-    Custom(#[from] CustomError),
+    Custom(#[from] CustomError)
 }
 
 pub type CustomError = anyhow::Error;
--- service/src/secrets.rs
+++ service/src/secrets.rs
@@ -1,16 +1,32 @@
-use regex::Regex;
-use lazy_static::lazy_static;
+use anyhow::anyhow;
 use async_trait::async_trait;
-use sqlx::{Executor, Execute, Row, Database, IntoArguments, Decode, ColumnIndex, Postgres, Type};
-use sqlx::postgres::PgArguments;
+use lazy_static::lazy_static;
+use regex::Regex;
 use sqlx::database::HasArguments;
+use sqlx::postgres::PgArguments;
 use sqlx::query::Query;
+use sqlx::{
+    ColumnIndex,
+    Database,
+    Decode,
+    Execute,
+    Executor,
+    IntoArguments,
+    Postgres,
+    Row,
+    Type
+};
 
-fn check_and_lower_secret_key(key: &str) -> Option<String> {
+use crate::error::Error;
+
+fn check_and_lower_secret_key(key: &str) -> Result<String, Error> {
     lazy_static! {
         static ref VALID_KEY: Regex = Regex::new(r"[_a-zA-Z][_a-zA-Z0-9]*").unwrap();
     }
-    VALID_KEY.is_match(key).then(|| key.to_lowercase())
+    VALID_KEY
+        .is_match(key)
+        .then(|| key.to_lowercase())
+        .ok_or_else(|| Error::Custom(anyhow!("invalid secret key name '{}'", key)))
 }
 
 /// Abstraction over a simple key/value 'secret' store. This may be used for any number of
@@ -32,23 +48,26 @@ where
     const GET_QUERY: &'static str;
     const SET_QUERY: &'static str;
 
-    /// Read the secret with the given key from the database. Will return `None` if a secret with
-    /// the given key does not exist or otherwise could not be accessed.
-    async fn get_secret(&self, key: &str) -> Option<String> {
+    /// Read the secret with the given key from the database. Will an error if a secret with the
+    /// given key does not exist or otherwise could not be accessed.
+    async fn get_secret(&self, key: &str) -> Result<String, Error> {
         let key = check_and_lower_secret_key(key)?;
 
         let query = sqlx::query(Self::GET_QUERY).bind(key);
 
-        self.fetch_one(query).await.map(|row| row.get(0)).ok()
+        self.fetch_one(query)
+            .await
+            .map(|row| row.get(0))
+            .map_err(Error::from)
     }
 
-    /// Create (or overwrite if already present) a key/value secret in the database. Will panic if
+    /// Create (or overwrite if already present) a key/value secret in the database. Will error if
     /// the database could not be accessed or execution of the query otherwise failed.
-    async fn set_secret(&self, key: &str, val: &str) {
-        if let Some(key) = check_and_lower_secret_key(key) {
-            let query = sqlx::query(Self::SET_QUERY).bind(key).bind(val);
-            self.execute(query).await.unwrap();
-        }
+    async fn set_secret(&self, key: &str, val: &str) -> Result<(), Error> {
+        let key = check_and_lower_secret_key(key)?;
+        let query = sqlx::query(Self::SET_QUERY).bind(key).bind(val);
+        self.execute(query).await?;
+        Ok(())
     }
 }
 
@@ -56,5 +75,5 @@ where
 impl SecretStore<sqlx::Postgres> for sqlx::PgPool {
     const GET_QUERY: &'static str = "SELECT value FROM secrets WHERE key = $1";
     const SET_QUERY: &'static str = "INSERT INTO secrets (key, value) VALUES ($1, $2)
-                             ON CONFLICT (key) DO UPDATE SET value = $2";
+                                     ON CONFLICT (key) DO UPDATE SET value = $2";
 }

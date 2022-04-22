8ac0868dac95f221f5584b57f8a947019dfb5b38
chesedo <pieter@chesedo.me>
2022-04-22 10:26:33 +0200

    refactor: remove private container registry from module

--- terraform/containers.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecr_repository" "backend" {
-  name                 = "backend"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}

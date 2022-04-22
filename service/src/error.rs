53635c429bc1080fb4c4d01537160f2f43233179
WiredSound <maxoblack@yahoo.com>
2022-04-22 15:53:22 +0100

    fix: segfault

--- service/src/error.rs
+++ service/src/error.rs
@@ -1,16 +1,23 @@
 //! Types representing various errors that can occur in the process of building and deploying a service.
 
 use thiserror::Error;
+use std::convert::From;
 
 #[derive(Debug, Error)]
 pub enum Error {
     #[error("IO error: {0}")]
     Io(#[from] std::io::Error),
-    #[cfg(feature = "sqlx-postgres")]
     #[error("Database error: {0}")]
-    Database(#[from] sqlx::Error),
+    Database(String),
     #[error("Custom error: {0}")]
     Custom(#[from] CustomError)
 }
 
 pub type CustomError = anyhow::Error;
+
+#[cfg(feature = "sqlx-postgres")]
+impl From<sqlx::Error> for Error {
+    fn from(e: sqlx::Error) -> Self {
+        Error::Database(e.to_string())
+    }
+}

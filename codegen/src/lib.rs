213663b7d4b7683cc97562ba3d57914088fc4170
WiredSound <maxoblack@yahoo.com>
2022-05-12 02:26:15 +0100

    refactor: unused imports

--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -1,4 +1,3 @@
-use std::panic::{UnwindSafe, AssertUnwindSafe};
 use proc_macro::TokenStream;
 use quote::{quote, ToTokens};
 use syn::{parse_macro_input, parse_quote, FnArg, Ident, ItemFn, Pat, ReturnType, Stmt};

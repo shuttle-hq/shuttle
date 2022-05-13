6baee8b0f628c172d71e69775cd724c49e504d46
chesedo <pieter@chesedo.me>
2022-05-13 11:53:10 +0200

    feat: simple RDS support

--- api/src/database.rs
+++ api/src/database.rs
@@ -1,10 +1,14 @@
-use std::time::Duration;
-
+use anyhow::anyhow;
+use aws_config::timeout;
+use aws_sdk_rds::{error::ModifyDBInstanceErrorKind, types::SdkError};
+use aws_smithy_types::tristate::TriState;
 use lazy_static::lazy_static;
 use rand::Rng;
-use shuttle_common::project::ProjectName;
-use shuttle_common::DatabaseReadyInfo;
+use shuttle_common::{project::ProjectName, DatabaseReadyInfo};
+use shuttle_service::error::CustomError;
 use sqlx::postgres::{PgPool, PgPoolOptions};
+use std::time::Duration;
+use tokio::time::sleep;
 
 lazy_static! {
     static ref SUDO_POSTGRES_CONNECTION_STRING: String = format!(
@@ -117,21 +121,128 @@ impl State {
     pub(crate) fn to_info(&self) -> Option<DatabaseReadyInfo> {
         self.info.clone()
     }
+
+    pub(crate) async fn aws_rds(&self) -> Result<String, shuttle_service::Error> {
+        let client = &self.context.rds_client;
+
+        let username = self.project.to_string().replace("-", "_");
+        let password = generate_role_password();
+        let engine = "postgres";
+        let class = "db.t3.micro";
+        let instance_name = format!("{}-{}", self.project, engine);
+        let db_name = "postgres";
+
+        let instances = client
+            .modify_db_instance()
+            .db_instance_identifier(&instance_name)
+            .master_user_password(&password)
+            .send()
+            .await;
+        debug!("got describe response");
+
+        let mut instance = match instances {
+            Ok(instances) => instances.db_instance.unwrap().clone(),
+            Err(SdkError::ServiceError { err, .. }) => {
+                if let ModifyDBInstanceErrorKind::DbInstanceNotFoundFault(_) = err.kind {
+                    debug!("creating new");
+
+                    client
+                        .create_db_instance()
+                        .db_instance_identifier(&instance_name)
+                        .master_username(username)
+                        .master_user_password(&password)
+                        .engine(engine)
+                        .db_instance_class(class)
+                        .allocated_storage(20)
+                        .backup_retention_period(0)
+                        .publicly_accessible(true)
+                        .db_name(db_name)
+                        .send()
+                        .await
+                        .map_err(shuttle_service::error::CustomError::new)?
+                        .db_instance
+                        .unwrap()
+                } else {
+                    return Err(shuttle_service::Error::Custom(anyhow!(
+                        "got unexpected error from AWS: {}",
+                        err
+                    )));
+                }
+            }
+            Err(unexpected) => {
+                return Err(shuttle_service::Error::Custom(anyhow!(
+                    "got unexpected error from AWS: {}",
+                    unexpected
+                )))
+            }
+        };
+
+        // Wait for up
+        debug!("waiting for password update");
+        sleep(Duration::from_secs(30)).await;
+        loop {
+            instance = client
+                .describe_db_instances()
+                .db_instance_identifier(&instance_name)
+                .send()
+                .await
+                .map_err(CustomError::new)?
+                .db_instances
+                .unwrap()
+                .get(0)
+                .unwrap()
+                .clone();
+
+            let status = instance.db_instance_status.as_ref().unwrap().clone();
+
+            debug!("status: {status}");
+            if status == "available" {
+                break;
+            }
+            sleep(Duration::from_secs(1)).await;
+        }
+
+        println!("{instance:#?}");
+        // let info = DatabaseReadyInfo::new(role_name, role_password, database_name);
+        let conn_string = format!(
+            "postgres://{}:{}@{}/{}",
+            instance.master_username.unwrap(),
+            password,
+            instance.endpoint.unwrap().address.unwrap(),
+            db_name
+        );
+
+        Ok(conn_string)
+    }
 }
 
 #[derive(Clone)]
 pub struct Context {
     sudo_pool: PgPool,
+    rds_client: aws_sdk_rds::Client,
 }
 
 impl Context {
     pub async fn new() -> sqlx::Result<Self> {
-        Ok(Context {
-            sudo_pool: PgPoolOptions::new()
-                .min_connections(4)
-                .max_connections(12)
-                .connect_timeout(Duration::from_secs(60))
-                .connect_lazy(&SUDO_POSTGRES_CONNECTION_STRING)?,
+        let sudo_pool = PgPoolOptions::new()
+            .min_connections(4)
+            .max_connections(12)
+            .connect_timeout(Duration::from_secs(60))
+            .connect_lazy(&SUDO_POSTGRES_CONNECTION_STRING)?;
+
+        let api_timeout_config =
+            timeout::Api::new().with_call_timeout(TriState::Set(Duration::from_secs(5)));
+        let timeout_config = timeout::Config::new().with_api_timeouts(api_timeout_config);
+        let aws_config = aws_config::from_env()
+            .timeout_config(timeout_config)
+            .load()
+            .await;
+
+        let rds_client = aws_sdk_rds::Client::new(&aws_config);
+
+        Ok(Self {
+            sudo_pool,
+            rds_client,
         })
     }
 }
--- api/src/factory.rs
+++ api/src/factory.rs
@@ -23,12 +23,13 @@ impl ShuttleFactory {
 #[async_trait]
 impl Factory for ShuttleFactory {
     async fn get_sql_connection_string(&mut self) -> Result<String, shuttle_service::Error> {
-        let conn_str = self
-            .database
-            .request()
-            .await
-            .map_err(shuttle_service::error::CustomError::new)?
-            .connection_string("localhost");
+        // let conn_str = self
+        //     .database
+        //     .request()
+        //     .await
+        //     .map_err(shuttle_service::error::CustomError::new)?
+        //     .connection_string("localhost");
+        let conn_str = self.database.aws_rds().await?;
         debug!("giving a sql connection string: {}", conn_str);
         Ok(conn_str)
     }

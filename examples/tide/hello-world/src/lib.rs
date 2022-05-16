251ecce2f7de58d59d6aa112356b2dc8cc37bc37
Mario Idival <marioidival@gmail.com>
2022-05-15 21:24:49 -0300

    test: add e2e tests to tide

--- examples/tide/hello-world/src/lib.rs
+++ examples/tide/hello-world/src/lib.rs
@@ -6,7 +6,7 @@ async fn tide() -> Result<SyncWrapper<tide::Server<()>>, shuttle_service::Error>
     app.with(tide::log::LogMiddleware::new());
 
     app.at("/hello")
-        .get(|_| async { Ok("Hello, world from Tide from Brazil hu3 hu3 BR!") });
+        .get(|_| async { Ok("Hello, world!") });
 
     let sync_wrapper = SyncWrapper::new(app);
     Ok(sync_wrapper)

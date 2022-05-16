c5dcef5ae6b51336f8b9eba4364b44690d706440
chesedo <pieter@chesedo.me>
2022-05-16 09:53:18 +0200

    chore: update nix pin

--- shell.nix
+++ shell.nix
@@ -1,6 +1,7 @@
 let
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
-  nixpkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/e7d63bd0d50df412f5a1d8acfa3caae75522e347.tar.gz") { overlays = [ moz_overlay ]; };
+  # Pin to stable from https://status.nixos.org/
+  nixpkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/3c5ae9be1f18c790ea890ef8decbd0946c0b4c04.tar.gz") { overlays = [ moz_overlay ]; };
 in
   with nixpkgs;
   stdenv.mkDerivation {

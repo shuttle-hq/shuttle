11f0d4650f43d6ef5cc1fc336f814c7769cacc8e
Mario Idival <marioidival@gmail.com>
2022-05-19 05:10:28 -0300

    fix: ci files

--- service/src/lib.rs
+++ service/src/lib.rs
@@ -459,7 +459,8 @@ where
 {
     fn build(&mut self, factory: &mut dyn Factory, logger: logger::Logger) -> Result<(), Error> {
         if let Some(builder) = self.builder.take() {
-            // We want to build any sqlx pools on the same runtime the client code will run on. Without this expect to get errors of no tokio reactor being present.
+            // We want to build any sqlx pools on the same runtime the client code will run on.
+            // Without this expect to get errors of no tokio reactor being present.
             let tower = self.runtime.block_on(async {
                 log::set_boxed_logger(Box::new(logger))
                     .map(|()| log::set_max_level(log::LevelFilter::Info))

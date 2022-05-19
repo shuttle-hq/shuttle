6e20ccf7e30a045ebb5aaa0f4b35b0ef137e7315
chesedo <pieter@chesedo.me>
2022-05-19 07:23:04 +0200

    refactor: clippy suggestions

--- service/src/database.rs
+++ service/src/database.rs
@@ -14,9 +14,9 @@ pub enum AwsRdsEngine {
 impl Display for AwsRdsEngine {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            &Self::MariaDB => write!(f, "mariadb"),
-            &Self::MySql => write!(f, "mysql"),
-            &Self::Postgres => write!(f, "postgres"),
+            Self::MariaDB => write!(f, "mariadb"),
+            Self::MySql => write!(f, "mysql"),
+            Self::Postgres => write!(f, "postgres"),
         }
     }
 }

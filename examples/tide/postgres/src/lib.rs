ff6cbef8bbc021b9451080f722c76b7d4dfe0d5f
chesedo <pieter@chesedo.me>
2022-05-23 15:16:03 +0200

    feat: aws::rds example

---
+++ examples/tide/postgres/src/lib.rs
@@ -0,0 +1,58 @@
+use serde::{Deserialize, Serialize};
+use shuttle_service::error::CustomError;
+use sqlx::{Executor, FromRow, PgPool};
+use tide::{Body, Request};
+
+async fn retrieve(req: Request<MyState>) -> tide::Result {
+    let id = req.param("id")?;
+    let todo: Todo = sqlx::query_as("SELECT * FROM todos WHERE id = $1")
+        .bind(id)
+        .fetch_one(&req.state().pool)
+        .await?;
+
+    Body::from_json(&todo).map(core::convert::Into::into)
+}
+
+async fn add(mut req: Request<MyState>) -> tide::Result {
+    let data: TodoNew = req.body_json().await?;
+    let todo: Todo = sqlx::query_as("INSERT INTO todos(note) VALUES ($1) RETURNING id, note")
+        .bind(&data.note)
+        .fetch_one(&req.state().pool)
+        .await?;
+
+    Body::from_json(&todo).map(core::convert::Into::into)
+}
+
+#[derive(Clone)]
+struct MyState {
+    pool: PgPool,
+}
+
+#[shuttle_service::main]
+async fn tide(
+    #[aws::rds::Postgres] pool: PgPool,
+) -> Result<tide::Server<MyState>, shuttle_service::Error> {
+    pool.execute(include_str!("../schema.sql"))
+        .await
+        .map_err(CustomError::new)?;
+
+    let state = MyState { pool };
+    let mut app = tide::with_state(state);
+
+    app.with(tide::log::LogMiddleware::new());
+    app.at("/todo/:id").get(retrieve);
+    app.at("/todo").post(add);
+
+    Ok(app)
+}
+
+#[derive(Deserialize)]
+struct TodoNew {
+    pub note: String,
+}
+
+#[derive(Serialize, FromRow)]
+struct Todo {
+    pub id: i32,
+    pub note: String,
+}

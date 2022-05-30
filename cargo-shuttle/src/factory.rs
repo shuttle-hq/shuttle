f31b887455b1a7a2665eb91c3b87b8802750a291
chesedo <pieter@chesedo.me>
2022-05-30 15:59:27 +0200

    refactor: make unsupported DBs clearer

--- cargo-shuttle/src/factory.rs
+++ cargo-shuttle/src/factory.rs
@@ -1,3 +1,4 @@
+use anyhow::anyhow;
 use async_trait::async_trait;
 use shuttle_service::Factory;
 
@@ -6,6 +7,8 @@ pub struct LocalFactory {}
 #[async_trait]
 impl Factory for LocalFactory {
     async fn get_sql_connection_string(&mut self) -> Result<String, shuttle_service::Error> {
-        todo!()
+        Err(shuttle_service::Error::Custom(anyhow!(
+            "Database dependencies are not supported for local runs yet. Try deploying instead",
+        )))
     }
 }

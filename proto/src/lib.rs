f84f88b0c227aa0d4499858c340cb1833250c1d6
chesedo <pieter@chesedo.me>
2022-06-03 13:57:12 +0200

    refactor: unneeded 'connection_string()'

--- proto/src/lib.rs
+++ proto/src/lib.rs
@@ -3,15 +3,6 @@ pub mod provisioner {
 
     tonic::include_proto!("provisioner");
 
-    impl DatabaseResponse {
-        pub fn connection_string(&self, ip: &str) -> String {
-            format!(
-                "postgres://{}:{}@{}/{}",
-                self.username, self.password, ip, self.database_name
-            )
-        }
-    }
-
     impl From<DatabaseResponse> for DatabaseReadyInfo {
         fn from(response: DatabaseResponse) -> Self {
             DatabaseReadyInfo {

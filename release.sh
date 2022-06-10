374816d715619de4fea251c77ce04bf66eb45a08
chesedo <pieter@chesedo.me>
2022-06-10 16:25:32 +0200

    feat: release script

---
+++ release.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+#
+# Little script to release a new version.
+# Usage: release.sh x.y.z
+
+set -uo pipefail
+
+function update-cargo-versions()
+{
+    local version=$1
+
+    cargo set-version --workspace $version
+    git commit -am "chore: v$version"
+}
+
+function update-examples-versions()
+{
+    local version=$1
+
+    rg "shuttle-service = \{ version" --files-with-matches -g '!www/*' | xargs sed -i "s/shuttle-service = { version = \"[[:digit:]]*.[[:digit:]]*.[[:digit:]]*\"/shuttle-service = { version = \"$version\"/g"
+    git commit -am "docs: v$version"
+}
+
+function main()
+{
+    version=$1
+
+    echo $version | rg "\d+\.\d+\.\d+" || { echo "first argument must be in the form x.y.z"; exit 1; }
+
+    echo "Will try to update to version $version"
+
+    update-cargo-versions $version
+    update-examples-versions $version
+
+    echo "Success!! You can now merge this branch"
+}
+
+main "${1-*}"

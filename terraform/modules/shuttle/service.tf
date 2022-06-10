a21c45635ccb04a07364c8d5048d80fb586ff945
Pieter <pieter@chesedo.me>
2022-06-10 15:15:15 +0200

    chore: v0.3.3 (#210)
    * chore: v0.3.3
    
    * docs: v0.3.3
    
    * bug: tf images causing kernel panics

--- terraform/modules/shuttle/service.tf
+++ terraform/modules/shuttle/service.tf
@@ -62,7 +62,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-arm64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-arm64-server-20220511"]
   }
 
   filter {

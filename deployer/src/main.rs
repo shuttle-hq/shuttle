5727c9b07ef7d4d221b0270e8db59cb21f2b67c9
WiredSound <maxoblack@yahoo.com>
2022-06-17 00:56:32 +0100

    feat: take secret env variable

--- deployer/src/main.rs
+++ deployer/src/main.rs
@@ -4,9 +4,19 @@ mod tower_service;
 
 use std::net::SocketAddr;
 
+const SECRET_KEY: &str = "GATEWAY_SECRET";
+
 #[tokio::main]
 async fn main() {
     env_logger::init();
+
+    let gateway_secret = std::env::var(SECRET_KEY).unwrap_or_else(|_| {
+        panic!(
+            "No gateway secret specified with environment variable {}",
+            SECRET_KEY
+        )
+    });
+    log::trace!("{SECRET_KEY} = {gateway_secret}");
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8001));
 

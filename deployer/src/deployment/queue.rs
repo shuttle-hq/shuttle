eaf142250b8ac0f7c883e52976a3c662c8777aae
WiredSound <maxoblack@yahoo.com>
2022-06-22 00:24:03 +0100

    feat: add '--strip-components 1' functionality to unzipping code, unit test

--- deployer/src/deployment/queue.rs
+++ deployer/src/deployment/queue.rs
@@ -6,8 +6,9 @@ use crate::persistence::Persistence;
 use shuttle_service::loader::build_crate;
 
 use std::fmt;
-use std::path::PathBuf;
+use std::path::{PathBuf, Path};
 use std::pin::Pin;
+use std::io::Read;
 
 use bytes::{BufMut, Bytes};
 use flate2::read::GzDecoder;
@@ -77,9 +78,7 @@ impl Queued {
 
         let project_path = PathBuf::from(BUILDS_PATH).join(&self.name);
 
-        let tar = GzDecoder::new(vec.as_slice());
-        let mut archive = Archive::new(tar);
-        archive.unpack(&project_path)?;
+        extract_tar_gz_data(vec.as_slice(), &project_path)?;
 
         // Build:
 
@@ -139,5 +138,47 @@ impl fmt::Debug for Queued {
             "Queued {{ name: \"{}\", state: {}, .. }}",
             self.name, self.state
         )
+    }
+}
+
+/// Equivalent to the command: `tar -xzf --strip-components 1`
+fn extract_tar_gz_data(data: impl Read, dest: impl AsRef<Path>) -> Result<()> {
+    let tar = GzDecoder::new(data);
+    let mut archive = Archive::new(tar);
+    archive.set_overwrite(true);
+
+    for entry in archive.entries()? {
+        let mut entry = entry?;
+        let path: PathBuf = entry.path()?.components().skip(1).collect();
+        entry.unpack(dest.as_ref().join(path))?;
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn tar_gz_extraction() {
+        // Binary data for an archive in the following form:
+        //
+        // - temp
+        //   - world.txt
+        //   - subdir
+        //     - hello.txt
+        let test_data = hex::decode("\
+1f8b0800000000000003edd5d10a823014c6f15df7143e41ede8997b1e4d\
+a3c03074528f9f0a41755174b1a2faff6e0653d8818f7d0bf5feb03271d9\
+91f76e5ac53b7bbd5e18d1d4a96a96e6a9b16225f7267191e79a0d7d28ba\
+2431fbe2f4f0bf67dfbf5498f23fb65d532dc329c439630a38cff541fe7a\
+977f6a9d98c4c619e7d69fe75f94ebc5a767c0e7ccf7bf1fca6ad7457b06\
+5eea7f95f1fe8b3aa5ffdfe13aff6ddd346d8467e0a5fef7e3be649928fd\
+ff0e55bda1ff01000000000000000000e0079c01ff12a55500280000").unwrap();
+
+        extract_tar_gz_data(test_data.as_slice(), "/tmp/shuttle-extraction-test").unwrap();
+        assert!(fs::read_to_string("/tmp/shuttle-extraction-test/world.txt").await.unwrap().starts_with("abc"));
+        assert!(fs::read_to_string("/tmp/shuttle-extraction-test/subdir/hello.txt").await.unwrap().starts_with("def"));
     }
 }

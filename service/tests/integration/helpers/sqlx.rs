b0ff5c3ef8f2f55f8fe3d89b5dd2ef0af749a8af
Damien Broka <damienbroka@mailbox.org>
2022-06-28 11:19:57 +0100

    Moving service tests

--- service/tests/integration/helpers/sqlx.rs
+++ service/tests/integration/helpers/sqlx.rs
@@ -1,13 +1,10 @@
 use std::future::Future;
-use std::path::PathBuf;
 use std::process::Command;
 use std::thread::sleep;
 use std::time::Duration;
 
 use portpicker::pick_unused_port;
 use sqlx::Connection;
-
-use shuttle_service::loader::{Loader, LoaderError};
 
 pub struct PostgresInstance {
     port: u16,
@@ -128,28 +125,4 @@ impl Drop for PostgresInstance {
             .wait()
             .expect("postgres container remove failed");
     }
-}
-
-pub fn build_so_create_loader(resources: &str, crate_name: &str) -> Result<Loader, LoaderError> {
-    let crate_dir: PathBuf = [resources, crate_name].iter().collect();
-
-    Command::new("cargo")
-        .args(["build", "--release"])
-        .current_dir(&crate_dir)
-        .spawn()
-        .unwrap()
-        .wait()
-        .unwrap();
-
-    let dashes_replaced = crate_name.replace('-', "_");
-
-    let lib_name = if cfg!(target_os = "windows") {
-        format!("{}.dll", dashes_replaced)
-    } else {
-        format!("lib{}.so", dashes_replaced)
-    };
-
-    let so_path = crate_dir.join("target/release").join(lib_name);
-
-    Loader::from_so_file(&so_path)
 }

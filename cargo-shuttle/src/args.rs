4c0d0cb357906bdc2cc91ca348e903d8690cc99b
Xavientois <joshua.rampersad@hotmail.com>
2022-07-11 21:11:32 -0400

    deps(clap): migrate uses of structopt to clap in cargo-shuttle

--- cargo-shuttle/src/args.rs
+++ cargo-shuttle/src/args.rs
@@ -1,39 +1,40 @@
 use std::{
-    ffi::{OsStr, OsString},
+    ffi::OsStr,
     fs::{canonicalize, create_dir_all},
+    io::{self, ErrorKind},
     path::PathBuf,
 };
 
+use clap::Parser;
 use shuttle_common::project::ProjectName;
-use structopt::StructOpt;
 
-#[derive(StructOpt)]
-#[structopt(
+#[derive(Parser)]
+#[clap(
     // Cargo passes in the subcommand name to the invoked executable. Use a
     // hidden, optional positional argument to deal with it.
-    arg(structopt::clap::Arg::with_name("dummy")
+    arg(clap::Arg::with_name("dummy")
         .possible_value("shuttle")
         .required(false)
         .hidden(true))
 )]
 pub struct Args {
-    #[structopt(
+    #[clap(
         long,
-        about = "allows targeting a custom deloyed instance for this command only",
+        help = "allows targeting a custom deloyed instance for this command only",
         env = "SHUTTLE_API"
     )]
     /// Run this command against the api at the supplied url
     pub api_url: Option<String>,
-    #[structopt(flatten)]
+    #[clap(flatten)]
     pub project_args: ProjectArgs,
-    #[structopt(subcommand)]
+    #[clap(subcommand)]
     pub cmd: Command,
 }
 
 // Common args for subcommands that deal with projects.
-#[derive(StructOpt, Debug)]
+#[derive(Parser, Debug)]
 pub struct ProjectArgs {
-    #[structopt(
+    #[clap(
         global = true,
         long,
         parse(try_from_os_str = parse_path),
@@ -41,61 +42,61 @@ pub struct ProjectArgs {
     )]
     /// Specify the working directory
     pub working_directory: PathBuf,
-    #[structopt(global = true, long)]
+    #[clap(global = true, long)]
     /// Specify the name of the project (overrides crate name)
     pub name: Option<ProjectName>,
 }
 
-#[derive(StructOpt)]
+#[derive(Parser)]
 pub enum Command {
-    #[structopt(about = "deploy a shuttle project")]
+    #[clap(help = "deploy a shuttle project")]
     Deploy(DeployArgs),
-    #[structopt(about = "create a new shuttle project")]
+    #[clap(help = "create a new shuttle project")]
     Init(InitArgs),
-    #[structopt(about = "view the status of a shuttle project")]
+    #[clap(help = "view the status of a shuttle project")]
     Status,
-    #[structopt(about = "view the logs of a shuttle project")]
+    #[clap(help = "view the logs of a shuttle project")]
     Logs,
-    #[structopt(about = "delete the latest deployment for a shuttle project")]
+    #[clap(help = "delete the latest deployment for a shuttle project")]
     Delete,
-    #[structopt(about = "create user credentials for the shuttle platform")]
+    #[clap(help = "create user credentials for the shuttle platform")]
     Auth(AuthArgs),
-    #[structopt(about = "login to the shuttle platform")]
+    #[clap(help = "login to the shuttle platform")]
     Login(LoginArgs),
-    #[structopt(about = "run a shuttle project locally")]
+    #[clap(help = "run a shuttle project locally")]
     Run(RunArgs),
 }
 
-#[derive(StructOpt)]
+#[derive(Parser)]
 pub struct LoginArgs {
-    #[structopt(long, about = "api key for the shuttle platform")]
+    #[clap(long, help = "api key for the shuttle platform")]
     pub api_key: Option<String>,
 }
 
-#[derive(StructOpt)]
+#[derive(Parser)]
 pub struct AuthArgs {
-    #[structopt(about = "the desired username for the shuttle platform")]
+    #[clap(help = "the desired username for the shuttle platform")]
     pub username: String,
 }
 
-#[derive(StructOpt)]
+#[derive(Parser)]
 pub struct DeployArgs {
-    #[structopt(long, about = "allow dirty working directories to be packaged")]
+    #[clap(long, help = "allow dirty working directories to be packaged")]
     pub allow_dirty: bool,
-    #[structopt(long, about = "allows pre-deploy tests to be skipped")]
+    #[clap(long, help = "allows pre-deploy tests to be skipped")]
     pub no_test: bool,
 }
 
-#[derive(StructOpt, Debug)]
+#[derive(Parser, Debug)]
 pub struct RunArgs {
-    #[structopt(long, about = "port to start service on", default_value = "8000")]
+    #[clap(long, help = "port to start service on", default_value = "8000")]
     pub port: u16,
 }
 
-#[derive(StructOpt)]
+#[derive(Parser)]
 pub struct InitArgs {
-    #[structopt(
-        about = "the path to initialize a new shuttle project",
+    #[clap(
+        help = "the path to initialize a new shuttle project",
         parse(try_from_os_str = parse_init_path),
         default_value = ".",
     )]
@@ -103,14 +104,19 @@ pub struct InitArgs {
 }
 
 // Helper function to parse and return the absolute path
-fn parse_path(path: &OsStr) -> Result<PathBuf, OsString> {
-    canonicalize(path).map_err(|e| format!("could not turn {path:?} into a real path: {e}").into())
+fn parse_path(path: &OsStr) -> Result<PathBuf, io::Error> {
+    canonicalize(path).map_err(|e| {
+        io::Error::new(
+            ErrorKind::InvalidInput,
+            format!("could not turn {path:?} into a real path: {e}"),
+        )
+    })
 }
 
 // Helper function to parse, create if not exists, and return the absolute path
-fn parse_init_path(path: &OsStr) -> Result<PathBuf, OsString> {
+fn parse_init_path(path: &OsStr) -> Result<PathBuf, io::Error> {
     // Create the directory if does not exist
-    create_dir_all(path).expect("could not find or create a directory with the given path");
+    create_dir_all(path)?;
 
     parse_path(path)
 }

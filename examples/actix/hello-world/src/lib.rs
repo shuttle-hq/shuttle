bc8f0d5ca398eaba91890140496c92697b33fa9e
Thibault <melkir13@gmail.com>
2022-07-21 00:03:41 +0200

    feat: add actix example

---
+++ examples/actix/hello-world/src/lib.rs
@@ -0,0 +1,15 @@
+#[macro_use]
+extern crate rocket;
+use actix_web::{get, web, App, Responder};
+
+#[get("/")]
+async fn hello_world(name: web::Path<String>) -> impl Responder {
+    format!("Hello, world!")
+}
+
+#[shuttle_service::main]
+async fn actix() -> shuttle_service::ShuttleActix {
+    let actix = App::new().service(hello_world);
+
+    Ok(actix)
+}

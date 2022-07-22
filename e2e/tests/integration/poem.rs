1115a25113f03d27bd36601412e734f27de91fc7
oddgrd <29732646+oddgrd@users.noreply.github.com>
2022-07-23 00:56:01 +0200

    feat: add postgres example

--- e2e/tests/integration/poem.rs
+++ e2e/tests/integration/poem.rs
@@ -17,3 +17,41 @@ fn hello_world_poem() {
 
     assert_eq!(request_text, "Hello, world!");
 }
+
+#[test]
+fn postgres_poem() {
+    let client = helpers::Services::new_docker("postgres", Color::Blue);
+    client.deploy("poem/postgres");
+
+    let add_response = client
+        .post("todo")
+        .body("{\"note\": \"To the stars\"}")
+        .header("Host", "postgres-poem-app.localhost.local")
+        .header("content-type", "application/json")
+        .send()
+        .unwrap()
+        .text()
+        .unwrap();
+
+    assert_eq!(add_response, "{\"id\":1,\"note\":\"To the stars\"}");
+
+    let fetch_response: String = client
+        .get("todo/1")
+        .header("Host", "postgres-poem-app.localhost.local")
+        .send()
+        .unwrap()
+        .text()
+        .unwrap();
+
+    assert_eq!(fetch_response, "{\"id\":1,\"note\":\"To the stars\"}");
+
+    let secret_response: String = client
+        .get("secret")
+        .header("Host", "postgres-poem-app.localhost.local")
+        .send()
+        .unwrap()
+        .text()
+        .unwrap();
+
+    assert_eq!(secret_response, "the contents of my API key");
+}

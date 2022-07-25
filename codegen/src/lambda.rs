5d2b5d49be895b02127ae0c4f3756e3debae33c2
Xavientois <joshua.rampersad@hotmail.com>
2022-07-25 12:13:51 -0400

    chore: remove lambda for refactor

--- codegen/src/lambda.rs
+++
@@ -1,5 +0,0 @@
-use proc_macro::TokenStream;
-
-pub(crate) fn r#impl(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    todo!()
-}
--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -1,4 +1,3 @@
-mod lambda;
 mod main;
 
 use proc_macro::TokenStream;
@@ -8,10 +7,4 @@ use proc_macro_error::proc_macro_error;
 #[proc_macro_attribute]
 pub fn main(attr: TokenStream, item: TokenStream) -> TokenStream {
     main::r#impl(attr, item)
-}
-
-#[proc_macro_error]
-#[proc_macro_attribute]
-pub fn lambda(attr: TokenStream, item: TokenStream) -> TokenStream {
-    lambda::r#impl(attr, item)
 }

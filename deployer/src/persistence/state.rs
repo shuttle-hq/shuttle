064c1417efe2de76156824f56adc1d715e5cf722
chesedo <pieter@chesedo.me>
2022-08-24 16:44:24 +0200

    refactor: restore state comments

--- deployer/src/persistence/state.rs
+++ deployer/src/persistence/state.rs
@@ -3,13 +3,28 @@ use strum::{Display, EnumString};
 /// States a deployment can be in
 #[derive(sqlx::Type, Debug, Display, Clone, Copy, EnumString, PartialEq, Eq)]
 pub enum State {
+    /// Deployment is queued to be build
     Queued,
+
+    /// Deployment is building, but is not done yet
     Building,
+
+    /// Deployment is built, but has not been started yet
     Built,
+
+    /// Deployment is running - ie. its thread is active
     Running,
+
+    /// Deployment was running, but stopped running all by itself. This is expected for things like background workers
     Completed,
+
+    /// Deployment was running, but has been stopped by the user.
     Stopped,
+
+    /// Something in the deployment process failed
     Crashed,
+
+    /// We never expect this state and entering this state should be considered a bug
     Unknown,
 }
 

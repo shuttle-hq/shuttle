ff70d3cadac7be6ac16d8798e6ade10bec6f522b
chesedo <pieter@chesedo.me>
2022-10-07 18:27:39 +0200

    refactor: switch deployer to sqlx migrator

--- deployer/src/persistence/mod.rs
+++ deployer/src/persistence/mod.rs
@@ -19,7 +19,7 @@ use std::str::FromStr;
 use chrono::Utc;
 use serde_json::json;
 use shuttle_common::STATE_MESSAGE;
-use sqlx::migrate::MigrateDatabase;
+use sqlx::migrate::{MigrateDatabase, Migrator};
 use sqlx::sqlite::{Sqlite, SqlitePool};
 use tokio::sync::broadcast::{self, Receiver, Sender};
 use tokio::task::JoinHandle;
@@ -36,6 +36,8 @@ pub use self::secret::{SecretGetter, SecretRecorder};
 pub use self::service::Service;
 pub use self::state::State;
 pub use self::user::User;
+
+pub static MIGRATIONS: Migrator = sqlx::migrate!("./migrations");
 
 #[derive(Clone)]
 pub struct Persistence {
@@ -70,51 +72,7 @@ impl Persistence {
     }
 
     async fn from_pool(pool: SqlitePool) -> (Self, JoinHandle<()>) {
-        sqlx::query("
-            CREATE TABLE IF NOT EXISTS services (
-                id TEXT PRIMARY KEY, -- Identifier of the service.
-                name TEXT UNIQUE     -- Name of the service.
-            );
-
-            CREATE TABLE IF NOT EXISTS deployments (
-                id TEXT PRIMARY KEY, -- Identifier of the deployment.
-                service_id TEXT,     -- Identifier of the service this deployment belongs to.
-                state TEXT,          -- Enum indicating the current state of the deployment.
-                last_update INTEGER, -- Unix epoch of the last status update
-                address TEXT,        -- Address a running deployment is active on
-                FOREIGN KEY(service_id) REFERENCES services(id)
-            );
-
-            CREATE TABLE IF NOT EXISTS logs (
-                id TEXT,           -- The deployment that this log line pertains to.
-                timestamp INTEGER, -- Unix epoch timestamp.
-                state TEXT,        -- The state of the deployment at the time at which the log text was produced.
-                level TEXT,        -- The log level
-                file TEXT,         -- The file log took place in
-                line INTEGER,      -- The line log took place on
-                target TEXT,       -- The module log took place in
-                fields TEXT,       -- Log fields object.
-                PRIMARY KEY (id, timestamp),
-                FOREIGN KEY(id) REFERENCES deployments(id)
-            );
-
-            CREATE TABLE IF NOT EXISTS resources (
-                service_id TEXT,   -- Identifier of the service this resource belongs to.
-                type TEXT,         -- Type of resource this is.
-                data TEXT,         -- Data about this resource.
-                PRIMARY KEY (service_id, type),
-                FOREIGN KEY(service_id) REFERENCES services(id)
-            );
-
-            CREATE TABLE IF NOT EXISTS secrets (
-                service_id TEXT,      -- Identifier of the service this secret belongs to.
-                key TEXT,             -- Key / name of this secret.
-                value TEXT,           -- The actual secret.
-                last_update INTEGER,  -- Unix epoch of the last secret update
-                PRIMARY KEY (service_id, key),
-                FOREIGN KEY(service_id) REFERENCES services(id)
-            );
-        ").execute(&pool).await.unwrap();
+        MIGRATIONS.run(&pool).await.unwrap();
 
         let (log_send, log_recv): (crossbeam_channel::Sender<deploy_layer::Log>, _) =
             crossbeam_channel::bounded(0);

fd728dcf28bde2f9b02df4022fd07194412fb856
chesedo <pieter@chesedo.me>
2022-10-10 15:59:42 +0200

    refactor: log more in deployer

--- gateway/src/project.rs
+++ gateway/src/project.rs
@@ -363,7 +363,7 @@ impl ProjectCreating {
                 "/opt/shuttle/deployer.sqlite",
             ],
             "Env": [
-                "RUST_LOG=shuttle_deployer,shuttle_service,sqlx",
+                "RUST_LOG=debug",
             ],
             "Healthcheck": {
                 "Interval": 5_000_000_000i64,

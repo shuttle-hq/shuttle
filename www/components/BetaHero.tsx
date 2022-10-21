31e24a1f72659d9912ef1f15b185b72a4d089d07
oddgrd <29732646+oddgrd@users.noreply.github.com>
2022-10-21 14:39:24 +0200

    feat: remove gif, add text

--- www/components/BetaHero.tsx
+++ www/components/BetaHero.tsx
@@ -6,17 +6,26 @@ const BetaHero = () => {
   return (
     <div className="-mt-8 flex min-h-screen w-full flex-col justify-center dark:bg-dark-700">
       <div className="mx-auto py-5 xl:px-12">
-        <div className="p-6 sm:py-8">
-          <div className="m-auto flex max-w-3xl flex-col gap-8 text-center sm:gap-11">
+        <div className="p-6 sm:py-5">
+          <div className="m-auto flex max-w-3xl flex-col gap-4 text-center sm:gap-10">
             <div className="relative m-auto flex">
               <img
                 className="h-20 w-auto"
                 src={`${basePath}/images/logo.png`}
                 alt="Shuttle"
               />
-              <span className="absolute bottom-[-26px] right-[-5px] scale-[.8] rounded bg-brand-orange1 px-[10px] py-[2px] text-base font-bold text-white">
+              <span className="absolute bottom-[-26px] right-[-5px] scale-[.8] rounded bg-brand-orange1 px-[10px] py-[2px] text-base font-bold text-slate-100 dark:text-dark-700">
                 BETA
               </span>
+            </div>
+            <div className="mt-8 mb-2 max-w-xl">
+              <h2 className="text-3xl font-extrabold tracking-tight dark:text-gray-200 sm:text-3xl">
+                Fastest backend development experience ever
+              </h2>
+              <p className="mt-6 text-lg text-slate-500 dark:text-gray-300 sm:mt-6">
+                A next-generation backend framework with the fastest build, test
+                and deployment times ever.
+              </p>
             </div>
             <SignupForm />
           </div>
--- www/components/SignupForm.tsx
+++ www/components/SignupForm.tsx
@@ -44,42 +44,13 @@ export default function SignupForm() {
   }
   return (
     <>
-      <div
-        id="code-snippets"
-        className="mx-auto max-w-6xl px-4 pt-5 pb-20 sm:px-6 lg:px-8 lg:pt-4 lg:pb-4"
-      >
-        <div className="grid w-full grid-cols-1 gap-4 lg:grid-cols-12">
-          <div className="mb-4 max-w-lg lg:col-span-5">
-            <p className="mt-3 text-left text-lg text-slate-500 dark:text-gray-300 sm:mt-4">
-              Lorem ipsum dolor.
-            </p>
-            <p className="mt-3 text-left text-lg text-slate-500 dark:text-gray-300 sm:mt-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a
-              placerat nisi. Mauris quis congue purus. Nullam orci est, laoreet
-              venenatis mi quis, consectetur semper ligula. Sed id augue
-              bibendum, finibus felis eget, ultrices eros. Donec feugiat sed
-              lacus id faucibus. Cras vitae efficitur lectus.
-            </p>
-          </div>
-          <div className="lg:col-span-7">
-            <div className="relative top-1/2 mx-auto max-w-xs -translate-y-1/2">
-              <Image
-                src={"/images/fuse.gif"}
-                layout="responsive"
-                height="100%"
-                width="100%"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
       <form onSubmit={handleSubmit} className="align-center flex flex-col">
         <input
           id="email"
           type="email"
           name="email"
           placeholder={`hello@shuttle.rs`}
-          className="text-m mt-3 block w-full max-w-sm self-center rounded rounded border border-gray-300 bg-slate-300 p-4 text-slate-700 text-gray-900 hover:border-brand-orange1 focus:border-brand-orange1 focus:ring-brand-orange1 dark:border-gray-600 dark:bg-gray-500 dark:text-white dark:placeholder-gray-400 dark:focus:border-brand-orange1 dark:focus:ring-brand-orange1"
+          className="text-m mt-3 block w-full max-w-sm self-center rounded border border-gray-300 bg-slate-300 p-4 text-slate-700  hover:border-brand-orange1 focus:border-brand-orange1 focus:ring-brand-orange1 dark:border-gray-600 dark:bg-gray-500 dark:text-white dark:placeholder-gray-400 dark:focus:border-brand-orange1 dark:focus:ring-brand-orange1"
         />
         <ValidationError prefix="Email" field="email" errors={state.errors} />
         <button

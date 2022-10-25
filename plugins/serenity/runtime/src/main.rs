cfa358ce8e9c78e3545fba971172d310983f1b54
Damien Broka <git@damien.sh>
2022-10-25 12:02:40 +0100

    feat: shuttle-serenity initial commit poc

---
+++ plugins/serenity/runtime/src/main.rs
@@ -0,0 +1,19 @@
+use std::env;
+use std::io;
+
+use serenity::prelude::*;
+
+use shuttle_runtime::Bot;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+
+    let token = env::var("DISCORD_TOKEN").unwrap();
+    let src = env::var("BOT_SRC").unwrap();
+
+    let mut client = Bot::new(src).into_client(token.as_str(), intents).await;
+    client.start().await.unwrap();
+
+    Ok(())
+}

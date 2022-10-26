44bb1378f3fc998100cf24dc3f4db2a5914bfb59
chesedo <pieter@chesedo.me>
2022-10-26 18:30:00 +0200

    feat: impl Runtime server for next

--- runtimes/next/src/main.rs
+++ runtimes/next/src/main.rs
@@ -1,20 +1,33 @@
+use std::net::{Ipv4Addr, SocketAddr};
+
 use clap::Parser;
-use serenity::prelude::*;
-use shuttle_next::{args::Args, Bot};
-use std::env;
-use std::io;
+use shuttle_next::{args::Args, Next};
+use shuttle_runtime_proto::runtime::runtime_server::RuntimeServer;
+use tonic::transport::Server;
+use tracing::trace;
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
 #[tokio::main]
-async fn main() -> io::Result<()> {
-    let _args = Args::parse();
+async fn main() {
+    let args = Args::parse();
 
-    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+    let fmt_layer = fmt::layer();
+    let filter_layer = EnvFilter::try_from_default_env()
+        .or_else(|_| EnvFilter::try_new("info"))
+        .unwrap();
 
-    let token = env::var("DISCORD_TOKEN").unwrap();
-    let src = env::var("BOT_SRC").unwrap();
+    tracing_subscriber::registry()
+        .with(filter_layer)
+        .with(fmt_layer)
+        .init();
 
-    let mut client = Bot::new(src).into_client(token.as_str(), intents).await;
-    client.start().await.unwrap();
+    trace!(args = ?args, "parsed args");
 
-    Ok(())
+    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8000);
+    let next = Next::new();
+    Server::builder()
+        .add_service(RuntimeServer::new(next))
+        .serve(addr)
+        .await
+        .unwrap();
 }

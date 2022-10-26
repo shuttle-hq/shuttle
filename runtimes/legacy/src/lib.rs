0e2ed68f0af268dbe009d3895dd3af5ad89f9228
chesedo <pieter@chesedo.me>
2022-10-26 16:47:23 +0200

    refactor: legacy move main to lib

--- runtimes/legacy/src/lib.rs
+++ runtimes/legacy/src/lib.rs
@@ -1,2 +1,125 @@
+use std::{
+    collections::BTreeMap,
+    net::{Ipv4Addr, SocketAddr},
+    path::PathBuf,
+    str::FromStr,
+};
+
+use anyhow::anyhow;
+use async_trait::async_trait;
+use shuttle_common::{database, LogItem};
+use shuttle_service::{
+    loader::{LoadedService, Loader},
+    Factory, Logger, ServiceName,
+};
+use tokio::sync::mpsc::{self, UnboundedReceiver};
+use tracing::{info, instrument, trace};
+
 pub mod args;
 pub mod error;
+
+pub struct Legacy {
+    so_path: Option<PathBuf>,
+    port: Option<u16>,
+}
+
+impl Legacy {
+    pub fn new() -> Self {
+        Self {
+            so_path: None,
+            port: None,
+        }
+    }
+
+    pub async fn load(&mut self, so_path: PathBuf) -> Result<bool, error::Error> {
+        self.so_path = Some(so_path);
+
+        Ok(true)
+    }
+
+    pub async fn start(&mut self) -> Result<(), error::Error> {
+        let port = 8000;
+        let address = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), port);
+        let mut factory = DummyFactory::new();
+        let (logger, _rx) = get_logger();
+        let so_path = self
+            .so_path
+            .as_ref()
+            .ok_or_else(|| -> error::Error {
+                error::Error::Start(anyhow!("trying to start a service that was not loaded"))
+            })?
+            .clone();
+
+        let service = load_service(address, so_path, &mut factory, logger)
+            .await
+            .unwrap();
+
+        self.port = Some(port);
+
+        _ = tokio::spawn(run(service, address)).await;
+
+        Ok(())
+    }
+}
+
+#[instrument(skip(service))]
+async fn run(service: LoadedService, addr: SocketAddr) {
+    let (handle, library) = service;
+
+    info!("starting deployment on {}", addr);
+    handle.await.unwrap().unwrap();
+
+    tokio::spawn(async move {
+        trace!("closing .so file");
+        library.close().unwrap();
+    });
+}
+
+#[instrument(skip(addr, so_path, factory, logger))]
+async fn load_service(
+    addr: SocketAddr,
+    so_path: PathBuf,
+    factory: &mut dyn Factory,
+    logger: Logger,
+) -> error::Result<LoadedService> {
+    let loader = Loader::from_so_file(so_path)?;
+
+    Ok(loader.load(factory, addr, logger).await?)
+}
+
+struct DummyFactory {
+    service_name: ServiceName,
+}
+
+impl DummyFactory {
+    fn new() -> Self {
+        Self {
+            service_name: ServiceName::from_str("legacy").unwrap(),
+        }
+    }
+}
+
+#[async_trait]
+impl Factory for DummyFactory {
+    fn get_service_name(&self) -> ServiceName {
+        self.service_name.clone()
+    }
+
+    async fn get_db_connection_string(
+        &mut self,
+        _: database::Type,
+    ) -> Result<String, shuttle_service::Error> {
+        todo!()
+    }
+
+    async fn get_secrets(&mut self) -> Result<BTreeMap<String, String>, shuttle_service::Error> {
+        todo!()
+    }
+}
+
+fn get_logger() -> (Logger, UnboundedReceiver<LogItem>) {
+    let (tx, rx) = mpsc::unbounded_channel();
+    let logger = Logger::new(tx, Default::default());
+
+    (logger, rx)
+}
--- runtimes/legacy/src/main.rs
+++ runtimes/legacy/src/main.rs
@@ -1,15 +1,8 @@
-use std::{collections::BTreeMap, net::SocketAddr, path::PathBuf, str::FromStr};
+use std::path::PathBuf;
 
-use async_trait::async_trait;
 use clap::Parser;
-use shuttle_common::{database, LogItem};
-use shuttle_legacy::args::Args;
-use shuttle_service::{
-    loader::{LoadedService, Loader},
-    Factory, Logger, ServiceName,
-};
-use tokio::sync::mpsc::{self, UnboundedReceiver};
-use tracing::{info, instrument, trace};
+use shuttle_legacy::{args::Args, Legacy};
+use tracing::trace;
 use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
 #[tokio::main(flavor = "multi_thread")]
@@ -28,76 +21,8 @@ async fn main() {
 
     trace!(args = ?args, "parsed args");
 
-    let address: SocketAddr = "127.0.0.1:8000".parse().unwrap();
-    let mut factory = DummyFactory::new();
-    let (logger, _rx) = get_logger();
     let so_path = PathBuf::from(args.file_path.as_str());
-
-    let service = load_service(address, so_path, &mut factory, logger)
-        .await
-        .unwrap();
-
-    _ = tokio::spawn(run(service, address)).await;
-}
-
-#[instrument(skip(service))]
-async fn run(service: LoadedService, addr: SocketAddr) {
-    let (handle, library) = service;
-
-    info!("starting deployment on {}", addr);
-    handle.await.unwrap().unwrap();
-
-    tokio::spawn(async move {
-        trace!("closing .so file");
-        library.close().unwrap();
-    });
-}
-
-#[instrument(skip(addr, so_path, factory, logger))]
-async fn load_service(
-    addr: SocketAddr,
-    so_path: PathBuf,
-    factory: &mut dyn Factory,
-    logger: Logger,
-) -> shuttle_legacy::error::Result<LoadedService> {
-    let loader = Loader::from_so_file(so_path)?;
-
-    Ok(loader.load(factory, addr, logger).await?)
-}
-
-struct DummyFactory {
-    service_name: ServiceName,
-}
-
-impl DummyFactory {
-    fn new() -> Self {
-        Self {
-            service_name: ServiceName::from_str("next").unwrap(),
-        }
-    }
-}
-
-#[async_trait]
-impl Factory for DummyFactory {
-    fn get_service_name(&self) -> ServiceName {
-        self.service_name.clone()
-    }
-
-    async fn get_db_connection_string(
-        &mut self,
-        _: database::Type,
-    ) -> Result<String, shuttle_service::Error> {
-        todo!()
-    }
-
-    async fn get_secrets(&mut self) -> Result<BTreeMap<String, String>, shuttle_service::Error> {
-        todo!()
-    }
-}
-
-fn get_logger() -> (Logger, UnboundedReceiver<LogItem>) {
-    let (tx, rx) = mpsc::unbounded_channel();
-    let logger = Logger::new(tx, Default::default());
-
-    (logger, rx)
+    let mut legacy = Legacy::new();
+    legacy.load(so_path).await.unwrap();
+    legacy.start().await.unwrap();
 }

35feec4d23f518d539e32365f4b287589aeb8d64
chesedo <pieter@chesedo.me>
2022-10-27 08:11:31 +0200

    feat: add --legacy flag

--- runtimes/legacy/src/args.rs
+++ runtimes/legacy/src/args.rs
@@ -6,4 +6,8 @@ pub struct Args {
     /// Address to reach provisioner at
     #[clap(long, default_value = "localhost:5000")]
     pub provisioner_address: Endpoint,
+
+    /// Is this runtime for a legacy service
+    #[clap(long)]
+    pub legacy: bool,
 }

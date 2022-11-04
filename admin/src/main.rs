caf6ea1c86f6a6c33e081f4e27068507bafd6951
chesedo <pieter@chesedo.me>
2022-11-04 12:22:05 +0200

    feat: read api_key

--- admin/src/main.rs
+++ admin/src/main.rs
@@ -1,8 +1,10 @@
 use clap::Parser;
-use shuttle_admin::args::Args;
+use shuttle_admin::{args::Args, config::get_api_key};
 
 fn main() {
     let args = Args::parse();
+    let api_key = get_api_key();
 
     println!("{args:?}");
+    println!("{api_key}");
 }

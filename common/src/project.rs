69c8b7568c2f859cc516cb3fcfe1a6e823618935
oddgrd <29732646+oddgrd@users.noreply.github.com>
2022-11-15 10:44:50 +0100

    feat: restrict underscores, max length

--- common/src/project.rs
+++ common/src/project.rs
@@ -6,15 +6,6 @@ use std::error::Error;
 use std::fmt::{Display, Formatter};
 use std::str::FromStr;
 
-/// Project names should conform to valid Host segments (or labels)
-/// as per [IETF RFC 1123](https://datatracker.ietf.org/doc/html/rfc1123).
-/// Initially we'll implement a strict subset of the IETF RFC 1123, concretely:
-/// - It does not start or end with `-` or `_`.
-/// - It does not contain any characters outside of the alphanumeric range, except for `-` or '_'.
-/// - It is not empty.
-/// - It does not contain profanity.
-/// - It is not a reserved word.
-///
 use rustrict::{Censor, Type};
 
 #[derive(Clone, Serialize, Debug, Eq, PartialEq)]
@@ -37,34 +28,46 @@ impl std::fmt::Display for ProjectName {
 }
 
 impl ProjectName {
-    pub fn is_valid(hostname: &str) -> bool {
+    /// Project names should conform to valid Host segments (or labels)
+    /// as per [IETF RFC 1123](https://datatracker.ietf.org/doc/html/rfc1123).
+    /// We implement a strict subset of the IETF RFC 1123, concretely:
+    /// - It does not start or end with `-`.
+    /// - It does not contain any characters outside of the alphanumeric range, except for `-`.
+    /// - It is between 1 and 63 characters in length.
+    ///
+    /// Additionaly, while host segments are technically case-insensitive, the filesystem isn't,
+    /// so we restrict project names to be lower case. We also restrict the use of profanity,
+    /// as well as a list of reserved words.
+    pub fn is_valid(label: &str) -> bool {
         fn is_valid_char(byte: u8) -> bool {
-            matches!(byte, b'a'..=b'z' | b'0'..=b'9' | b'-' | b'_')
+            matches!(byte, b'a'..=b'z' | b'0'..=b'9' | b'-')
         }
 
-        fn is_profanity_free(hostname: &str) -> bool {
-            let (_censored, analysis) = Censor::from_str(hostname).censor_and_analyze();
+        fn is_profanity_free(label: &str) -> bool {
+            let (_censored, analysis) = Censor::from_str(label).censor_and_analyze();
             !analysis.is(Type::MODERATE_OR_HIGHER)
         }
 
-        fn is_reserved(hostname: &str) -> bool {
+        fn is_reserved(label: &str) -> bool {
             static INSTANCE: OnceCell<HashSet<&str>> = OnceCell::new();
-            INSTANCE.get_or_init(|| HashSet::from(["shuttle.rs"]));
+            INSTANCE.get_or_init(|| HashSet::from(["shuttleapp", "shuttle"]));
 
             INSTANCE
                 .get()
                 .expect("Reserved words not set")
-                .contains(hostname)
+                .contains(label)
         }
 
-        let separators = ['-', '_'];
+        // each label in a hostname can be between 1 and 63 chars
+        let is_invalid_length = label.len() > 63;
 
-        !(hostname.bytes().any(|byte| !is_valid_char(byte))
-            || is_reserved(hostname)
-            || !is_profanity_free(hostname)
-            || hostname.ends_with(separators)
-            || hostname.starts_with(separators)
-            || hostname.is_empty())
+        !(label.bytes().any(|byte| !is_valid_char(byte))
+            || is_reserved(label)
+            || !is_profanity_free(label)
+            || label.ends_with('-')
+            || label.starts_with('-')
+            || label.is_empty()
+            || is_invalid_length)
     }
 
     pub fn as_str(&self) -> &str {
@@ -104,9 +107,10 @@ impl Display for ProjectNameError {
 1. start and end with alphanumeric characters.
 2. only contain characters inside of the alphanumeric range, except for `-`, or `_`.
 3. be lowercase.
-4. not be empty.
+4. be between 1 and 63 characters in length.
 5. not contain profanity.
-6. not be a reserved word."#,
+6. not be a reserved word.
+"#,
                 name
             ),
         }
@@ -122,28 +126,29 @@ pub mod tests {
     use super::*;
 
     #[test]
-    fn valid_hostnames() {
-        for hostname in [
+    fn valid_labels() {
+        for label in [
             "50-name",
             "235235",
             "123",
-            "s________e",
-            "snake_case",
+            "s------e",
             "kebab-case",
             "lowercase",
             "myassets",
             "dachterrasse",
+            "thisoneislongbutvalidthisoneislongbutvalidthisoneislongbutvalid",
         ] {
-            let project_name = ProjectName::from_str(hostname);
-            assert!(project_name.is_ok(), "{:?} was err", hostname);
+            let project_name = ProjectName::from_str(label);
+            assert!(project_name.is_ok(), "{:?} was err", label);
         }
     }
 
     #[test]
-    fn invalid_hostnames() {
-        for hostname in [
-            "inVaLiD-HoStNaMe",
+    fn invalid_labels() {
+        for label in [
+            "inVaLiD-HoStNaMeinVaLiD",
             "CamelCase",
+            "snake_case",
             "pascalCase",
             "inVaLid",
             "-invalid-name",
@@ -159,9 +164,11 @@ pub mod tests {
             "invalid__",
             "test-condom-condom",
             "shuttle.rs",
+            "thisoneislongandinvalidthisoneislongandinvalidthisoneislongandinvalid",
+            "shuttleapp",
         ] {
-            let project_name = ProjectName::from_str(hostname);
-            assert!(project_name.is_err(), "{:?} was ok", hostname);
+            let project_name = ProjectName::from_str(label);
+            assert!(project_name.is_err(), "{:?} was ok", label);
         }
     }
 }

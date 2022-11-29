7a4bf4903d09e7bdac27b652c362542238567be3
maksim <maksim@blockchaincmdt.io>
2022-11-29 10:09:29 +0400

    Support actix-web: formatted boilerplate code for actix-web hello world example

--- cargo-shuttle/src/init.rs
+++ cargo-shuttle/src/init.rs
@@ -50,17 +50,18 @@ impl ShuttleInit for ShuttleInitActixWeb {
     fn get_boilerplate_code_for_framework(&self) -> &'static str {
         indoc! {r#"
         use actix_web::web::{resource, ServiceConfig};
-        use shuttle_service::{ShuttleActixWeb};
+        use shuttle_service::ShuttleActixWeb;
 
         async fn hello_world() -> &'static str {
             "Hello World!"
         }
 
         #[shuttle_service::main]
-        async fn actix_web() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Sync + Send + Copy + Clone + 'static> {
+        async fn actix_web(
+        ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Sync + Send + Copy + Clone + 'static> {
             let h = hello_world;
             Ok(move |cfg: &mut ServiceConfig| {
-                cfg.service(resource("/").to(h));
+                cfg.service(resource("/hello").to(h));
             })
         }"#}
     }

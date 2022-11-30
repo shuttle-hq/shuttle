08eca22c4615715484e83023bede8579e7e696f1
chesedo <pieter@chesedo.me>
2022-11-30 09:26:58 +0200

    refactor: comments

--- deployer/src/deployment/storage_manager.rs
+++ deployer/src/deployment/storage_manager.rs
@@ -2,6 +2,7 @@ use std::{fs, path::PathBuf};
 
 use uuid::Uuid;
 
+/// Manager to take care of directories for storing project, services and deployment files
 #[derive(Clone)]
 pub struct StorageManager {
     artifacts_path: PathBuf,
@@ -12,7 +13,7 @@ impl StorageManager {
         Self { artifacts_path }
     }
 
-    // Path of the directory that contains extracted service Cargo projects.
+    /// Path of the directory that contains extracted service Cargo projects.
     pub fn builds_path(&self) -> PathBuf {
         let builds_path = self.artifacts_path.join("shuttle-builds");
         fs::create_dir_all(&builds_path).expect("could not create builds directory");
@@ -20,7 +21,7 @@ impl StorageManager {
         builds_path
     }
 
-    // Path for a specific service
+    /// Path for a specific service
     pub fn service_build_path<S: AsRef<str>>(&self, service_name: S) -> PathBuf {
         let builds_path = self.builds_path().join(service_name.as_ref());
         fs::create_dir_all(&builds_path).expect("could not create service builds directory");
@@ -28,7 +29,7 @@ impl StorageManager {
         builds_path
     }
 
-    // The directory in which compiled '.so' files are stored.
+    /// The directory in which compiled '.so' files are stored.
     pub fn libraries_path(&self) -> PathBuf {
         let libs_path = self.artifacts_path.join("shuttle-libs");
         fs::create_dir_all(&libs_path).expect("could not create libs directory");
@@ -36,12 +37,12 @@ impl StorageManager {
         libs_path
     }
 
-    // Path to `.so` for a service
+    /// Path to `.so` for a service
     pub fn deployment_library_path(&self, deployment_id: &Uuid) -> PathBuf {
         self.libraries_path().join(deployment_id.to_string())
     }
 
-    // Path of the directory to store user files
+    /// Path of the directory to store user files
     pub fn storage_path(&self) -> PathBuf {
         let storage_path = self.artifacts_path.join("shuttle-storage");
         fs::create_dir_all(&storage_path).expect("could not create storage directory");
@@ -49,7 +50,7 @@ impl StorageManager {
         storage_path
     }
 
-    // Path to folder for storing deployment files
+    /// Path to folder for storing deployment files
     pub fn deployment_storage_path<S: AsRef<str>>(
         &self,
         service_name: S,

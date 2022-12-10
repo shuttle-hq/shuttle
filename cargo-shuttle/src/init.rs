811e6d3020049796466ba039df03e93eda8642bc
oddgrd <29732646+oddgrd@users.noreply.github.com>
2022-12-10 10:46:11 +0100

    feat: update init code to match updated example

--- cargo-shuttle/src/init.rs
+++ cargo-shuttle/src/init.rs
@@ -85,18 +85,19 @@ impl ShuttleInit for ShuttleInitActixWeb {
 
     fn get_boilerplate_code_for_framework(&self) -> &'static str {
         indoc! {r#"
-        use actix_web::web::{resource, ServiceConfig};
+        use actix_web::{get, web::ServiceConfig};
         use shuttle_service::ShuttleActixWeb;
 
+        #[get("/hello")]
         async fn hello_world() -> &'static str {
             "Hello World!"
         }
 
         #[shuttle_service::main]
         async fn actix_web(
-        ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Sync + Send + Copy + Clone + 'static> {
+        ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Sync + Send + Clone + 'static> {
             Ok(move |cfg: &mut ServiceConfig| {
-                cfg.service(resource("/hello").to(hello_world));
+                cfg.service(hello_world);
             })
         }"#}
     }

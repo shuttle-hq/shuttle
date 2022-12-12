489b92595f0e41290d376a8965678c0263a2ef85
Oddbjørn Grødem <29732646+oddgrd@users.noreply.github.com>
2022-12-12 10:13:41 +0100

    fix: actix integration with state (#523)

--- service/src/lib.rs
+++ service/src/lib.rs
@@ -557,10 +557,14 @@ impl Service for sync_wrapper::SyncWrapper<axum::Router> {
 #[async_trait]
 impl<F> Service for F
 where
-    F: FnOnce(&mut actix_web::web::ServiceConfig) + Sync + Send + Copy + Clone + 'static,
+    F: FnOnce(&mut actix_web::web::ServiceConfig) + Sync + Send + Clone + 'static,
 {
-    async fn bind(self: Box<Self>, addr: SocketAddr) -> Result<(), Error> {
-        let srv = actix_web::HttpServer::new(move || actix_web::App::new().configure(*self))
+    async fn bind(mut self: Box<Self>, addr: SocketAddr) -> Result<(), Error> {
+        // Start a worker for each cpu, but no more than 8.
+        let worker_count = num_cpus::get().max(8);
+
+        let srv = actix_web::HttpServer::new(move || actix_web::App::new().configure(self.clone()))
+            .workers(worker_count)
             .bind(addr)?
             .run();
         srv.await.map_err(error::CustomError::new)?;

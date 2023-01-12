4056a18ff1b6697237d394b3f71f114eae9e7ea5
chesedo <pieter@chesedo.me>
2023-01-12 14:08:24 +0200

    refactor: update to shuttle_next::endpoint

--- tmp/axum-wasm/src/lib.rs
+++ tmp/axum-wasm/src/lib.rs
@@ -1,13 +1,13 @@
 use tracing::debug;
 
 shuttle_next::app! {
-    #[shuttle_codegen::endpoint(method = get, route = "/hello")]
+    #[shuttle_next::endpoint(method = get, route = "/hello")]
     async fn hello() -> &'static str {
         debug!("called hello()");
         "Hello, World!"
     }
 
-    #[shuttle_codegen::endpoint(method = get, route = "/goodbye")]
+    #[shuttle_next::endpoint(method = get, route = "/goodbye")]
     async fn goodbye() -> &'static str {
         debug!("called goodbye()");
         "Goodbye, World!"

2db8cac3131b2727029792b2bd6ee8585a9dc9f7
chesedo <pieter@chesedo.me>
2023-01-13 08:16:56 +0200

    feat: shutdown runtimes

--- deployer/src/runtime_manager.rs
+++ deployer/src/runtime_manager.rs
@@ -2,7 +2,7 @@ use std::{path::PathBuf, sync::Arc};
 
 use anyhow::Context;
 use shuttle_proto::runtime::{self, runtime_client::RuntimeClient, SubscribeLogsRequest};
-use tokio::sync::Mutex;
+use tokio::{process, sync::Mutex};
 use tonic::transport::Channel;
 
 use crate::deployment::deploy_layer;
@@ -10,7 +10,9 @@ use crate::deployment::deploy_layer;
 #[derive(Clone)]
 pub struct RuntimeManager {
     legacy: Option<RuntimeClient<Channel>>,
+    legacy_process: Option<Arc<std::sync::Mutex<process::Child>>>,
     next: Option<RuntimeClient<Channel>>,
+    next_process: Option<Arc<std::sync::Mutex<process::Child>>>,
     binary_bytes: Vec<u8>,
     artifacts_path: PathBuf,
     provisioner_address: String,
@@ -26,7 +28,9 @@ impl RuntimeManager {
     ) -> Arc<Mutex<Self>> {
         Arc::new(Mutex::new(Self {
             legacy: None,
+            legacy_process: None,
             next: None,
+            next_process: None,
             binary_bytes: binary_bytes.to_vec(),
             artifacts_path,
             provisioner_address,
@@ -41,6 +45,7 @@ impl RuntimeManager {
         if is_next {
             Self::get_runtime_client_helper(
                 &mut self.next,
+                &mut self.next_process,
                 6002,
                 &self.binary_bytes,
                 self.artifacts_path.clone(),
@@ -51,6 +56,7 @@ impl RuntimeManager {
         } else {
             Self::get_runtime_client_helper(
                 &mut self.legacy,
+                &mut self.legacy_process,
                 6001,
                 &self.binary_bytes,
                 self.artifacts_path.clone(),
@@ -63,6 +69,7 @@ impl RuntimeManager {
 
     async fn get_runtime_client_helper(
         runtime_option: &mut Option<RuntimeClient<Channel>>,
+        process_option: &mut Option<Arc<std::sync::Mutex<process::Child>>>,
         port: u16,
         binary_bytes: &[u8],
         artifacts_path: PathBuf,
@@ -72,7 +79,7 @@ impl RuntimeManager {
         if let Some(ref runtime_client) = runtime_option {
             Ok(runtime_client.clone())
         } else {
-            let (_runtime, mut runtime_client) = runtime::start(
+            let (process, mut runtime_client) = runtime::start(
                 binary_bytes,
                 true,
                 runtime::StorageManagerType::Artifacts(artifacts_path),
@@ -96,8 +103,21 @@ impl RuntimeManager {
             });
 
             *runtime_option = Some(runtime_client.clone());
+            *process_option = Some(Arc::new(std::sync::Mutex::new(process)));
 
             Ok(runtime_client)
+        }
+    }
+}
+
+impl Drop for RuntimeManager {
+    fn drop(&mut self) {
+        if let Some(ref process) = self.legacy_process {
+            let _ = process.lock().unwrap().start_kill();
+        }
+
+        if let Some(ref process) = self.next_process {
+            let _ = process.lock().unwrap().start_kill();
         }
     }
 }

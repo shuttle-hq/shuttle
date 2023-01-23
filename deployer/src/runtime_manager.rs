d191d66ac16146e4af3f401bdb917396785e2dd4
Oddbjørn Grødem <29732646+oddgrd@users.noreply.github.com>
2023-01-23 12:24:12 +0100

    feat: build our images with the release profile (#583)
    * feat: build our images with the release profile
    
    Currently our containerfile builds images with the default debug profile, which means when `get_runtime_executable` is called it will try to install from a checked out local version of `shuttle-runtime`.
    
    * feat: add build arg for cargo profile
    
    * refactor: update comment
    
    * fix: broken codegen/axum test
    
    * feat: install wasm32-wasi target in deploy.sh
    
    * feat: build deps in `CARGO_PROFILE`
    
    * feat: use closure to determine shuttle-runtime path
    
    * fix: path to runtime in docker deployer, clippy
    
    * fix: incorrect function for cargo home
    
    * refactor: always use cargo_home for path to runtime in deployer

--- deployer/src/runtime_manager.rs
+++ deployer/src/runtime_manager.rs
@@ -8,6 +8,8 @@ use tracing::{info, instrument, trace};
 
 use crate::deployment::deploy_layer;
 
+const MANIFEST_DIR: &str = env!("CARGO_MANIFEST_DIR");
+
 #[derive(Clone)]
 pub struct RuntimeManager {
     legacy: Option<RuntimeClient<Channel>>,
@@ -80,11 +82,39 @@ impl RuntimeManager {
             Ok(runtime_client)
         } else {
             trace!("making new client");
+
+            let get_runtime_executable = || {
+                if cfg!(debug_assertions) {
+                    // If we're running deployer natively, install shuttle-runtime using the
+                    // version of runtime from the calling repo.
+                    let path = std::fs::canonicalize(format!("{MANIFEST_DIR}/../runtime"));
+
+                    // The path will not be valid if we are in a deployer container, in which
+                    // case we don't try to install and use the one installed in deploy.sh.
+                    if let Ok(path) = path {
+                        std::process::Command::new("cargo")
+                            .arg("install")
+                            .arg("shuttle-runtime")
+                            .arg("--path")
+                            .arg(path)
+                            .output()
+                            .expect("failed to install the local version of shuttle-runtime");
+                    }
+                }
+
+                // If we're in a deployer built with the containerfile, the runtime will have
+                // been installed in deploy.sh.
+                home::cargo_home()
+                    .expect("failed to find path to cargo home")
+                    .join("bin/shuttle-runtime")
+            };
+
             let (process, runtime_client) = runtime::start(
                 is_next,
                 runtime::StorageManagerType::Artifacts(artifacts_path),
                 provisioner_address,
                 port,
+                get_runtime_executable,
             )
             .await
             .context("failed to start shuttle runtime")?;

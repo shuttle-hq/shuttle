b8a303db4b5d7aba8c0df926c78fc37fd4448c9d
chesedo <pieter@chesedo.me>
2023-02-13 16:19:38 +0200

    feat: --address

--- auth/src/args.rs
+++ auth/src/args.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{net::SocketAddr, path::PathBuf};
 
 use clap::Parser;
 
@@ -7,4 +7,8 @@ pub struct Args {
     /// Where to store auth state (such as users)
     #[arg(long, default_value = "./")]
     pub state: PathBuf,
+
+    /// Address to bind to
+    #[arg(long, default_value = "127.0.0.1:8000")]
+    pub user: SocketAddr,
 }

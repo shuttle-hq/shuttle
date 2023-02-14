b3e11b5ec0eb129dfbe06f90b99c785ea5334126
Pieter <pieter@chesedo.me>
2023-02-14 15:43:17 +0200

    feat: create an auth project + clap (#630)
    * feat: shuttle-auth
    
    * refactor: make clap a workspace dependency
    
    * feat: tracing skeleton
    
    * feat: --address

---
+++ auth/src/args.rs
@@ -0,0 +1,14 @@
+use std::{net::SocketAddr, path::PathBuf};
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+pub struct Args {
+    /// Where to store auth state (such as users)
+    #[arg(long, default_value = "./")]
+    pub state: PathBuf,
+
+    /// Address to bind to
+    #[arg(long, default_value = "127.0.0.1:8000")]
+    pub user: SocketAddr,
+}

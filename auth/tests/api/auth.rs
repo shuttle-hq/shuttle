25ab1157bfd2d486612a59738f7faabc5371e539
oddgrd <29732646+oddgrd@users.noreply.github.com>
2023-02-17 16:41:52 +0100

    fix: clippy

--- auth/tests/api/auth.rs
+++ auth/tests/api/auth.rs
@@ -15,7 +15,7 @@ async fn convert_api_key_to_jwt() {
 
     // GET /auth/key without bearer token.
     let request = Request::builder()
-        .uri(format!("/auth/key"))
+        .uri("/auth/key")
         .body(Body::empty())
         .unwrap();
 
@@ -25,7 +25,7 @@ async fn convert_api_key_to_jwt() {
 
     // GET /auth/key with invalid bearer token.
     let request = Request::builder()
-        .uri(format!("/auth/key"))
+        .uri("/auth/key")
         .header(AUTHORIZATION, "Bearer notadmin")
         .body(Body::empty())
         .unwrap();
@@ -36,7 +36,7 @@ async fn convert_api_key_to_jwt() {
 
     // GET /auth/key with valid bearer token.
     let request = Request::builder()
-        .uri(format!("/auth/key"))
+        .uri("/auth/key")
         .header(AUTHORIZATION, format!("Bearer {ADMIN_KEY}"))
         .body(Body::empty())
         .unwrap();
@@ -45,9 +45,5 @@ async fn convert_api_key_to_jwt() {
 
     assert_eq!(response.status(), StatusCode::OK);
 
-    let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
-    // println!("{:?}", body);
-
-    // TODO: get the decoding key from the public key endpoint when it's added,
-    // then decode the JWT.
+    // TODO: decode the JWT?
 }

c5c322c5c4de2f4345afd508d7a3005b3ff64053
oddgrd <29732646+oddgrd@users.noreply.github.com>
2023-02-27 16:20:09 +0100

    fix: missing import

--- deployer/src/handlers/mod.rs
+++ deployer/src/handlers/mod.rs
@@ -29,6 +29,7 @@ use crate::deployment::{DeploymentManager, Queued};
 use crate::persistence::{Deployment, Log, Persistence, ResourceManager, SecretGetter, State};
 
 use std::collections::HashMap;
+use std::sync::Arc;
 
 pub use {self::error::Error, self::error::Result};
 

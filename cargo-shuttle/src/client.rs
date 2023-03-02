48e55d6e8883f994f3a51ce4f56cd31cb77228f6
chesedo <pieter@chesedo.me>
2023-03-02 14:39:58 +0200

    bug: sending project config

--- cargo-shuttle/src/client.rs
+++ cargo-shuttle/src/client.rs
@@ -2,11 +2,11 @@ use std::fmt::Write;
 
 use anyhow::{Context, Result};
 use headers::{Authorization, HeaderMapExt};
-use reqwest::{Body, Response};
+use reqwest::Response;
 use reqwest_middleware::{ClientBuilder, ClientWithMiddleware, RequestBuilder};
 use reqwest_retry::policies::ExponentialBackoff;
 use reqwest_retry::RetryTransientMiddleware;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use shuttle_common::models::{deployment, project, secret, service, ToJson};
 use shuttle_common::project::ProjectName;
 use shuttle_common::{ApiKey, ApiUrl, LogItem};
@@ -49,7 +49,16 @@ impl Client {
             let _ = write!(path, "?no-test");
         }
 
-        self.post(path, Some(data))
+        let url = format!("{}{}", self.api_url, path);
+
+        let mut builder = Self::get_retry_client().post(url);
+
+        builder = self.set_builder_auth(builder);
+
+        builder
+            .body(data)
+            .header("Transfer-Encoding", "chunked")
+            .send()
             .await
             .context("failed to send deployment to the Shuttle server")?
             .to_json()
@@ -92,8 +101,6 @@ impl Client {
         config: project::Config,
     ) -> Result<project::Response> {
         let path = format!("/projects/{}", project.as_str());
-
-        let config = serde_json::to_string(&config)?;
 
         self.post(path, Some(config))
             .await
@@ -227,11 +234,7 @@ impl Client {
             .await
     }
 
-    async fn post<T: Into<Body>>(
-        &self,
-        path: String,
-        body: Option<T>,
-    ) -> Result<Response, reqwest_middleware::Error> {
+    async fn post<T: Serialize>(&self, path: String, body: Option<T>) -> Result<Response> {
         let url = format!("{}{}", self.api_url, path);
 
         let mut builder = Self::get_retry_client().post(url);
@@ -239,11 +242,12 @@ impl Client {
         builder = self.set_builder_auth(builder);
 
         if let Some(body) = body {
+            let body = serde_json::to_string(&body)?;
             builder = builder.body(body);
-            builder = builder.header("Transfer-Encoding", "chunked");
+            builder = builder.header("Content-Type", "application/json");
         }
 
-        builder.send().await
+        Ok(builder.send().await?)
     }
 
     async fn delete<M>(&self, path: String) -> Result<M>

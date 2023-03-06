52096fc6d895ab0146f24313d112d13d2198a582
Oddbjørn Grødem <29732646+oddgrd@users.noreply.github.com>
2023-03-06 08:14:09 +0000

    feat: extract next runtime into separate binary (#679)
    * feat: extract next runtime into separate binary
    
    * fix: remove startrequestsservice name

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1,9 +1,8 @@
-mod args;
-mod axum;
 mod legacy;
+mod next;
 mod provisioner_factory;
 
-pub use args::{Args, StorageManagerType};
-pub use axum::AxumWasm;
 pub use legacy::{start, Legacy};
+pub use next::AxumWasm;
+pub use next::NextArgs;
 pub use provisioner_factory::ProvisionerFactory;

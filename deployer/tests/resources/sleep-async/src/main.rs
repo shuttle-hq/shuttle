5f21fbc4ddd6ac428b09befb468d77055590bb2a
oddgrd <29732646+oddgrd@users.noreply.github.com>
2023-03-08 17:04:56 +0100

    tests: make deployer test resources bins

--- deployer/tests/resources/sleep-async/src/main.rs
+++ deployer/tests/resources/sleep-async/src/main.rs
@@ -14,10 +14,7 @@ async fn simple() -> Result<SleepService, shuttle_service::Error> {
 
 #[shuttle_service::async_trait]
 impl Service for SleepService {
-    async fn bind(
-        mut self: Box<Self>,
-        _: std::net::SocketAddr,
-    ) -> Result<(), shuttle_service::error::Error> {
+    async fn bind(mut self, _: std::net::SocketAddr) -> Result<(), shuttle_service::error::Error> {
         let duration = Duration::from_secs(self.duration);
 
         sleep(duration).await;

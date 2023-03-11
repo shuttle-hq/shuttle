d042562d1d21f4715c56ffd58257c9beb0e76ef4
oddgrd <29732646+oddgrd@users.noreply.github.com>
2023-03-11 14:53:56 +0100

    feat: extract the serenity service integration

--- service/src/lib.rs
+++ service/src/lib.rs
@@ -457,17 +457,4 @@ where
     }
 }
 
-#[cfg(feature = "bot-serenity")]
-#[async_trait]
-impl Service for serenity::Client {
-    async fn bind(mut self, _addr: SocketAddr) -> Result<(), error::Error> {
-        self.start().await.map_err(error::CustomError::new)?;
-
-        Ok(())
-    }
-}
-
-#[cfg(feature = "bot-serenity")]
-pub type ShuttleSerenity = Result<serenity::Client, Error>;
-
 pub const NEXT_NAME: &str = "shuttle-next";

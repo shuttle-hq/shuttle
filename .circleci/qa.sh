1e9709e397a30ce3a274ed48806063333acc3bac
chesedo <pieter@chesedo.me>
2023-04-14 14:51:02 +0200

    ci: uncomment

--- .circleci/qa.sh
+++ .circleci/qa.sh
@@ -1,19 +1,18 @@
 #! /usr/bin/env sh
 
 # Prepare directory
-# rm -R tmp/qa-linux
-# mkdir -p tmp/qa-linux
+rm -R tmp/qa-linux
+mkdir -p tmp/qa-linux
 cd tmp/qa-linux
 
 # Init app
-# cargo shuttle init --name qa-linux --axum
+cargo shuttle init --name qa-linux --axum
 
 # Start locally
-# cargo shuttle run &
-# sleep 20
-# sleep 2
+cargo shuttle run &
+sleep 20
 
 output=$(curl --silent localhost:8000/hello)
-[ "$output" != "Hello, world!" ] && ( echo "Did not expect output: $output"; exit 1 )
+[ "$output" != "Hello, worl" ] && ( echo "Did not expect output: $output"; exit 1 )
 
 exit 0

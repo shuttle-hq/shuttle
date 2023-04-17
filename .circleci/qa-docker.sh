db6e6468351b68f85585c7d493ea51572f911fcf
chesedo <pieter@chesedo.me>
2023-04-17 14:43:38 +0200

    ci: allow more time

--- .circleci/qa-docker.sh
+++ .circleci/qa-docker.sh
@@ -7,7 +7,7 @@ cd examples/rocket/postgres
 
 # Start locally
 cargo shuttle run &
-sleep 90
+sleep 120
 
 echo "Testing local docker endpoint"
 output=$(curl --silent --request POST --header "Content-Type: application/json" --data '{"note": "test"}' localhost:8000/todo)

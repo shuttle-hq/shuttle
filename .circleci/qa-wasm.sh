edb1d2e9f5a1ab564c4fe8612b8e0d4f6db16b7e
chesedo <pieter@chesedo.me>
2023-04-17 11:10:23 +0200

    ci: add wasm (next) qa

---
+++ .circleci/qa-wasm.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env sh
+
+set -ue
+
+# Install the WASM target
+rustup target add wasm32-wasi
+
+# Install wasm runtime from checked out code
+cargo install shuttle-runtime --path runtime --bin shuttle-next --features next
+
+# cd into the WASM example
+cd examples/next/hello-world
+
+# Start locally
+cargo shuttle run &
+sleep 60
+
+echo "Testing local wasm endpoint"
+output=$(curl --silent localhost:8000/hello)
+[ "$output" != "Hello, world!" ] && ( echo "Did not expect output: $output"; exit 1 )
+
+killall cargo-shuttle

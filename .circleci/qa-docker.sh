4f05378466a9f8315065321c17c417076d522d62
chesedo <pieter@chesedo.me>
2023-04-17 11:58:08 +0200

    ci: add docker qa

---
+++ .circleci/qa-docker.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env sh
+
+set -ue
+
+# cd into the Docker example
+cd examples/rocket/postgres
+
+# Start locally
+cargo shuttle run &
+sleep 60
+
+echo "Testing local docker endpoint"
+output=$(curl --silent --request POST --header "Content-Type: application/json" --data '{"note": "test"}' localhost:8000/todo)
+[ "$output" != '{"id":1,"note":"test"}' ] && ( echo "Did not expect POST output: $output"; exit 1 )
+
+output=$(curl --silent localhost:8000/todo/1)
+[ "$output" != '{"id":1,"note":"test"}' ] && ( echo "Did not expect output: $output"; exit 1 )
+
+killall cargo-shuttle

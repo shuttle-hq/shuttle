838bc3d965bfd2ccdfe653e69b287c136d7c00b3
Pieter <pieter@chesedo.me>
2023-04-23 21:01:56 +0200

    ci: add mac qa (#801)
    * ci: add mac qa
    
    * ci: larger resource
    
    * ci: smaller resources
    
    * ci: install protobuf
    
    * ci: make project name unique
    
    * ci: install docker
    
    * ci: more time
    
    * bug: parse runtime version correctly
    
    * ci: more time
    
    * ci: more time
    
    * ci: more time
    
    * ci: remove Docker mac test
    
    * ci: mac-qa requirement

--- .circleci/qa-docker.sh
+++ .circleci/qa-docker.sh
@@ -7,7 +7,7 @@ cd examples/rocket/postgres
 
 # Start locally
 cargo shuttle run &
-sleep 100
+sleep 300
 
 echo "Testing local docker endpoint"
 output=$(curl --silent --request POST --header "Content-Type: application/json" --data '{"note": "test"}' localhost:8000/todo)

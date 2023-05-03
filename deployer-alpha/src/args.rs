0aa99076a34fb28937f4e0550db4a98b2967888f
Pieter <pieter@chesedo.me>
2023-05-03 17:09:26 +0200

    [orchestrator] refactor: prepare feature branch (#850)
    * refactor: rename old deployer
    
    * feat: new deployer
    
    * Update deployer/src/main.rs
    
    Co-authored-by: Oddbjørn Grødem <29732646+oddgrd@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Oddbjørn Grødem <29732646+oddgrd@users.noreply.github.com>

---
+++ deployer-alpha/src/args.rs
@@ -0,0 +1,53 @@
+use std::{net::SocketAddr, path::PathBuf};
+
+use clap::Parser;
+use fqdn::FQDN;
+use hyper::Uri;
+use shuttle_common::project::ProjectName;
+use tonic::transport::Endpoint;
+
+/// Program to handle the deploys for a single project
+/// Handling includes, building, testing, and running each service
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+pub struct Args {
+    /// Uri to the `.sqlite` file used to store state
+    #[clap(long, default_value = "./deployer.sqlite")]
+    pub state: String,
+
+    /// Address to connect to the provisioning service
+    #[clap(long, default_value = "http://provisioner:5000")]
+    pub provisioner_address: Endpoint,
+
+    /// FQDN where the proxy can be reached at
+    #[clap(long)]
+    pub proxy_fqdn: FQDN,
+
+    /// Address to bind API to
+    #[clap(long, default_value = "0.0.0.0:8001")]
+    pub api_address: SocketAddr,
+
+    /// Address to bind proxy to
+    #[clap(long, default_value = "0.0.0.0:8000")]
+    pub proxy_address: SocketAddr,
+
+    /// Address to reach gateway's control plane at
+    #[clap(long, default_value = "http://gateway:8001")]
+    pub gateway_uri: Uri,
+
+    /// Project being served by this deployer
+    #[clap(long)]
+    pub project: ProjectName,
+
+    /// Secret that will be used to perform admin tasks on this deployer
+    #[clap(long)]
+    pub admin_secret: String,
+
+    /// Address to reach the authentication service at
+    #[clap(long, default_value = "http://127.0.0.1:8008")]
+    pub auth_uri: Uri,
+
+    /// Uri to folder to store all artifacts
+    #[clap(long, default_value = "/tmp")]
+    pub artifacts_path: PathBuf,
+}
--- deployer/src/main.rs
+++ deployer/src/main.rs
@@ -1,41 +1,13 @@
-use std::process::exit;
-
 use clap::Parser;
 use shuttle_common::backends::tracing::setup_tracing;
-use shuttle_deployer::{start, start_proxy, Args, DeployLayer, Persistence, RuntimeManager};
-use tokio::select;
-use tracing::{error, trace};
-use tracing_subscriber::prelude::*;
+use shuttle_deployer::args::Args;
+use tracing::trace;
 
-// The `multi_thread` is needed to prevent a deadlock in shuttle_service::loader::build_crate() which spawns two threads
-// Without this, both threads just don't start up
-#[tokio::main(flavor = "multi_thread")]
+#[tokio::main]
 async fn main() {
     let args = Args::parse();
 
     trace!(args = ?args, "parsed args");
 
-    let (persistence, _) = Persistence::new(&args.state).await;
-    setup_tracing(
-        tracing_subscriber::registry().with(DeployLayer::new(persistence.clone())),
-        "deployer",
-    );
-
-    let runtime_manager = RuntimeManager::new(
-        args.artifacts_path.clone(),
-        args.provisioner_address.uri().to_string(),
-        Some(args.auth_uri.to_string()),
-        persistence.get_log_sender(),
-    );
-
-    select! {
-        _ = start_proxy(args.proxy_address, args.proxy_fqdn.clone(), persistence.clone()) => {
-            error!("Proxy stopped.")
-        },
-        _ = start(persistence, runtime_manager, args) => {
-            error!("Deployment service stopped.")
-        },
-    }
-
-    exit(1);
+    setup_tracing(tracing_subscriber::registry(), "deployer");
 }

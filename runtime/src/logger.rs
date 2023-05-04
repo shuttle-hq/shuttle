b99969ba45419e2c183a72168ba26b8aea4ad49c
Vlad Stepanov <8uk.8ak@gmail.com>
2023-05-04 16:18:09 +0400

    feat: add on_new_span impl to runtime Logger

--- runtime/src/logger.rs
+++ runtime/src/logger.rs
@@ -5,7 +5,10 @@ use prost_types::Timestamp;
 use shuttle_common::tracing::JsonVisitor;
 use shuttle_proto::runtime::{LogItem, LogLevel};
 use tokio::sync::mpsc::UnboundedSender;
-use tracing::Subscriber;
+use tracing::{
+    span::{Attributes, Id},
+    Subscriber,
+};
 use tracing_subscriber::Layer;
 
 pub struct Logger {
@@ -22,6 +25,41 @@ impl<S> Layer<S> for Logger
 where
     S: Subscriber,
 {
+    fn on_new_span(
+        &self,
+        attrs: &Attributes,
+        _id: &Id,
+        _ctx: tracing_subscriber::layer::Context<'_, S>,
+    ) {
+        let datetime = Utc::now();
+
+        let item = {
+            let metadata = attrs.metadata();
+            let mut visitor = JsonVisitor::default();
+
+            attrs.record(&mut visitor);
+
+            // TODO: This is a hack to get the name of span to show up in the logs
+            visitor.fields.insert(
+                "message".to_string(),
+                format!("[span] {}", metadata.name()).into(),
+            );
+
+            LogItem {
+                level: LogLevel::from(metadata.level()) as i32,
+                timestamp: Some(Timestamp::from(SystemTime::from(datetime))),
+                file: visitor.file.or_else(|| metadata.file().map(str::to_string)),
+                line: visitor.line.or_else(|| metadata.line()),
+                target: visitor
+                    .target
+                    .unwrap_or_else(|| metadata.target().to_string()),
+                fields: serde_json::to_vec(&visitor.fields).unwrap(),
+            }
+        };
+
+        self.tx.send(item).expect("sending log should succeed");
+    }
+
     fn on_event(
         &self,
         event: &tracing::Event<'_>,
@@ -66,11 +104,18 @@ mod tests {
 
         let _guard = tracing_subscriber::registry().with(logger).set_default();
 
-        tracing::debug!("this is");
-        tracing::info!("hi");
-        tracing::warn!("from");
-        tracing::error!("logger");
+        let span = tracing::debug_span!("this is a span");
+        span.in_scope(|| {
+            tracing::debug!("this is");
+            tracing::info!("hi");
+            tracing::warn!("from");
+            tracing::error!("logger");
+        });
 
+        assert_eq!(
+            r.blocking_recv().map(to_tuple),
+            Some(("[span] this is a span".to_string(), LogLevel::Debug as i32))
+        );
         assert_eq!(
             r.blocking_recv().map(to_tuple),
             Some(("this is".to_string(), LogLevel::Debug as i32))

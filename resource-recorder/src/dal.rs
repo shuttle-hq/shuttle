3775d522b7c8c7e11b305b9bdcdc90a84f135d37
chesedo <pieter@chesedo.me>
2023-05-04 17:31:33 +0200

    feat: keep track of inactive resources

--- resource-recorder/src/dal.rs
+++ resource-recorder/src/dal.rs
@@ -1,6 +1,7 @@
 use crate::r#type::Type;
 use async_trait::async_trait;
-use sqlx::{migrate::Migrator, QueryBuilder, SqlitePool};
+use chrono::{DateTime, Utc};
+use sqlx::{migrate::Migrator, SqlitePool};
 use tracing::warn;
 use uuid::Uuid;
 
@@ -48,11 +49,14 @@ impl Dal for Sqlite {
         service_id: Uuid,
         resources: Vec<Resource>,
     ) -> Result<(), Self::Error> {
-        let mut query_builder = QueryBuilder::new(
-            "INSERT OR REPLACE INTO resources (id, service_id, type, config, data) ",
-        );
+        let mut transaction = self.pool.begin().await?;
 
-        query_builder.push_values(resources, |mut b, mut resource| {
+        sqlx::query("UPDATE resources SET is_active = false WHERE service_id = ?")
+            .bind(service_id)
+            .execute(&mut transaction)
+            .await?;
+
+        for mut resource in resources {
             if let Some(r_service_id) = resource.service_id {
                 if r_service_id != service_id {
                     warn!("adding a resource that belongs to another service");
@@ -64,14 +68,18 @@ impl Dal for Sqlite {
                 resource.id = Some(Uuid::new_v4());
             }
 
-            b.push_bind(resource.id)
-                .push_bind(service_id)
-                .push_bind(resource.r#type)
-                .push_bind(resource.config)
-                .push_bind(resource.data);
-        });
+            sqlx::query("INSERT OR REPLACE INTO resources (id, service_id, type, config, data, is_active) VALUES(?, ?, ?, ?, ?, ?)")
+            .bind(resource.id)
+            .bind(service_id)
+            .bind(resource.r#type)
+            .bind(resource.config)
+            .bind(resource.data)
+            .bind(resource.is_active)
+            .execute(&mut transaction)
+            .await?;
+        }
 
-        query_builder.build().execute(&self.pool).await.map(|_| ())
+        transaction.commit().await
     }
 
     async fn get_resources(&self, service_id: Uuid) -> Result<Vec<Resource>, Self::Error> {
@@ -89,6 +97,22 @@ pub struct Resource {
     pub r#type: Type,
     pub data: serde_json::Value,
     pub config: serde_json::Value,
+    pub is_active: bool,
+    pub created_at: DateTime<Utc>,
+}
+
+impl Resource {
+    fn new(r#type: Type, data: serde_json::Value, config: serde_json::Value) -> Self {
+        Self {
+            id: None,
+            service_id: None,
+            r#type,
+            data,
+            config,
+            is_active: true,
+            created_at: Default::default(),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -108,36 +132,57 @@ mod tests {
         let dal = Sqlite::new_in_memory().await;
         let service_id = Uuid::new_v4();
 
-        let database = Resource {
-            id: None,
-            service_id: None,
-            r#type: Type::Database(crate::r#type::database::Type::Shared(
+        // Test with a small set of initial resources
+        let mut database = Resource::new(
+            Type::Database(crate::r#type::database::Type::Shared(
                 crate::r#type::database::SharedType::Postgres,
             )),
-            config: json!({"private": false}),
-            data: json!({"username": "test"}),
-        };
-        let secrets = Resource {
-            id: None,
-            service_id: None,
-            r#type: Type::Secrets,
-            config: json!({}),
-            data: json!({"password": "p@ssw0rd"}),
-        };
-        let static_folder = Resource {
-            id: None,
-            service_id: None,
-            r#type: Type::StaticFolder,
-            config: json!({"path": "static"}),
-            data: json!({"path": "/tmp/static"}),
-        };
+            json!({"private": false}),
+            json!({"username": "test"}),
+        );
+        let mut static_folder = Resource::new(
+            Type::StaticFolder,
+            json!({"path": "static"}),
+            json!({"path": "/tmp/static"}),
+        );
 
-        dal.add_resources(
-            service_id,
-            vec![database.clone(), secrets.clone(), static_folder.clone()],
-        )
-        .await
-        .unwrap();
+        dal.add_resources(service_id, vec![database.clone(), static_folder.clone()])
+            .await
+            .unwrap();
+
+        let actual = dal.get_resources(service_id).await.unwrap();
+
+        // The query would set these
+        database.id = actual[0].id;
+        database.created_at = actual[0].created_at;
+        static_folder.id = actual[1].id;
+        static_folder.created_at = actual[1].created_at;
+
+        let expected = vec![database.clone(), static_folder]
+            .into_iter()
+            .map(|mut r| {
+                r.service_id = Some(service_id);
+                r
+            })
+            .collect::<Vec<_>>();
+
+        assert_eq!(expected, actual);
+
+        // This time the user is adding secrets but dropping the static folders
+        let mut secrets = Resource::new(Type::Secrets, json!({}), json!({"password": "p@ssw0rd"}));
+
+        let database = actual[0].clone();
+        let mut static_folder = actual[1].clone();
+
+        dal.add_resources(service_id, vec![database.clone(), secrets.clone()])
+            .await
+            .unwrap();
+
+        let actual = dal.get_resources(service_id).await.unwrap();
+
+        static_folder.is_active = false;
+        secrets.id = actual[1].id;
+        secrets.created_at = actual[1].created_at;
 
         let expected = vec![database, secrets, static_folder]
             .into_iter()
@@ -146,16 +191,7 @@ mod tests {
                 r
             })
             .collect::<Vec<_>>();
-        let actual = dal.get_resources(service_id).await.unwrap();
-        let actual_without_id = actual
-            .iter()
-            .map(|r| {
-                let mut r = r.clone();
-                r.id = None;
-                r
-            })
-            .collect::<Vec<_>>();
 
-        assert_eq!(expected, actual_without_id);
+        assert_eq!(expected, actual);
     }
 }

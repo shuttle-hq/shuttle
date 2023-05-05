775b57784d15d950b77194818fd118ae89886cf6
Pieter <pieter@chesedo.me>
2023-05-05 19:01:01 +0200

    misc: rename examples to shuttle-examples (#871)
    * misc: rename examples to shuttle-examples
    
    * misc: update examples repo

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -51,7 +51,7 @@
 //! ```
 //!
 //! Check out [our docs](https://docs.shuttle.rs/introduction/welcome) to see all the frameworks we support, or
-//! our [examples](https://github.com/shuttle-hq/examples) if you prefer that format.
+//! our [examples](https://github.com/shuttle-hq/shuttle-examples) if you prefer that format.
 //!
 //! ## Running locally
 //! To test your app locally before deploying, use:

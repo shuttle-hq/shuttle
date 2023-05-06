6f42e5366e26c77a87707d9ed5698556bf2ccbf2
frectonz <fraol0912@gmail.com>
2023-05-06 22:07:32 +0300

    feat(services): add /healthz route to the `poem` service
    This commit includes `/healthz` route implementations for `poem`.
    This might conflict with user defined routes if they have a
    `/healthz` defined in their code.

--- services/shuttle-poem/src/lib.rs
+++ services/shuttle-poem/src/lib.rs
@@ -21,14 +21,23 @@
 /// A wrapper type for [poem::Endpoint] so we can implement [shuttle_runtime::Service] for it.
 pub struct PoemService<T>(pub T);
 
+#[poem::handler]
+fn healthz() -> poem::http::StatusCode {
+    poem::http::StatusCode::OK
+}
+
 #[shuttle_runtime::async_trait]
 impl<T> shuttle_runtime::Service for PoemService<T>
 where
     T: poem::Endpoint + Send + 'static,
 {
     async fn bind(mut self, addr: std::net::SocketAddr) -> Result<(), shuttle_runtime::Error> {
+        let app = poem::Route::new()
+            .at("/healthz", poem::get(healthz))
+            .nest("/", self.0);
+
         poem::Server::new(poem::listener::TcpListener::bind(addr))
-            .run(self.0)
+            .run(app)
             .await
             .map_err(shuttle_runtime::CustomError::new)?;
 

3549d75eb45485c1a6556ba5e9664991fbfa29bc
Ethan Holz <iplaybassx2@gmail.com>
2023-05-07 16:49:30 -0500

    docs(secrets): Added secrets description

--- resources/secrets/src/lib.rs
+++ resources/secrets/src/lib.rs
@@ -1,9 +1,34 @@
+//! [Shuttle Secrets](https://docs.shuttle.rs/resources/shuttle-secrets)
+//! A secrets plugin for Shuttle for storing environment secrets.
+//!
+//! # Example
+//!
+//! ```
+//! #[shuttle_runtime::main]
+//! async fn rocket(
+//!     #[shuttle_secrets::Secrets] secret_store: SecretStore,
+//! ) -> ShuttleRocket {
+//!     // get secret defined in `Secrets.toml` file.
+//!     let secret = if let Some(secret) = secret_store.get("MY_API_KEY") {
+//!         secret
+//!     } else {
+//!         return Err(anyhow!("secret was not found").into());
+//!     };
+//!
+//!     let state = MyState { secret };
+//!     let rocket = rocket::build().mount("/", routes![secret]).manage(state);
+//!
+//!     Ok(rocket.into())
+//! }
+//! ```
+
 use async_trait::async_trait;
 
 use serde::Serialize;
 pub use shuttle_service::SecretStore;
 use shuttle_service::{Error, Factory, ResourceBuilder, Type};
 
+/// A struct that represents service secrets
 #[derive(Serialize)]
 pub struct Secrets;
 

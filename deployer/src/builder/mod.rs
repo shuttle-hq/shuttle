eec1e664883eedaf0d769edc95d79fc2c7d6db6f
Iulian Barbu <iulianbarbu2@gmail.com>
2023-05-09 16:44:26 +0300

    wip

--- deployer/src/builder/mod.rs
+++ deployer/src/builder/mod.rs
@@ -20,7 +20,7 @@ impl MockedBuilder {
     }
 
     /// This method consumes a source_code_archive and returns a deployable Docker image archive.
-    pub async fn get_default_image_archive(&self, source_code_archive: &Vec<u8>) -> Vec<u8> {
+    pub async fn default_image_archive(&self, source_code_archive: &Vec<u8>) -> Vec<u8> {
         debug!(
             "MockedBuilder received a source code archive of length: {}",
             source_code_archive.len()
@@ -31,5 +31,10 @@ impl MockedBuilder {
         archive.read_to_end(&mut buf).await.unwrap();
         debug!("MockedBuilder returing an image of length: {}", buf.len());
         buf
+    }
+
+    /// This method pushes a built image to an archive registry.
+    pub async fn push_archive(&self) {
+        // TODO
     }
 }

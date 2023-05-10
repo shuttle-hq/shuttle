1f676b6a1652e9359ad9f091a8a3df479458c788
oddgrd <29732646+oddgrd@users.noreply.github.com>
2023-05-10 19:56:56 +0200

    chore: upgrade salvo in shuttle-salvo

--- services/shuttle-salvo/src/lib.rs
+++ services/shuttle-salvo/src/lib.rs
@@ -16,6 +16,7 @@
 //! }
 //!
 //! ```
+use salvo::Listener;
 use shuttle_runtime::Error;
 use std::net::SocketAddr;
 
@@ -27,9 +28,9 @@ impl shuttle_runtime::Service for SalvoService {
     /// Takes the router that is returned by the user in their [shuttle_runtime::main] function
     /// and binds to an address passed in by shuttle.
     async fn bind(mut self, addr: SocketAddr) -> Result<(), Error> {
-        salvo::Server::new(salvo::listener::TcpListener::bind(addr))
-            .serve(self.0)
-            .await;
+        let listener = salvo::conn::TcpListener::new(addr).bind().await;
+
+        salvo::Server::new(listener).serve(self.0).await;
 
         Ok(())
     }

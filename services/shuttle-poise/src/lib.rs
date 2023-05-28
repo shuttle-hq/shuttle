74a5ac452e9a051f8e5aac617bf48c778f247fbe
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-05-28 01:04:43 -0400

    docs(poise): update docs with the official examples

--- services/shuttle-poise/src/lib.rs
+++ services/shuttle-poise/src/lib.rs
@@ -77,10 +77,47 @@ impl<T, E> From<Arc<poise::Framework<T, E>>> for PoiseService<T, E> {
 /// # Example
 ///
 /// ```rust,no_run
+/// use shuttle_runtime::Context as _;
+/// use poise::serenity_prelude as serenity;
+/// use shuttle_secrets::SecretStore;
+/// use shuttle_poise::ShuttlePoise;
+///
+/// struct Data {} // User data, which is stored and accessible in all command invocations
+/// type Error = Box<dyn std::error::Error + Send + Sync>;
+/// type Context<'a> = poise::Context<'a, Data, Error>;
+///
+///
+/// #[poise::command(slash_command)]
+/// async fn hello(ctx: Context<'_>) -> Result<(), Error> {
+///     ctx.say("world!").await?;
+///     Ok(())
+/// }
+///
 /// #[shuttle_runtime::main]
-/// async example_service() ->
-///   ShuttlePoise<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
-///   todo!()
+/// async fn poise(#[shuttle_secrets::Secrets] secret_store: SecretStore) -> ShuttlePoise<Data, Error> {
+///
+///     let discord_token = secret_store
+///         .get("DISCORD_TOKEN")
+///         .context("'DISCORD_TOKEN' was not found")?;
+///
+///     let framework = poise::Framework::builder()
+///         .options(poise::FrameworkOptions {
+///             commands: vec![hello()],
+///             ..Default::default()
+///         })
+///         .token(discord_token)
+///         .intents(serenity::GatewayIntents::non_privileged())
+///         .setup(|ctx, _ready, framework| {
+///             Box::pin(async move {
+///                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+///                 Ok(Data {})
+///             })
+///         })
+///         .build()
+///         .await
+///         .map_err(shuttle_runtime::CustomError::new)?;
+///
+///     Ok(framework.into())
 /// }
 /// ```
 pub type ShuttlePoise<T, E> = Result<PoiseService<T, E>, shuttle_runtime::Error>;

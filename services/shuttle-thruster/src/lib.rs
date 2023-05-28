980ca547f03e8492fec2ab82b5198a614efa7fb6
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-05-28 00:47:34 -0400

    docs(thruster): update docs to align with official examples

--- services/shuttle-thruster/src/lib.rs
+++ services/shuttle-thruster/src/lib.rs
@@ -55,15 +55,23 @@ where
 /// # Example
 /// ```rust,no_run
 /// use shuttle_thruster::ShuttleThruster;
-/// use thruster::{App, Server, ThrusterServer, BasicContext as Ctx, Request};
+/// use thruster::{
+///     context::basic_hyper_context::{generate_context, BasicHyperContext as Ctx, HyperRequest},
+///     m, middleware_fn, App, HyperServer, MiddlewareNext, MiddlewareResult, ThrusterServer,
+/// };
+///
+/// #[middleware_fn]
+/// async fn hello(mut context: Ctx, _next: MiddlewareNext<Ctx>) -> MiddlewareResult<Ctx> {
+///     context.body("Hello, World!");
+///     Ok(context)
+/// }
 ///
 /// #[shuttle_runtime::main]
-/// async fn example_service() ->
-///     ShuttleThruster<impl thruster::ThrusterServer + Send + 'static> {
-///     let app = App::<Request, Ctx, ()>::new_basic();
-///     let server = Server::new(app);
-///
-///   Ok(server.into())
+/// async fn thruster() -> ShuttleThruster<HyperServer<Ctx, ()>> {
+///     Ok(HyperServer::new(
+///         App::<HyperRequest, Ctx, ()>::create(generate_context, ()).get("/hello", m![hello]),
+///     ).into())
 /// }
+///
 /// ```
 pub type ShuttleThruster<T> = Result<ThrusterService<T>, Error>;

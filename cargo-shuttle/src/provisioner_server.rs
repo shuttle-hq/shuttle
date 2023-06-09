9d59a2eb45ac07e1f4238dfb5bcd6bf3209b16d7
Paul Otten <lightnica@yahoo.ca>
2023-06-09 15:33:56 +0100

    cargo fmt

--- cargo-shuttle/src/provisioner_server.rs
+++ cargo-shuttle/src/provisioner_server.rs
@@ -486,10 +486,7 @@ fn dynamodb_config() -> DynamoDbConfig {
         aws_access_key_id: "DUMMY_ID_EXAMPLE".to_string(),
         aws_secret_access_key: "DUMMY_EXAMPLE_KEY".to_string(),
         aws_default_region: "DUMMY_EXAMPLE_REGION".to_string(),
-        is_ready_cmd: vec![
-            "curl".to_string(),
-            "0.0.0.0:8000".to_string(),
-        ],
+        is_ready_cmd: vec!["curl".to_string(), "0.0.0.0:8000".to_string()],
     }
 }
 

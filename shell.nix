9c3d16d0590c7dabe1e2134c6f13bcfbd2aa2fc2
Timon Vonk <timonv@gmail.com>
2023-06-23 12:12:06 +0200

    build(shell.nix): add openssl to nativeBuildInputs to fix build error related to missing openssl library

--- shell.nix
+++ shell.nix
@@ -8,6 +8,7 @@ in
     name = "moz_overlay_shell";
     nativeBuildInputs = with nixpkgs; [
       pkg-config
+      openssl
     ];
     buildInputs = with nixpkgs; [
       ((rustChannelOf{ channel = "1.70.0"; }).rust.override {

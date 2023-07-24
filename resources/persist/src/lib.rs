568e1c203f9a68fa20704c232a2c4662425ea430
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2023-07-23 19:36:11 -0700

    Add tests for all list, size, remove and clear

--- resources/persist/src/lib.rs
+++ resources/persist/src/lib.rs
@@ -16,14 +16,10 @@ pub enum PersistError {
     Open(std::io::Error),
     #[error("failed to create folder: {0}")]
     CreateFolder(std::io::Error),
-    #[error("failed to read folder contents: {0}")]
-    ReadFolder(std::io::Error),
-    #[error("failed to read the entry in the folder: {0}")]
-    ReadEntry(std::io::Error),
     #[error("failed to list contents of folder: {0}")]
     ListFolder(std::io::Error),
-    #[error("failed to clear the folder: {0}")]
-    ClearFolder(std::io::Error),
+    #[error("failed to clear folder: {0}")]
+    RemoveFolder(std::io::Error),
     #[error("failed to remove file: {0}")]
     RemoveFile(std::io::Error),
     #[error("failed to serialize data: {0}")]
@@ -72,20 +68,17 @@ impl PersistInstance {
         Ok(list)
     }
 
-    /// clear method removes the storage folder from the PersistInstance
+    /// clear method removes the keys within the PersistInstance
     pub fn clear(&self) -> Result<(), PersistError> {
         let storage_folder = self.get_storage_folder();
-        let entries = fs::read_dir(storage_folder).map_err(PersistError::ReadFolder)?;
-
-        for entry in entries {
-            let entry = entry.map_err(PersistError::ReadEntry)?;
-            let path = entry.path();
-
-            if path.is_file() {
-                fs::remove_file(path).map_err(PersistError::RemoveFile)?;
-            }
-        }
+        fs::remove_dir_all(&storage_folder).map_err(PersistError::RemoveFolder)?;
+        fs::create_dir_all(&storage_folder).map_err(PersistError::CreateFolder)?;
         Ok(())
+    }
+
+    /// size method returns the number of keys in a folder within a PersistInstance
+    pub fn size(&self) -> Result<usize, PersistError> {
+        Ok(self.list()?.len())
     }
 
     /// remove method deletes a key from the PersistInstance
@@ -93,24 +86,6 @@ impl PersistInstance {
         let file_path = self.get_storage_file(key);
         fs::remove_file(file_path).map_err(PersistError::RemoveFile)?;
         Ok(())
-    }
-
-    /// size method determines the size of all keys stored within a folder, within the PersistInstance
-    pub fn size(&self) -> Result<u64, PersistError> {
-        let storage_folder = self.get_storage_folder();
-        let mut size = 0;
-
-        let entries = fs::read_dir(storage_folder).map_err(PersistError::ReadFolder)?;
-
-        for entry in entries {
-            let entry = entry.map_err(PersistError::ReadEntry)?;
-            let path = entry.path();
-
-            if path.is_file() {
-                size += fs::metadata(&path).map_err(PersistError::ReadEntry)?.len();
-            }
-        }
-        Ok(size)
     }
 
     pub fn load<T>(&self, key: &str) -> Result<T, PersistError>
@@ -170,13 +145,7 @@ impl ResourceBuilder<PersistInstance> for Persist {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use rand::Rng;
     use std::str::FromStr;
-
-    fn get_range() -> usize {
-        let mut rng = rand::thread_rng();
-        rng.gen_range(1..=20) as usize
-    }
 
     #[test]
     fn test_save_and_load() {
@@ -190,78 +159,85 @@ mod tests {
     }
 
     #[test]
-    fn test_list() {
+    fn test_list_and_size() {
         let persist = PersistInstance {
-            service_name: ServiceName::from_str("test_list").unwrap(),
+            service_name: ServiceName::from_str("test1").unwrap(),
         };
-        let list_length = get_range();
-        for list_key in 1..=list_length {
-            let key_name = format!("list_key_{}", list_key);
-            let key_value = format!("list_key_value_{}", list_key);
-            persist.save(&key_name, &key_value).unwrap();
-        }
 
-        let length = persist.list().unwrap().len();
-        assert_eq!(length, list_length);
-        persist.clear().unwrap();
+        persist.save("test", "test").unwrap();
+        let list_result = persist.list().unwrap().len();
+        let size_result = persist.size().unwrap();
+        assert_eq!(list_result, 1);
+        assert_eq!(size_result, 1);
     }
 
     #[test]
-    fn test_clear() {
+    fn test_list_error() {
         let persist = PersistInstance {
-            service_name: ServiceName::from_str("test_clear").unwrap(),
+            service_name: ServiceName::from_str("test2").unwrap(),
         };
 
-        let list_length = get_range();
-        for clear_key in 1..=list_length {
-            let key_name = format!("clear_key_{}", clear_key);
-            let key_value = format!("clear_key_value_{}", clear_key);
-            persist.save(&key_name, &key_value).unwrap();
-        }
-        persist.clear().unwrap();
-        let actual_length = persist.list().unwrap().len();
-        assert_eq!(actual_length, 0);
+        // unwrap error
+        let result = persist.list().unwrap_err();
+        assert_eq!(
+            result.to_string(),
+            "failed to list contents of folder: No such file or directory (os error 2)"
+        );
     }
 
     #[test]
     fn test_remove() {
         let persist = PersistInstance {
-            service_name: ServiceName::from_str("test_remove").unwrap(),
+            service_name: ServiceName::from_str("test3").unwrap(),
         };
 
-        let list_length = get_range();
-        for remove_key in 1..=list_length {
-            let key_name = format!("remove_key_{}", remove_key);
-            let key_value = format!("remove_key_value_{}", remove_key);
-            persist.save(&key_name, &key_value).unwrap();
-        }
-        persist
-            .remove(persist.list().unwrap()[list_length - 1].as_str())
-            .unwrap();
-        let actual_length = persist.list().unwrap().len();
-        assert_eq!(actual_length, list_length - 1);
-        persist.clear().unwrap();
+        persist.save("test", "test").unwrap();
+        persist.save("test2", "test2").unwrap();
+        let list = persist.list().unwrap();
+        let key = list[0].as_str();
+        persist.remove(key).unwrap();
+        let result = persist.list().unwrap();
+        assert_eq!(result.len(), 1);
     }
 
     #[test]
-    fn test_size() {
+    fn test_remove_error() {
         let persist = PersistInstance {
-            service_name: ServiceName::from_str("test_size").unwrap(),
+            service_name: ServiceName::from_str("test4").unwrap(),
         };
 
-        let mut expected_size = 0;
-        let list_length = get_range();
-        for size_key in 1..=list_length {
-            let key_name = format!("size_key_{}", size_key);
-            let key_value = format!("size_key_value_{}", size_key);
-            persist.save(&key_name, &key_value).unwrap();
-            expected_size += fs::metadata(persist.get_storage_file(&key_name))
-                .unwrap()
-                .len();
-        }
-        let actual_size = persist.size().unwrap();
-        assert_eq!(expected_size, actual_size);
+        // unwrap error
+        let result = persist.remove("test4").unwrap_err();
+        assert_eq!(
+            result.to_string(),
+            "failed to remove file: No such file or directory (os error 2)"
+        );
+    }
+
+    #[test]
+    fn test_clear() {
+        let persist = PersistInstance {
+            service_name: ServiceName::from_str("test5").unwrap(),
+        };
+
+        persist.save("test5", "test5").unwrap();
         persist.clear().unwrap();
+        let result = persist.list().unwrap();
+        assert_eq!(result.len(), 0);
+    }
+
+    #[test]
+    fn test_clear_error() {
+        let persist = PersistInstance {
+            service_name: ServiceName::from_str("test6").unwrap(),
+        };
+
+        // unwrap error
+        let result = persist.clear().unwrap_err();
+        assert_eq!(
+            result.to_string(),
+            "failed to clear folder: No such file or directory (os error 2)"
+        );
     }
 
     #[test]

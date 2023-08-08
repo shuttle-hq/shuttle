bed5e88b3af9f6f76aa72e1f40a52dbebf2ceb77
Iulian Barbu <iulianbarbu2@gmail.com>
2023-08-08 16:34:38 +0300

    proto: add back LogItem and LogLevel

--- proto/src/generated/runtime.rs
+++ proto/src/generated/runtime.rs
@@ -70,6 +70,24 @@ pub struct SubscribeStopResponse {
     #[prost(string, tag = "2")]
     pub message: ::prost::alloc::string::String,
 }
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct LogItem {
+    #[prost(message, optional, tag = "2")]
+    pub timestamp: ::core::option::Option<::prost_types::Timestamp>,
+    #[prost(enumeration = "LogLevel", tag = "4")]
+    pub level: i32,
+    #[prost(string, optional, tag = "5")]
+    pub file: ::core::option::Option<::prost::alloc::string::String>,
+    #[prost(uint32, optional, tag = "6")]
+    pub line: ::core::option::Option<u32>,
+    #[prost(string, tag = "7")]
+    pub target: ::prost::alloc::string::String,
+    #[prost(bytes = "vec", tag = "8")]
+    pub fields: ::prost::alloc::vec::Vec<u8>,
+    #[prost(string, tag = "9")]
+    pub state: ::prost::alloc::string::String,
+}
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
 #[repr(i32)]
 pub enum StopReason {
@@ -98,6 +116,41 @@ impl StopReason {
             "Request" => Some(Self::Request),
             "End" => Some(Self::End),
             "Crash" => Some(Self::Crash),
+            _ => None,
+        }
+    }
+}
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+#[repr(i32)]
+pub enum LogLevel {
+    Trace = 0,
+    Debug = 1,
+    Info = 2,
+    Warn = 3,
+    Error = 4,
+}
+impl LogLevel {
+    /// String value of the enum field names used in the ProtoBuf definition.
+    ///
+    /// The values are not transformed in any way and thus are considered stable
+    /// (if the ProtoBuf definition does not change) and safe for programmatic use.
+    pub fn as_str_name(&self) -> &'static str {
+        match self {
+            LogLevel::Trace => "Trace",
+            LogLevel::Debug => "Debug",
+            LogLevel::Info => "Info",
+            LogLevel::Warn => "Warn",
+            LogLevel::Error => "Error",
+        }
+    }
+    /// Creates an enum from field names used in the ProtoBuf definition.
+    pub fn from_str_name(value: &str) -> ::core::option::Option<Self> {
+        match value {
+            "Trace" => Some(Self::Trace),
+            "Debug" => Some(Self::Debug),
+            "Info" => Some(Self::Info),
+            "Warn" => Some(Self::Warn),
+            "Error" => Some(Self::Error),
             _ => None,
         }
     }

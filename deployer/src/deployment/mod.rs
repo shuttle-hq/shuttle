490e256c92c9bd209c1f7858f3c9ba01513e786b
oddgrd <29732646+oddgrd@users.noreply.github.com>
2023-08-31 14:18:34 +0200

    refactor(logger): revert switch to native-tls

--- deployer/src/deployment/mod.rs
+++ deployer/src/deployment/mod.rs
@@ -196,7 +196,6 @@ pub struct DeploymentManager {
     _join_set: Arc<Mutex<JoinSet<()>>>,
 }
 
-// let provisioner_client = ProvisionerClient::new(channel);
 /// ```no-test
 /// queue channel   all deployments here are State::Queued until the get a slot from gateway
 ///       |

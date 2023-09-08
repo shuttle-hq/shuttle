a7d0ee000350e3ffbd988883b55675e89df57258
Iulian Barbu <14218860+iulianbarbu@users.noreply.github.com>
2023-09-08 10:19:06 +0300

    refactor(runtime,codegen): avoid double timestamps problem (#1210)
    * refactor(codegen): create a fmt subscriber without time
    
    * refactor(runtime): emit resource tracker logs without timestmap

--- codegen/src/shuttle_main/mod.rs
+++ codegen/src/shuttle_main/mod.rs
@@ -16,8 +16,10 @@ pub(crate) fn r#impl(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let tracing_setup = if cfg!(feature = "setup-tracing") {
         Some(quote! {
                 use shuttle_runtime::colored::*;
+                use shuttle_runtime::tracing_subscriber::{registry, fmt, prelude::*};
                 shuttle_runtime::colored::control::set_override(true);
-                shuttle_runtime::tracing_subscriber::fmt::init();
+                registry().with(fmt::layer().without_time()).init();
+
                 println!(
                     "{}\n{}\nTo disable tracing, remove the default features from {}:\n{}\n{}",
                     "Shuttle's default tracing subscriber is initialized!".yellow().bold(),

62fe595a407052766b87d75f0a96ea43d9946daf
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2023-09-27 13:24:34 +0200

    clraify

--- resources/static-folder/src/lib.rs
+++ resources/static-folder/src/lib.rs
@@ -7,7 +7,7 @@ use shuttle_service::{Error, Factory, ResourceBuilder, Type};
 #[derive(Serialize)]
 #[deprecated(
     since = "0.27.0",
-    note = "Folder names can now be hard coded. https://docs.shuttle.rs/configuration/files"
+    note = "Folder names can now be hard coded. More about deployment files: https://docs.shuttle.rs/configuration/files"
 )]
 pub struct StaticFolder<'a> {
     /// The folder to reach at runtime. Defaults to `static`

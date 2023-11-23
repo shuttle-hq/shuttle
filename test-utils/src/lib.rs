1055b2f41da00631b865bf2bb5f7e1d4791fc8ea
Iulian Barbu <iulianbarbu2@gmail.com>
2023-11-23 19:07:25 +0200

    test-utils: added crate for testing utilities

---
+++ test-utils/src/lib.rs
@@ -0,0 +1,150 @@
+use portpicker::pick_unused_port;
+use rand::{distributions::Alphanumeric, Rng};
+use std::{
+    process::Command,
+    thread::sleep,
+    time::{Duration, SystemTime},
+};
+
+/// A docker instance for a postgres database. It should be used
+/// by tests as a singleton (e.g. once_cell::sync::Lazy), and any
+/// test logic that connects to it should separate cases by creating
+/// an unique database. Also, the instance should implement a destructor.
+///
+/// Example usage:
+///
+/// static PG: Lazy<PostgresDockerInstance> = Lazy::new(PostgresDockerInstance::default);
+
+/// #[dtor]
+/// fn cleanup() {
+///    PG.cleanup();
+/// }
+///
+///  #[tokio::test]
+/// async fn test_case() {
+///     // Create a unique database name so we have a new database for each test.
+///     let db_name = Uuid::new_v4().to_string();
+///     let db_uri = format!("{}/{}", PG.uri, db_name);
+///     
+///     // Test logic below, which can use `db_uri` to connect to the postgres instance.
+/// }
+///
+pub struct PostgresDockerInstance {
+    container_name: String,
+    base_uri: String,
+}
+
+impl Default for PostgresDockerInstance {
+    fn default() -> Self {
+        let s: String = rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(7)
+            .map(char::from)
+            .collect();
+
+        PostgresDockerInstance::new(s.as_str())
+    }
+}
+
+impl PostgresDockerInstance {
+    /// Create a new postgres docker instance.
+    pub fn new(name: &str) -> Self {
+        let container_name = format!("shuttle_test_pg_{}", name);
+        let engine = "postgres";
+        let env = ["POSTGRES_PASSWORD=password", "PGUSER=postgres"];
+        let port = "5432";
+        let image = "docker.io/library/postgres:15";
+        let is_ready_cmd = vec!["exec", container_name.as_str(), "pg_isready"];
+        let host_port = pick_unused_port().unwrap();
+        let port_binding = format!("{}:{}", host_port, port);
+
+        let mut args = vec![
+            "run",
+            "--rm",
+            "--name",
+            container_name.as_str(),
+            "-p",
+            &port_binding,
+        ];
+
+        args.extend(env.iter().flat_map(|e| ["-e", e]));
+
+        args.push(image);
+
+        Command::new("docker").args(args).spawn().unwrap();
+
+        Self::_wait_ready(Duration::from_secs(120), &is_ready_cmd);
+
+        // The container enters the ready state and then reboots, sleep a little and then
+        // check if it's ready again afterwards.
+        sleep(Duration::from_millis(350));
+        Self::_wait_ready(Duration::from_secs(120), &is_ready_cmd);
+
+        Self {
+            container_name,
+            base_uri: format!("{engine}://{engine}:password@localhost:{host_port}"),
+        }
+    }
+
+    fn _wait_ready(mut timeout: Duration, is_ready_cmd: &[&str]) {
+        let mut now = SystemTime::now();
+        while !timeout.is_zero() {
+            let status = Command::new("docker")
+                .args(is_ready_cmd)
+                .output()
+                .unwrap()
+                .status;
+
+            if status.success() {
+                println!("{is_ready_cmd:?} succeeded...");
+                return;
+            }
+
+            println!("{is_ready_cmd:?} did not succeed this time...");
+            sleep(Duration::from_millis(350));
+
+            timeout = timeout
+                .checked_sub(now.elapsed().unwrap())
+                .unwrap_or_default();
+            now = SystemTime::now();
+        }
+        panic!("timed out while waiting for provisioner DB to come up");
+    }
+
+    // Remove the docker container.
+    pub fn cleanup(&self) {
+        Command::new("docker")
+            .args(["stop", self.container_name.as_str()])
+            .output()
+            .expect("failed to stop provisioner test DB container");
+        Command::new("docker")
+            .args(["rm", self.container_name.as_str()])
+            .output()
+            .expect("failed to remove provisioner test DB container");
+    }
+
+    /// This endpoint should be used to get a unique connection string from
+    /// the docker instance, so that the instance can be used by multiple
+    /// clients in parallel, accessing different
+    pub fn get_unique_uri(&self, db_name: &str) -> String {
+        // Get the PG uri first so the static PG is initialized.
+        self.exec_psql(&format!(r#"CREATE DATABASE "{}";"#, &db_name));
+        format!("{}/{}", &self.base_uri, db_name)
+    }
+
+    // Execute queries in `psql` via `docker exec`
+    fn exec_psql(&self, query: &str) -> String {
+        let args = [
+            "exec",
+            self.container_name.as_str(),
+            "psql",
+            "--username",
+            "postgres",
+            "--command",
+            query,
+        ];
+        let output = Command::new("docker").args(args).output().unwrap().stdout;
+
+        String::from_utf8(output).unwrap().trim().to_string()
+    }
+}

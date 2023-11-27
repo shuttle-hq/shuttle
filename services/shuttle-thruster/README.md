cefe619af8a3b64ae371505e14ae7ba08edf2712
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2023-11-27 07:09:37 +0530

    add: crate docs in readme

---
+++ services/shuttle-thruster/README.md
@@ -0,0 +1,25 @@
+## Shuttle service integration for the Thruster web framework.
+
+### Example
+
+```rust,no_run
+use thruster::{
+    context::basic_hyper_context::{generate_context, BasicHyperContext as Ctx, HyperRequest},
+    m, middleware_fn, App, HyperServer, MiddlewareNext, MiddlewareResult, ThrusterServer,
+};
+
+#[middleware_fn]
+async fn hello(mut context: Ctx, _next: MiddlewareNext<Ctx>) -> MiddlewareResult<Ctx> {
+    context.body("Hello, World!");
+    Ok(context)
+}
+
+#[shuttle_runtime::main]
+async fn thruster() -> shuttle_thruster::ShuttleThruster<HyperServer<Ctx, ()>> {
+    let server = HyperServer::new(
+        App::<HyperRequest, Ctx, ()>::create(generate_context, ()).get("/", m![hello]),
+    );
+
+    Ok(server.into())
+}
+```
--- services/shuttle-thruster/src/lib.rs
+++ services/shuttle-thruster/src/lib.rs
@@ -1,28 +1,4 @@
-//! Shuttle service integration for the Thruster web framework.
-//!
-//! ## Example
-//!
-//! ```rust,no_run
-//! use thruster::{
-//!     context::basic_hyper_context::{generate_context, BasicHyperContext as Ctx, HyperRequest},
-//!     m, middleware_fn, App, HyperServer, MiddlewareNext, MiddlewareResult, ThrusterServer,
-//! };
-//!
-//! #[middleware_fn]
-//! async fn hello(mut context: Ctx, _next: MiddlewareNext<Ctx>) -> MiddlewareResult<Ctx> {
-//!     context.body("Hello, World!");
-//!     Ok(context)
-//! }
-//!
-//! #[shuttle_runtime::main]
-//! async fn thruster() -> shuttle_thruster::ShuttleThruster<HyperServer<Ctx, ()>> {
-//!     let server = HyperServer::new(
-//!         App::<HyperRequest, Ctx, ()>::create(generate_context, ()).get("/", m![hello]),
-//!     );
-//!
-//!     Ok(server.into())
-//! }
-//! ```
+#![doc = include_str!("../README.md")]
 use shuttle_runtime::Error;
 use std::net::SocketAddr;
 
@@ -52,28 +28,5 @@ where
     }
 }
 
-/// The return type of the [shuttle_runtime::main] function for the Thruster service.
-///
-/// ## Example
-///
-/// ```rust,no_run
-/// use shuttle_thruster::ShuttleThruster;
-/// use thruster::{
-///     context::basic_hyper_context::{generate_context, BasicHyperContext as Ctx, HyperRequest},
-///     m, middleware_fn, App, HyperServer, MiddlewareNext, MiddlewareResult, ThrusterServer,
-/// };
-///
-/// #[middleware_fn]
-/// async fn hello(mut context: Ctx, _next: MiddlewareNext<Ctx>) -> MiddlewareResult<Ctx> {
-///     context.body("Hello, World!");
-///     Ok(context)
-/// }
-///
-/// #[shuttle_runtime::main]
-/// async fn thruster() -> ShuttleThruster<HyperServer<Ctx, ()>> {
-///     Ok(HyperServer::new(
-///         App::<HyperRequest, Ctx, ()>::create(generate_context, ()).get("/", m![hello]),
-///     ).into())
-/// }
-/// ```
+#[doc = include_str!("../README.md")]
 pub type ShuttleThruster<T> = Result<ThrusterService<T>, Error>;

168b1efbfeae41401488474ec7646049f48b3d24
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2023-11-29 00:55:58 +0100

    feat(shuttle-axum): support axum 0.7 through feature flag

--- services/shuttle-axum/src/lib.rs
+++ services/shuttle-axum/src/lib.rs
@@ -19,25 +19,40 @@
 use shuttle_runtime::{CustomError, Error};
 use std::net::SocketAddr;
 
+#[cfg(feature = "axum")]
+use axum::Router;
+#[cfg(feature = "axum-0-7")]
+use axum_0_7::Router;
+
 /// A wrapper type for [axum::Router] so we can implement [shuttle_runtime::Service] for it.
-pub struct AxumService(pub axum::Router);
+pub struct AxumService(pub Router);
 
 #[shuttle_runtime::async_trait]
 impl shuttle_runtime::Service for AxumService {
     /// Takes the router that is returned by the user in their [shuttle_runtime::main] function
     /// and binds to an address passed in by shuttle.
     async fn bind(mut self, addr: SocketAddr) -> Result<(), Error> {
+        #[cfg(feature = "axum")]
         axum::Server::bind(&addr)
             .serve(self.0.into_make_service())
             .await
             .map_err(CustomError::new)?;
+        #[cfg(feature = "axum-0-7")]
+        axum_0_7::serve(
+            shuttle_runtime::tokio::net::TcpListener::bind(addr)
+                .await
+                .map_err(CustomError::new)?,
+            self.0,
+        )
+        .await
+        .map_err(CustomError::new)?;
 
         Ok(())
     }
 }
 
-impl From<axum::Router> for AxumService {
-    fn from(router: axum::Router) -> Self {
+impl From<Router> for AxumService {
+    fn from(router: Router) -> Self {
         Self(router)
     }
 }

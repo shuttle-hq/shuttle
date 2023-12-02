b9c7cdbc4b056e83f39ad1cddf2358784a6cf3bc
Jocelyn Boullier <jocelyn@boullier.bzh>
2023-12-02 12:42:39 +0100

    feat(gateway): attempt at adding back some kind of retry

--- gateway/src/task.rs
+++ gateway/src/task.rs
@@ -295,7 +295,7 @@ impl Task<ProjectContext> for RunUntilDone {
     async fn poll(&mut self, ctx: ProjectContext) -> TaskResult<Self::Output, Self::Error> {
         // Make sure the project state has not changed from Docker
         // Else we will make assumptions when trying to run next which can cause a failure
-        let project = match ctx.state.refresh(&ctx.gateway).await {
+        let project = match refresh_with_retry(ctx.state, &ctx.gateway).await {
             Ok(project) => project,
             Err(error) => return TaskResult::Err(error),
         };
@@ -329,7 +329,7 @@ impl Task<ProjectContext> for DeleteProject {
     async fn poll(&mut self, ctx: ProjectContext) -> TaskResult<Self::Output, Self::Error> {
         // Make sure the project state has not changed from Docker
         // Else we will make assumptions when trying to run next which can cause a failure
-        let project = match ctx.state.refresh(&ctx.gateway).await {
+        let project = match refresh_with_retry(ctx.state, &ctx.gateway).await {
             Ok(project) => project,
             Err(error) => return TaskResult::Err(error),
         };

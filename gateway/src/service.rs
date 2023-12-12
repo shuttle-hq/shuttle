8059406b33cbb2c59100ea71fc1711cc8b5971d4
Bailey Townsend <baileytownsend2323@gmail.com>
2023-12-12 07:04:50 -0600

    Update gateway/src/service.rs
    Co-authored-by: Jocelyn Boullier <jocelyn@boullier.bzh>

--- gateway/src/service.rs
+++ gateway/src/service.rs
@@ -1041,7 +1041,7 @@ impl DockerContext for GatewayContext {
         &self.settings
     }
 
-    #[instrument(name = "getting container stats from the proper source", skip_all, fields(docker_stats_source = %self.docker_stats_source, shuttle.container.id = container_id))]
+    #[instrument(name = "get container stats", skip_all, fields(docker_stats_source = %self.docker_stats_source, shuttle.container.id = container_id))]
     async fn get_stats(&self, container_id: &str) -> Result<u64, Error> {
         match self.docker_stats_source {
             DockerStatsSource::CgroupV1 => {

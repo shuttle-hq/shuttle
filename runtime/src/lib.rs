fa0fce6eb44218551ef71e7fe3a2fa67b03009f1
Iulian Barbu <14218860+iulianbarbu@users.noreply.github.com>
2023-12-13 10:15:29 +0200

    chore: v0.35.1 (#1480)
    * chore: v0.35.1
    
    * gateway: fix comment
    
    * examples: change back to main commit
    
    * chore(changelog): updated for 0.35.1

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -28,8 +28,8 @@
 //!
 //! ```toml
 //! axum = "0.6.20"
-//! shuttle-axum = "0.35.0"
-//! shuttle-runtime = "0.35.0"
+//! shuttle-axum = "0.35.1"
+//! shuttle-runtime = "0.35.1"
 //! tokio = "1.28.2"
 //! ```
 //!
@@ -112,7 +112,7 @@
 //! `runtime-tokio-native-tls` and `postgres` features inside `Cargo.toml`:
 //!
 //! ```toml
-//! shuttle-shared-db = { version = "0.35.0", features = ["postgres"] }
+//! shuttle-shared-db = { version = "0.35.1", features = ["postgres"] }
 //! sqlx = "0.7.1"
 //! ```
 //!

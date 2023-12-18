44381debceb63ddae39ca3dd1b109ab325c2b278
oddgrd <29732646+oddgrd@users.noreply.github.com>
2023-12-18 12:44:23 +0100

    refactor(auth): make price_id fetching env dependent

--- auth/src/subscription.rs
+++ auth/src/subscription.rs
@@ -1,18 +1,31 @@
+use shuttle_common::backends::subscription::SubscriptionItem;
+
 pub mod stripe_price_ids {
-    /// The price ID of the recurring AWS RDS instance product.
     /// TODO: this is set to the id of the test product, set to real product before releasing.
-    pub const AWS_RDS_INSTANCE_RECURRING: &str = "price_1OIS06FrN7EDaGOjaV0GXD7P";
+    pub const PRODUCTION_AWS_RDS_INSTANCE_RECURRING: &str = "TODO: change to prod ID";
+    /// The price ID of the recurring AWS RDS instance product on staging.
+    pub const STAGING_AWS_RDS_INSTANCE_RECURRING: &str = "price_1OIS06FrN7EDaGOjaV0GXD7P";
 }
 
 pub trait SubscriptionItemExt {
     fn price_id(&self) -> String;
 }
 
-impl SubscriptionItemExt for shuttle_common::backends::subscription::SubscriptionItem {
+impl SubscriptionItemExt for SubscriptionItem {
     fn price_id(&self) -> String {
-        match self {
-            shuttle_common::backends::subscription::SubscriptionItem::AwsRds => {
-                stripe_price_ids::AWS_RDS_INSTANCE_RECURRING.to_string()
+        let is_production = std::env::var("SHUTTLE_ENV").is_ok_and(|env| env == "production");
+
+        if is_production {
+            match self {
+                SubscriptionItem::AwsRds => {
+                    stripe_price_ids::PRODUCTION_AWS_RDS_INSTANCE_RECURRING.to_string()
+                }
+            }
+        } else {
+            match self {
+                SubscriptionItem::AwsRds => {
+                    stripe_price_ids::STAGING_AWS_RDS_INSTANCE_RECURRING.to_string()
+                }
             }
         }
     }

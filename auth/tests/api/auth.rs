2bcdc661a0b2f94da1a04056ae9d3f5e918231f0
Iulian Barbu <iulianbarbu2@gmail.com>
2023-12-20 10:05:46 +0200

    auth: simplify tests

--- auth/tests/api/auth.rs
+++ auth/tests/api/auth.rs
@@ -5,7 +5,7 @@ mod needs_docker {
     use serde_json::Value;
     use shuttle_common::claims::AccountTier;
 
-    use crate::helpers::app;
+    use crate::helpers::{app, ADMIN_KEY};
 
     #[tokio::test]
     async fn convert_api_key_to_jwt() {
@@ -18,14 +18,6 @@ mod needs_docker {
         let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
         let user: Value = serde_json::from_slice(&body).unwrap();
         let basic_user_key = user["key"].as_str().unwrap();
-
-        // Create test admin user
-        let response = app.post_user("test-user-admin", "admin").await;
-        assert_eq!(response.status(), StatusCode::OK);
-        // Extract the API key from the response so we can use it in a future request.
-        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
-        let user: Value = serde_json::from_slice(&body).unwrap();
-        let admin_user_key = user["key"].as_str().unwrap();
 
         // GET /auth/key without bearer token.
         let request = Request::builder()
@@ -45,14 +37,14 @@ mod needs_docker {
         assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
 
         // GET /auth/key with an admin user key.
-        let response = app.get_jwt_from_api_key(admin_user_key).await;
+        let response = app.get_jwt_from_api_key(ADMIN_KEY).await;
         assert_eq!(response.status(), StatusCode::OK);
 
         // Decode the JWT into a Claim.
         let claim = app.claim_from_response(response).await;
 
         // Verify the claim subject and tier matches the test user we created at the start of the test.
-        assert_eq!(claim.sub, "test-user-admin");
+        assert_eq!(claim.sub, "admin");
         assert_eq!(claim.tier, AccountTier::Admin);
         assert_eq!(claim.limits.project_limit(), 3);
 
@@ -64,7 +56,7 @@ mod needs_docker {
 
         // GET /auth/key with an admin user key that has an XShuttleAdminSecret header with a basic user key.
         let response = app
-            .get_jwt_from_non_admin_api_key(admin_user_key, basic_user_key)
+            .get_jwt_from_non_admin_api_key(ADMIN_KEY, basic_user_key)
             .await;
         assert_eq!(response.status(), StatusCode::OK);
 
@@ -72,13 +64,13 @@ mod needs_docker {
         let claim = app.claim_from_response(response).await;
 
         // Verify the claim subject and tier matches the test user we created at the start of the test.
-        assert_eq!(claim.sub, "test-user-admin");
+        assert_eq!(claim.sub, "admin");
         assert_eq!(claim.tier, AccountTier::Admin);
         assert_eq!(claim.limits.project_limit(), 3);
 
         // GET /auth/key with a basic user key that has an XShuttleAdminSecret header with an admin user key.
         let response = app
-            .get_jwt_from_non_admin_api_key(basic_user_key, admin_user_key)
+            .get_jwt_from_non_admin_api_key(basic_user_key, ADMIN_KEY)
             .await;
         assert_eq!(response.status(), StatusCode::OK);
 

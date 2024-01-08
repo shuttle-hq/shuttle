afeea5b2dae5bd35f9ed89d8377f4bd9fe6c849f
oddgrd <29732646+oddgrd@users.noreply.github.com>
2024-01-08 18:36:11 +0100

    chore: v0.36.0

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -28,8 +28,8 @@
 //!
 //! ```toml
 //! axum = "0.7.3"
-//! shuttle-axum = "0.35.1"
-//! shuttle-runtime = "0.35.1"
+//! shuttle-axum = "0.36.0"
+//! shuttle-runtime = "0.36.0"
 //! tokio = "1.28.2"
 //! ```
 //!
@@ -112,7 +112,7 @@
 //! `runtime-tokio-native-tls` and `postgres` features inside `Cargo.toml`:
 //!
 //! ```toml
-//! shuttle-shared-db = { version = "0.35.1", features = ["postgres"] }
+//! shuttle-shared-db = { version = "0.36.0", features = ["postgres"] }
 //! sqlx = "0.7.1"
 //! ```
 //!

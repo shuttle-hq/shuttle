b8789b5469f715198327642128de164ceba3a48b
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2024-01-23 16:24:50 +0100

    chore(shuttle-poise): deprecate shuttle-poise

--- services/shuttle-poise/README.md
+++ services/shuttle-poise/README.md
@@ -1,56 +1,5 @@
-## Shuttle service integration for the Poise discord bot framework.
+## Shuttle service integration for the Poise Discord bot framework
 
-Poise 0.6 is used by default.
+**This plugin is deprecated.**
 
-Poise 0.5 is supported by using these feature flags:
-
-```toml,ignore
-poise = { version = "0.5.7", features = ["..."] }
-shuttle-poise = { version = "0.36.0", default-features = false, features = ["poise-0-5"] }
-```
-
-### Example
-
-```rust,ignore
-use poise::serenity_prelude as serenity;
-use shuttle_secrets::SecretStore;
-use shuttle_poise::ShuttlePoise;
-
-struct Data {} // User data, which is stored and accessible in all command invocations
-type Error = Box<dyn std::error::Error + Send + Sync>;
-type Context<'a> = poise::Context<'a, Data, Error>;
-
-/// Responds with "world!"
-#[poise::command(slash_command)]
-async fn hello(ctx: Context<'_>) -> Result<(), Error> {
-    ctx.say("world!").await?;
-    Ok(())
-}
-
-#[shuttle_runtime::main]
-async fn poise(#[shuttle_secrets::Secrets] secret_store: SecretStore) -> ShuttlePoise<Data, Error> {
-    // Get the discord token set in `Secrets.toml`
-    let discord_token = secret_store
-        .get("DISCORD_TOKEN")
-        .expect("'DISCORD_TOKEN' was not found");
-
-    let framework = poise::Framework::builder()
-        .options(poise::FrameworkOptions {
-            commands: vec![hello()],
-            ..Default::default()
-        })
-        .token(discord_token)
-        .intents(serenity::GatewayIntents::non_privileged())
-        .setup(|ctx, _ready, framework| {
-            Box::pin(async move {
-                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                Ok(Data {})
-            })
-        })
-        .build()
-        .await
-        .map_err(shuttle_runtime::CustomError::new)?;
-
-    Ok(framework.into())
-}
-```
+Poise >=0.6 can now be used together with [shuttle-serenity](https://docs.rs/shuttle-serenity/latest/shuttle_serenity/).
--- services/shuttle-poise/src/lib.rs
+++ services/shuttle-poise/src/lib.rs
@@ -1,20 +1,10 @@
 #![doc = include_str!("../README.md")]
-#[cfg(feature = "poise")]
-use poise::serenity_prelude as serenity;
 use std::net::SocketAddr;
-#[cfg(feature = "poise-0-5")]
 use std::sync::Arc;
 
 /// A wrapper type for [poise::Framework] so we can implement [shuttle_runtime::Service] for it.
-#[cfg(feature = "poise")]
-pub struct PoiseService<T, E> {
-    pub framework: poise::Framework<T, E>,
-    pub token: String,
-    pub intents: serenity::GatewayIntents,
-}
-/// A wrapper type for [poise::Framework] so we can implement [shuttle_runtime::Service] for it.
-#[cfg(feature = "poise-0-5")]
-pub struct PoiseService<T, E>(pub Arc<poise_0_5::Framework<T, E>>);
+#[deprecated = "shuttle-poise for poise 0.5 is deprecated. To use poise 0.6 and Shuttle, check https://docs.rs/shuttle-serenity/latest/shuttle_serenity/"]
+pub struct PoiseService<T, E>(pub Arc<poise::Framework<T, E>>);
 
 #[shuttle_runtime::async_trait]
 impl<T, E> shuttle_runtime::Service for PoiseService<T, E>
@@ -23,16 +13,6 @@ where
     E: Send + Sync + 'static,
 {
     async fn bind(mut self, _addr: SocketAddr) -> Result<(), shuttle_runtime::Error> {
-        #[cfg(feature = "poise")]
-        serenity::ClientBuilder::new(self.token, self.intents)
-            .framework(self.framework)
-            .await
-            .map_err(shuttle_runtime::CustomError::new)?
-            .start_autosharded()
-            .await
-            .map_err(shuttle_runtime::CustomError::new)?;
-
-        #[cfg(feature = "poise-0-5")]
         self.0
             .start_autosharded()
             .await
@@ -42,22 +22,8 @@ where
     }
 }
 
-#[cfg(feature = "poise")]
-impl<T, E> From<(poise::Framework<T, E>, String, serenity::GatewayIntents)> for PoiseService<T, E> {
-    fn from(
-        (framework, token, intents): (poise::Framework<T, E>, String, serenity::GatewayIntents),
-    ) -> Self {
-        Self {
-            framework,
-            token,
-            intents,
-        }
-    }
-}
-
-#[cfg(feature = "poise-0-5")]
-impl<T, E> From<Arc<poise_0_5::Framework<T, E>>> for PoiseService<T, E> {
-    fn from(framework: Arc<poise_0_5::Framework<T, E>>) -> Self {
+impl<T, E> From<Arc<poise::Framework<T, E>>> for PoiseService<T, E> {
+    fn from(framework: Arc<poise::Framework<T, E>>) -> Self {
         Self(framework)
     }
 }

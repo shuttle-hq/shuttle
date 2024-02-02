a51c70c8ea607d7c3c2291bf12396a168dc97ba0
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2024-02-02 12:09:30 +0100

    misc: add cargo-make tasks for some maintenance and ci tasks (#1595)
    * wip: use cargo-make for tasks and ci
    
    * fix: unzip
    
    * feat: cargo make proto
    
    * feat: cargo make audit
    
    * cliff version
    
    * move
    
    * cmt tag
    
    * ci: add cargo make to integration

--- proto/README.md
+++ proto/README.md
@@ -1,13 +1,6 @@
 # shuttle-proto
 
-This crate contains the protofiles used to generate the provisioner and runtime services,
-as well as the generated code.
+This crate contains the protofiles used to generate code for the gRPC APIs.
 Having the generated files commited means users don't need protoc installed to install `cargo-shuttle`.
 
-If you make changes to the protofiles, run
-
-```bash
-./proto/integration.sh generate
-```
-
-to regenerate the generated files in `proto/src/generated`.
+If you make changes to the protofiles, run `cargo make proto` to generate new files.
--- proto/integration.sh
+++ proto/integration.sh
@@ -1,23 +1,3 @@
 #!/usr/bin/env bash
 
-if ! which proto-gen &>/dev/null; then
-    echo "Need 'proto-gen' to generate/validate protobufs:"
-    echo "cargo (b)install proto-gen"
-    exit 1
-fi
-
-OP="validate"
-if [ "$1" == "generate" ]; then
-    OP="generate"
-fi
-
-proto-gen \
-    --generate-transport --build-client --build-server --format \
-    ${OP} \
-    -d proto \
-    -o proto/src/generated \
-    -f proto/builder.proto \
-    -f proto/logger.proto \
-    -f proto/provisioner.proto \
-    -f proto/resource-recorder.proto \
-    -f proto/runtime.proto
+cargo make proto validate

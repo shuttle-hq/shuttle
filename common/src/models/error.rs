f1c51b14478344d854386eb398a4827354dbb904
biplab5464 <biplab5464@outlook.com>
2024-02-07 11:20:57 +0530

    formatting issue

--- common/src/models/error.rs
+++ common/src/models/error.rs
@@ -29,7 +29,6 @@ impl Display for ApiError {
     }
 }
 
-
 impl std::error::Error for ApiError {}
 
 #[derive(Debug, Clone, PartialEq, Error)]
@@ -111,7 +110,7 @@ impl From<ErrorKind> for ApiError {
             ErrorKind::ProjectHasRunningDeployment => StatusCode::INTERNAL_SERVER_ERROR,
             ErrorKind::ProjectHasBuildingDeployment => StatusCode::BAD_REQUEST,
             ErrorKind::ProjectCorrupted => StatusCode::BAD_REQUEST,
-            ErrorKind::ProjectHasResources(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            ErrorKind::ProjectHasResources() => StatusCode::INTERNAL_SERVER_ERROR,
             ErrorKind::InvalidProjectName(_) => StatusCode::BAD_REQUEST,
             ErrorKind::InvalidOperation => StatusCode::BAD_REQUEST,
             ErrorKind::ProjectAlreadyExists => StatusCode::BAD_REQUEST,

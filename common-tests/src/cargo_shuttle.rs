aa925211750b2491b988bd4767849a8b17e402a8
nopeNoshishi <noah.of.havegreat@icloud.com>
2024-03-02 15:19:57 +0900

    fix: common-tests

--- common-tests/src/cargo_shuttle.rs
+++ common-tests/src/cargo_shuttle.rs
@@ -30,6 +30,7 @@ pub async fn cargo_shuttle_run(working_directory: &str, external: bool) -> Strin
         port,
         external,
         release: false,
+        raw: false,
     };
 
     let runner = Shuttle::new().unwrap().run(

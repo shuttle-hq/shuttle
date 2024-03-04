0c577463ccf5af6328a09c5ff82c1f099e6dbda5
Fedor Tkachenko <38206129+Pulko@users.noreply.github.com>
2024-03-04 15:31:59 +0100

    feat(install): change powershell installation script (#1636)
    * feat(install): change powershell installation script
    
    * fix: declare variables before usage
    
    * fix: change order
    
    * refa: reorder variables

--- install.ps1
+++ install.ps1
@@ -1,7 +1,5 @@
 function Install-Cargo-Shuttle {
-	$RepoUrl = "https://github.com/shuttle-hq/shuttle"
-
-	Write-Host @"
+    Write-Host @"
      _           _   _   _
  ___| |__  _   _| |_| |_| | ___
 / __| '_ \| | | | __| __| |/ _ \
@@ -15,57 +13,11 @@ Please file an issue if you encounter any problems!
 ===================================================
 "@
 
-	if (Get-Command -CommandType Application -ErrorAction SilentlyContinue cargo-binstall.exe) {
-		Write-Host "Installing cargo-shuttle using cargo binstall"
-		cargo-binstall.exe cargo-shuttle --no-confirm
-		if ($?) {
-			Write-Host "Installed cargo-shuttle, try running ``cargo shuttle --help``" -ForegroundColor Green
-			return
-		}
-		else {
-			Write-Host "Could not install from release using cargo binstall, trying manual binary download" -ForegroundColor Red
-		}
-	}
-	else {
-		Write-Host "cargo binstall not found, trying manual binary download" -ForegroundColor Red
-	}
+    $Arch = [Environment]::GetEnvironmentVariable("PROCESSOR_ARCHITECTURE", [EnvironmentVariableTarget]::Machine)
+    $TempDir = $Env:TEMP
 
-	$CargoHome = if ($null -ne $Env:CARGO_HOME) { $Env:CARGO_HOME } else { "$HOME\.cargo" }
-	$TempDir = $Env:TEMP
-	$Arch = [Environment]::GetEnvironmentVariable("PROCESSOR_ARCHITECTURE", [EnvironmentVariableTarget]::Machine)
-	if (($Arch -eq "AMD64") -and (Get-Command -CommandType Application -ErrorAction SilentlyContinue tar.exe)) {
-	(Invoke-WebRequest "$RepoUrl/releases/latest" -Headers @{ "Accept" = "application/json" }).Content -match '"tag_name":"([^"]*)"' | Out-Null
-		$LatestRelease = $Matches.1
-		$BinaryUrl = "$RepoUrl/releases/download/$LatestRelease/cargo-shuttle-$LatestRelease-x86_64-pc-windows-msvc.tar.gz"
-		Invoke-WebRequest $BinaryUrl -OutFile "$TempDir\cargo-shuttle.tar.gz"
-		New-Item -ItemType Directory -Force "$TempDir\cargo-shuttle"
-		tar.exe -xzf "$TempDir\cargo-shuttle.tar.gz" -C "$TempDir\cargo-shuttle"
-		Move-Item -Force "$TempDir\cargo-shuttle\cargo-shuttle-x86_64-pc-windows-msvc-$LatestRelease\cargo-shuttle.exe" "$CargoHome\bin\cargo-shuttle.exe"
-		Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "$TempDir\cargo-shuttle.tar.gz", "$TempDir\cargo-shuttle"
-		Write-Host "Installed cargo-shuttle, try running ``cargo shuttle --help``" -ForegroundColor Green
-		return
-	}
-	elseif ($Arch -ne "AMD64") {
-		Write-Host "Unsupported Architecture: Binaries are not currently built for $Arch, skipping manual binary download" -ForegroundColor Red
-	}
-	else {
-		Write-Host "Could not find tar.exe, skipping manual binary download (required to extract the release asset)" -ForegroundColor Red
-	}
-
-	if (Get-Command -CommandType Application -ErrorAction SilentlyContinue cargo.exe) {
-		Write-Host "Installing cargo-shuttle using cargo install (from source)"
-		cargo.exe install cargo-shuttle --locked
-		if ($?) {
-			Write-Host "Installed cargo-shuttle, try running ``cargo shuttle --help``" -ForegroundColor Green
-			return
-		}
-		else {
-			Write-Host "Could not install cargo-shuttle using cargo" -ForegroundColor Red
-			return
-		}
-	}
-	else {
-		if ($Arch -in "AMD64", "x86") {
+    if (!(Get-Command -CommandType Application -ErrorAction SilentlyContinue cargo.exe)) {
+        if ($Arch -in "AMD64", "x86") {
 			Write-Host "Could not find cargo.exe, Rust may not be installed" -ForegroundColor Red
 			$Confirm = Read-Host -Prompt "Would you like to install Rust via Rustup? [y/N]"
 			if ($Confirm -inotin "y", "yes") {
@@ -92,8 +44,63 @@ Please file an issue if you encounter any problems!
 			Write-Host "Please install Rust manually, more info at: https://rust-lang.github.io/rustup/installation/other.html" -ForegroundColor Red
 			return
 		}
-	}
+    }
+
+    if (Get-Command -CommandType Application -ErrorAction SilentlyContinue cargo-binstall.exe) {
+        Write-Host "Installing cargo-shuttle using cargo binstall"
+        cargo-binstall.exe cargo-shuttle --no-confirm
+        if ($?) {
+            Write-Host "Installed cargo-shuttle, try running ``cargo shuttle --help``" -ForegroundColor Green
+            return
+        }
+        else {
+            Write-Host "Could not install from release using cargo binstall, trying manual binary download" -ForegroundColor Red
+        }
+    }
+    else {
+        Write-Host "Cargo binstall not found, trying manual binary download" -ForegroundColor Red
+    }
+
+    $RepoUrl = "https://github.com/shuttle-hq/shuttle"
+    $CargoHome = if ($null -ne $Env:CARGO_HOME) { $Env:CARGO_HOME } else { "$HOME\.cargo" }
+
+    if (($Arch -eq "AMD64") -and (Get-Command -CommandType Application -ErrorAction SilentlyContinue tar.exe)) {
+        (Invoke-WebRequest "$RepoUrl/releases/latest" -Headers @{ "Accept" = "application/json" }).Content -match '"tag_name":"([^"]*)"' | Out-Null
+        $LatestRelease = $Matches.1
+        $BinaryUrl = "$RepoUrl/releases/download/$LatestRelease/cargo-shuttle-$LatestRelease-x86_64-pc-windows-msvc.tar.gz"
+        Invoke-WebRequest $BinaryUrl -OutFile "$TempDir\cargo-shuttle.tar.gz"
+        New-Item -ItemType Directory -Force "$TempDir\cargo-shuttle"
+        tar.exe -xzf "$TempDir\cargo-shuttle.tar.gz" -C "$TempDir\cargo-shuttle"
+        Move-Item -Force "$TempDir\cargo-shuttle\cargo-shuttle-x86_64-pc-windows-msvc-$LatestRelease\cargo-shuttle.exe" "$CargoHome\bin\cargo-shuttle.exe"
+        Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "$TempDir\cargo-shuttle.tar.gz", "$TempDir\cargo-shuttle"
+        Write-Host "Installed cargo-shuttle, try running ``cargo shuttle --help``" -ForegroundColor Green
+        return
+    }
+    elseif ($Arch -ne "AMD64") {
+        Write-Host "Unsupported Architecture: Binaries are not currently built for $Arch, skipping manual binary download" -ForegroundColor Red
+    }
+    else {
+        Write-Host "Could not find tar.exe, skipping manual binary download (required to extract the release asset)" -ForegroundColor Red
+    }
+
+
+    if (Get-Command -CommandType Application -ErrorAction SilentlyContinue cargo.exe) {
+        Write-Host "Installing cargo-shuttle using cargo install (from source)"
+        cargo.exe install cargo-shuttle --locked
+        if ($?) {
+            Write-Host "Installed cargo-shuttle, try running ``cargo shuttle --help``" -ForegroundColor Green
+            return
+        }
+        else {
+            Write-Host "Could not install cargo-shuttle using cargo" -ForegroundColor Red
+            return
+        }
+    }
+    else {
+        Write-Host "Could not find cargo.exe, Rust may not be installed" -ForegroundColor Red
+    }
 }
+
 
 $OldErrorAction = $ErrorActionPreference
 $ErrorActionPreference = "Stop"

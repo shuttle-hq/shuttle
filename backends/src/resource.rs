68205b1db679d0d998ed4ccdab3894c255635505
oddgrd <29732646+oddgrd@users.noreply.github.com>
2024-04-08 21:07:25 +0200

    feat: add provisioner api models, ctl-secret header

--- backends/src/resource.rs
+++ backends/src/resource.rs
@@ -1,4 +1,22 @@
-#[derive(serde::Serialize, serde::Deserialize)]
+use serde::{Deserialize, Serialize};
+
+/// Used by the runner service to send requests to control plane, where the requested resources
+/// will be provisioned.
+#[derive(Serialize, Deserialize)]
 pub struct ResourceRequest {
+    /// The resource input returned from the runtime::load call.
     pub resources: Vec<Vec<u8>>,
+}
+
+/// Used to request a shared DB from the provisioner service.
+#[derive(Deserialize, Serialize)]
+pub struct SharedDbRequest {
+    pub project_id: String,
+}
+
+/// Used to request the deletion of a shared DB from the provisioner service.
+#[derive(Deserialize, Serialize)]
+pub struct SharedDbDeletionRequest {
+    pub db_name: String,
+    pub role_name: String,
 }

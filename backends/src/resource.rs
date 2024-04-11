d09d8036c7374d009d77f0bfb3ad19f706c1d081
Oddbjørn Grødem <29732646+oddgrd@users.noreply.github.com>
2024-04-11 10:17:38 +0200

    feat: add models and header for new ecs provisioner (#1730)

--- backends/src/resource.rs
+++ backends/src/resource.rs
@@ -1,4 +1,16 @@
-#[derive(serde::Serialize, serde::Deserialize)]
+use serde::{Deserialize, Serialize};
+
+/// Used by the runner service to send requests to control plane, where the requested resources
+/// will be provisioned.
+#[derive(Serialize, Deserialize)]
 pub struct ResourceRequest {
+    /// The resource input returned from the runtime::load call.
     pub resources: Vec<Vec<u8>>,
+}
+
+/// Used to request the provisioning or deletion of a shared DB from the provisioner service.
+#[derive(Deserialize, Serialize)]
+pub struct SharedDbRequest {
+    pub db_name: String,
+    pub role_name: String,
 }

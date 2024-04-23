b52786b96e8ecee7bf6ea3bfd67a17e832346618
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2024-04-23 12:02:54 +0200

    fix(cargo-shuttle): skip org project listing on beta

--- cargo-shuttle/src/lib.rs
+++ cargo-shuttle/src/lib.rs
@@ -2101,6 +2101,11 @@ impl Shuttle {
         println!("{}", "Personal Projects".bold());
         println!("{projects_table}");
 
+        if self.beta {
+            println!("Not listing org projects (not implemented yet on beta)");
+            return Ok(CommandOutcome::Ok);
+        }
+
         let orgs = client.get_organizations_list().await?;
 
         for org in orgs {

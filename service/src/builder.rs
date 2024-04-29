89982aa05e892639376b4b6f6a22452bdcc59731
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2024-04-29 11:23:39 +0200

    poc: shuttle feature flag

--- service/src/builder.rs
+++ service/src/builder.rs
@@ -223,7 +223,23 @@ async fn compile(
         cmd.arg("--jobs=4");
     }
 
+    // TODO: Compile only one package.
+    // (later: only one binary target)
+    // (both package/binary flag should be configurable in Shuttle.toml, but the default should be auto discovered)
+    // (These flags should also be determined at deploy time in CLI, since our new builder does not run any of this library's code)
     for package in &packages {
+        // Allows a custom feature set when compiling for Shuttle.
+        // ```toml
+        // [features]
+        // default = ["foo"]
+        // foo = []
+        // bar = []
+        // shuttle = ["default", "bar"]
+        // ```
+        // Check: Can shuttle->default->shuttle cause problem?
+        if package.features.contains_key("shuttle") {
+            cmd.arg("--no-default-features").arg("--features=shuttle");
+        }
         cmd.arg("--package").arg(package.name.as_str());
     }
 

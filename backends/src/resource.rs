7a5a4c6f277a8d2eda6a2993ef126164fafde6ee
oddgrd <29732646+oddgrd@users.noreply.github.com>
2024-06-09 13:11:25 +0200

    feat: make res req clone

--- backends/src/resource.rs
+++ backends/src/resource.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 /// Used by the runner service to send requests to control plane, where the requested resources
 /// will be provisioned.
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct ResourceRequest {
     /// The resource input returned from the runtime::load call.
     pub resources: Vec<Vec<u8>>,

af809d21bc14f8eb48a44300db9de26a358468c0
edgerunnergit <edgerunnergit@riseup.net>
2024-06-16 18:25:38 +0530

    added initial support for diesel-async for in aws-rds

--- resources/aws-rds/src/lib.rs
+++ resources/aws-rds/src/lib.rs
@@ -8,6 +8,15 @@ use shuttle_service::{
     DatabaseResource, DbInput, Error, IntoResource, ResourceFactory, ResourceInputBuilder,
 };
 
+#[cfg(any(feature = "diesel-async-bb8", feature = "diesel-async-deadpool"))]
+use diesel_async::pooled_connection::AsyncDieselConnectionManager;
+
+#[cfg(feature = "diesel-async-bb8")]
+use diesel_async::pooled_connection::bb8 as diesel_bb8;
+
+#[cfg(feature = "diesel-async-deadpool")]
+use diesel_async::pooled_connection::deadpool as diesel_deadpool;
+
 macro_rules! aws_engine {
     ($feature:expr, $struct_ident:ident) => {
         paste::paste! {
@@ -76,6 +85,71 @@ impl IntoResource<String> for OutputWrapper {
 }
 
 // If these were done in the main macro above, this would produce two conflicting `impl IntoResource<sqlx::MySqlPool>`
+
+#[cfg(feature = "diesel_async")]
+mod _diesel_async {
+    use super::*;
+
+    #[cfg(feature = "postgres")]
+    #[async_trait]
+    impl IntoResource<diesel_async::AsyncPgConnection> for OutputWrapper {
+        async fn into_resource(self) -> Result<diesel_async::AsyncPgConnection, Error> {
+            use diesel_async::{AsyncConnection, AsyncPgConnection};
+
+            let connection_string: String = self.into_resource().await.unwrap();
+            Ok(AsyncPgConnection::establish(&connection_string)
+                .await
+                .map_err(shuttle_service::error::CustomError::new)?)
+        }
+    }
+}
+
+#[cfg(feature = "diesel-async-bb8")]
+mod _diesel_async_bb8 {
+    use super::*;
+
+    #[cfg(feature = "postgres")]
+    #[async_trait]
+    impl IntoResource<diesel_bb8::Pool<diesel_async::AsyncPgConnection>> for OutputWrapper {
+        async fn into_resource(
+            self,
+        ) -> Result<diesel_bb8::Pool<diesel_async::AsyncPgConnection>, Error> {
+            let connection_string: String = self.into_resource().await.unwrap();
+
+            Ok(diesel_bb8::Pool::builder()
+                .min_idle(Some(1))
+                .max_size(5)
+                .build(AsyncDieselConnectionManager::new(connection_string))
+                .await
+                .map_err(shuttle_service::error::CustomError::new)?)
+        }
+    }
+}
+
+#[cfg(feature = "diesel-async-deadpool")]
+mod _diesel_async_deadpool {
+    use super::*;
+
+    #[cfg(feature = "postgres")]
+    #[async_trait]
+    impl IntoResource<diesel_deadpool::Pool<diesel_async::AsyncPgConnection>> for OutputWrapper {
+        async fn into_resource(
+            self,
+        ) -> Result<diesel_deadpool::Pool<diesel_async::AsyncPgConnection>, Error> {
+            let connection_string: String = self.into_resource().await.unwrap();
+
+            Ok(
+                diesel_deadpool::Pool::builder(AsyncDieselConnectionManager::new(
+                    connection_string,
+                ))
+                .max_size(5 as usize)
+                .build()
+                .map_err(shuttle_service::error::CustomError::new)?,
+            )
+        }
+    }
+}
+
 #[cfg(feature = "sqlx")]
 mod _sqlx {
     use super::*;

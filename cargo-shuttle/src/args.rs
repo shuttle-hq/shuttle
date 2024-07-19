16378454626df31628d47b4deb0b0b975a40ea5c
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2024-07-19 17:04:07 +0200

    feat(cargo-shuttle): beta deployment stop command (#1830)
    * feat(cargo-shuttle): beta deployment stop command
    
    * nit

--- cargo-shuttle/src/args.rs
+++ cargo-shuttle/src/args.rs
@@ -168,6 +168,8 @@ pub enum DeploymentCommand {
         /// ID of deployment to get status for
         id: Option<String>,
     },
+    /// BETA: Stop running deployment(s)
+    Stop,
 }
 
 #[derive(Parser)]

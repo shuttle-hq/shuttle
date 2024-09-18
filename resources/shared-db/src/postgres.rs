4e388eca88594dd101a8dbfb9adc5a672475ead6
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2024-09-18 18:31:31 +0200

    fix: always request shared db in deployment

--- resources/shared-db/src/postgres.rs
+++ resources/shared-db/src/postgres.rs
@@ -3,7 +3,8 @@ use serde::{Deserialize, Serialize};
 use shuttle_service::{
     database,
     resource::{ProvisionResourceRequest, ShuttleResourceOutput, Type},
-    DatabaseResource, DbInput, Error, IntoResource, ResourceFactory, ResourceInputBuilder,
+    DatabaseResource, DbInput, Environment, Error, IntoResource, ResourceFactory,
+    ResourceInputBuilder,
 };
 
 #[cfg(any(feature = "diesel-async-bb8", feature = "diesel-async-deadpool"))]
@@ -46,16 +47,24 @@ impl ResourceInputBuilder for Postgres {
     type Input = MaybeRequest;
     type Output = OutputWrapper;
 
-    async fn build(self, _factory: &ResourceFactory) -> Result<Self::Input, Error> {
-        Ok(match self.0.local_uri {
-            Some(local_uri) => {
-                MaybeRequest::NotRequest(DatabaseResource::ConnectionString(local_uri))
-            }
-            None => MaybeRequest::Request(ProvisionResourceRequest::new(
+    async fn build(self, factory: &ResourceFactory) -> Result<Self::Input, Error> {
+        let md = factory.get_metadata();
+        Ok(match md.env {
+            Environment::Deployment => MaybeRequest::Request(ProvisionResourceRequest::new(
                 Type::Database(database::Type::Shared(database::SharedEngine::Postgres)),
                 serde_json::to_value(self.0).unwrap(),
                 serde_json::Value::Null,
             )),
+            Environment::Local => match self.0.local_uri {
+                Some(local_uri) => {
+                    MaybeRequest::NotRequest(DatabaseResource::ConnectionString(local_uri))
+                }
+                None => MaybeRequest::Request(ProvisionResourceRequest::new(
+                    Type::Database(database::Type::Shared(database::SharedEngine::Postgres)),
+                    serde_json::to_value(self.0).unwrap(),
+                    serde_json::Value::Null,
+                )),
+            },
         })
     }
 }

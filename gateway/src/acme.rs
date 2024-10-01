6354536b1b1074b26f7426ec02b241541cd68174
oddgrd <29732646+oddgrd@users.noreply.github.com>
2024-10-01 15:14:17 +0200

    misc: update comment

--- gateway/src/acme.rs
+++ gateway/src/acme.rs
@@ -276,7 +276,7 @@ impl AcmeClient {
         let digest = order.key_authorization(challenge).dns_value();
         warn!("dns-01 challenge: _acme-challenge.{domain} 300 IN TXT \"{digest}\"");
 
-        // Wait 60 secs to insert the record manually and for it to
+        // Wait 50 secs to insert the record manually and for it to
         // propagate before moving on
         sleep(Duration::from_secs(50)).await;
 

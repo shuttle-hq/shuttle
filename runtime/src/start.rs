9cdaf911fb2b833f7bc8d15c6874794a3fd65ed0
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2024-10-10 15:18:34 +0200

    fix: beta mentions

--- runtime/src/start.rs
+++ runtime/src/start.rs
@@ -37,7 +37,9 @@ impl Args {
 
         if args.beta {
             if std::env::var("SHUTTLE_ENV").is_err() {
-                return Err(anyhow::anyhow!("SHUTTLE_ENV is required to be set on beta"));
+                return Err(anyhow::anyhow!(
+                    "SHUTTLE_ENV is required to be set on shuttle.dev"
+                ));
             }
         } else if args.port.is_none() {
             return Err(anyhow::anyhow!("--port is required"));

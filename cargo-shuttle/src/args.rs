9c7c887196c02e5012600fe1b5328c4ac2ecb552
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2024-11-12 13:41:06 +0100

    fix: hide redeploy command

--- cargo-shuttle/src/args.rs
+++ cargo-shuttle/src/args.rs
@@ -190,7 +190,7 @@ pub enum DeploymentCommand {
         id: Option<String>,
     },
     /// Redeploy a previous deployment (if possible)
-    #[command(visible_alias = "re")]
+    #[command(visible_alias = "re", hide = true)]
     Redeploy {
         /// ID of deployment to redeploy
         id: String,

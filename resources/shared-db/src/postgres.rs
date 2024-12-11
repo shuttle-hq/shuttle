46ef5d56e423fbcb99c19aad6c9fc14ce1f0da19
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2024-12-11 16:54:44 +0100

    feat(shared-db): opendal on postgres output type

--- resources/shared-db/src/postgres.rs
+++ resources/shared-db/src/postgres.rs
@@ -97,6 +97,7 @@ impl IntoResource<diesel_async::AsyncPgConnection> for OutputWrapper {
         use diesel_async::{AsyncConnection, AsyncPgConnection};
 
         let connection_string: String = self.into_resource().await.unwrap();
+
         Ok(AsyncPgConnection::establish(&connection_string)
             .await
             .map_err(shuttle_service::error::CustomError::new)?)
@@ -141,7 +142,7 @@ impl IntoResource<diesel_deadpool::Pool<diesel_async::AsyncPgConnection>> for Ou
 #[async_trait]
 impl IntoResource<sqlx::PgPool> for OutputWrapper {
     async fn into_resource(self) -> Result<sqlx::PgPool, Error> {
-        let connection_string: String = self.into_resource().await.unwrap();
+        let connection_string: String = self.into_resource().await?;
 
         Ok(sqlx::postgres::PgPoolOptions::new()
             .min_connections(MIN_CONNECTIONS)
@@ -149,5 +150,39 @@ impl IntoResource<sqlx::PgPool> for OutputWrapper {
             .connect(&connection_string)
             .await
             .map_err(shuttle_service::error::CustomError::new)?)
+    }
+}
+
+#[cfg(feature = "opendal")]
+#[async_trait]
+impl IntoResource<opendal::Operator> for OutputWrapper {
+    async fn into_resource(self) -> Result<opendal::Operator, Error> {
+        let connection_string: String = self.into_resource().await?;
+        let pool = sqlx::postgres::PgPoolOptions::new()
+            .min_connections(MIN_CONNECTIONS)
+            .max_connections(MAX_CONNECTIONS)
+            .connect(&connection_string)
+            .await
+            .map_err(shuttle_service::error::CustomError::new)?;
+        sqlx::query(
+            "CREATE TABLE IF NOT EXISTS opendal (key TEXT PRIMARY KEY, value BYTEA NOT NULL)",
+        )
+        .execute(&pool)
+        .await
+        .map_err(shuttle_service::error::CustomError::new)?;
+
+        let config = opendal::services::Postgresql::default()
+            .root("/")
+            .connection_string(&connection_string)
+            .table("opendal")
+            // key field type in the table should be compatible with Rust's &str like text
+            .key_field("key")
+            // value field type in the table should be compatible with Rust's Vec<u8> like bytea
+            .value_field("value");
+        let op = opendal::Operator::new(config)
+            .map_err(shuttle_service::error::CustomError::new)?
+            .finish();
+
+        Ok(op)
     }
 }

3b6ebbae479692603d5f60e475aee8794d032562
Mark S <the@wondersmith.dev>
2025-02-19 11:50:46 -0500

    perf: simplify test implementation

--- common/src/models/log.rs
+++ common/src/models/log.rs
@@ -47,43 +47,39 @@ pub struct LogsResponse {
 
 #[cfg(test)]
 mod tests {
+    #[cfg_attr(not(feature = "display"), allow(unused_imports))]
     use super::*;
 
     // Chrono uses std Time (to libc) internally, if you want to use this method
     // in more than one test, you need to handle async tests properly.
+    #[cfg(feature = "display")]
     fn with_tz<F: FnOnce()>(tz: &str, f: F) {
-        let prev_tz = std::env::var("TZ").unwrap_or("".to_string());
+        let prev_tz = std::env::var("TZ").unwrap_or_default();
         std::env::set_var("TZ", tz);
         f();
         std::env::set_var("TZ", prev_tz);
     }
 
-    #[test]
-    fn test_timezone_formatting() {
+    #[cfg(feature = "display")]
+    #[rstest::rstest]
+    #[case::utc("utc")]
+    #[case::cest("cest")]
+    fn test_timezone_formatting(#[case] tz: &str) {
         let item = LogItem::new(
             Utc::now(),
             "test".to_string(),
             r#"{"message": "Building"}"#.to_owned(),
         );
 
-        with_tz("CEST", || {
-            let cest_dt = item
+        with_tz(tz, || {
+            let value = item
                 .timestamp
                 .with_timezone(&chrono::Local)
                 .to_rfc3339_opts(chrono::SecondsFormat::Millis, false);
+
             let log_line = format!("{}", &item);
 
-            assert!(log_line.contains(&cest_dt));
-        });
-
-        with_tz("UTC", || {
-            let utc_dt = item
-                .timestamp
-                .with_timezone(&chrono::Local)
-                .to_rfc3339_opts(chrono::SecondsFormat::Millis, false);
-            let log_line = format!("{}", &item);
-
-            assert!(log_line.contains(&utc_dt));
+            assert!(log_line.contains(&value));
         });
     }
 }

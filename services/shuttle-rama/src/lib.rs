f25c8359a8baa60622f65ea0bbbaa63f65d7ce78
Glen De Cauwsemaecker <contact@glendc.com>
2025-05-18 14:51:14 +0200

    improve rama code + sync lib code with rama 0.2

--- services/shuttle-rama/src/lib.rs
+++ services/shuttle-rama/src/lib.rs
@@ -1,9 +1,16 @@
 #![doc = include_str!("../README.md")]
 
+use rama::{
+    error::OpaqueError,
+    http::{server::HttpServer, service::web::response::IntoResponse, Request},
+    rt::Executor,
+    tcp::server::TcpListener,
+    Service,
+};
 use shuttle_runtime::{tokio, CustomError, Error};
 use std::{convert::Infallible, fmt, net::SocketAddr};
 
-/// A wrapper type for [rama::Service] so we can implement [shuttle_runtime::Service] for it.
+/// A wrapper type for [`Service`] so we can implement [`shuttle_runtime::Service`] for it.
 pub struct RamaService<T, State> {
     svc: T,
     state: State,
@@ -74,7 +81,9 @@ impl<S> RamaService<Application<S>, ()> {
 
 impl<T> RamaService<T, ()> {
     /// Attach state to this [`RamaService`], such that it will be passed
-    /// as part of each request's [`rama::Context`].
+    /// as part of each request's [`Context`].
+    ///
+    /// [`Context`]: rama::Context
     pub fn with_state<State>(self, state: State) -> RamaService<T, State>
     where
         State: Clone + Send + Sync + 'static,
@@ -89,13 +98,13 @@ impl<T> RamaService<T, ()> {
 #[shuttle_runtime::async_trait]
 impl<S, State> shuttle_runtime::Service for RamaService<Transport<S>, State>
 where
-    S: rama::Service<State, tokio::net::TcpStream>,
+    S: Service<State, tokio::net::TcpStream>,
     State: Clone + Send + Sync + 'static,
 {
     /// Takes the service that is returned by the user in their [shuttle_runtime::main] function
     /// and binds to an address passed in by shuttle.
     async fn bind(self, addr: SocketAddr) -> Result<(), Error> {
-        rama::tcp::server::TcpListener::build_with_state(self.state)
+        TcpListener::build_with_state(self.state)
             .bind(addr)
             .await
             .map_err(|err| Error::BindPanic(err.to_string()))?
@@ -108,17 +117,17 @@ where
 #[shuttle_runtime::async_trait]
 impl<S, State, Response> shuttle_runtime::Service for RamaService<Application<S>, State>
 where
-    S: rama::Service<State, rama::http::Request, Response = Response, Error = Infallible>,
-    Response: rama::http::IntoResponse + Send + 'static,
+    S: Service<State, Request, Response = Response, Error = Infallible>,
+    Response: IntoResponse + Send + 'static,
     State: Clone + Send + Sync + 'static,
 {
     /// Takes the service that is returned by the user in their [shuttle_runtime::main] function
     /// and binds to an address passed in by shuttle.
     async fn bind(self, addr: SocketAddr) -> Result<(), Error> {
-        rama::http::server::HttpServer::auto(rama::rt::Executor::new())
+        HttpServer::auto(Executor::new())
             .listen_with_state(self.state, addr, self.svc.0)
             .await
-            .map_err(|err| CustomError::new(rama::error::OpaqueError::from_boxed(err)))?;
+            .map_err(|err| CustomError::new(OpaqueError::from_boxed(err)))?;
         Ok(())
     }
 }

2622f1d5287c41cab8ae2316c6fb5d10008036b7
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2025-06-12 22:50:57 +0200

    add tests

--- cargo-shuttle/tests/integration/builder.rs
+++ cargo-shuttle/tests/integration/builder.rs
@@ -21,10 +21,24 @@ async fn not_shuttle() {
 async fn not_bin() {
     let (tx, _) = tokio::sync::mpsc::channel::<String>(256);
     let project_path = concat!(env!("CARGO_MANIFEST_DIR"), "/tests/resources/not-bin");
-    match build_workspace(Path::new(&project_path), false, tx).await {
-        Ok(_) => {}
-        Err(e) => panic!("{}", e.to_string()),
-    }
+    build_workspace(Path::new(&project_path), false, tx)
+        .await
+        .unwrap();
+}
+
+#[tokio::test]
+#[should_panic(
+    expected = "Expected at least one package that Shuttle can build. Make sure your crate has a binary target that uses `#[shuttle_runtime::main]`."
+)]
+async fn not_full_macro() {
+    let (tx, _) = tokio::sync::mpsc::channel::<String>(256);
+    let project_path = concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/tests/resources/not-full-macro"
+    );
+    build_workspace(Path::new(&project_path), false, tx)
+        .await
+        .unwrap();
 }
 
 #[tokio::test]
@@ -40,6 +54,23 @@ async fn is_bin() {
             workspace_path: PathBuf::from(&project_path),
             target_name: "is-bin".to_string(),
             executable_path: PathBuf::from(&project_path).join("target/debug/is-bin"),
+        }
+    );
+}
+
+#[tokio::test]
+async fn is_bin2() {
+    let (tx, _) = tokio::sync::mpsc::channel::<String>(256);
+    let project_path = concat!(env!("CARGO_MANIFEST_DIR"), "/tests/resources/is-bin2");
+
+    assert_eq!(
+        build_workspace(Path::new(&project_path), false, tx)
+            .await
+            .unwrap(),
+        BuiltService {
+            workspace_path: PathBuf::from(&project_path),
+            target_name: "weirdchamp".to_string(),
+            executable_path: PathBuf::from(&project_path).join("target/debug/weirdchamp"),
         }
     );
 }

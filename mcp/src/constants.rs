5bf97cbe21c9dd0f1948ae2528e1b84fe29f372e
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2025-06-30 17:29:37 +0300

    removed mcp server crate

--- mcp/src/constants.rs
+++
@@ -1,3 +0,0 @@
-pub const SHUTTLE_DOCS_SEARCH_BASE_URL: &'static str =
-    "https://09ga7allme.execute-api.eu-west-2.amazonaws.com/prod";
-pub const INSTALL_SHUTTLE_MCP_COMMAND: &'static str = "cargo install shuttle-mcp";
